eda5f4a023311cfbc61ffc6bbac6a9c1dda456ce
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-07 14:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (38.94%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-       int pos;
-       int count=0;
-       int k=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==val)
-            {
-                count++;
-            }
-            else
-            {
-                k++;
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
         }
-while(count--)
-{
-    for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==val)
-            {
-                pos=i;
-                break;
-            }
-        }
-        for(int i=pos;i<nums.size()-1;i++)
-        {
-            nums[i]=nums[i+1];
-        }
-
-}
-return k;
-        
+        return index;
     }
 };
+
+
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//        int pos;
+//        int count=0;
+//        int k=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]==val)
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 k++;
+//             }
+//         }
+// while(count--)
+// {
+//     for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]==val)
+//             {
+//                 pos=i;
+//                 break;
+//             }
+//         }
+//         for(int i=pos;i<nums.size()-1;i++)
+//         {
+//             nums[i]=nums[i+1];
+//         }
+
+// }
+// return k;
+        
+//     }
+// };
