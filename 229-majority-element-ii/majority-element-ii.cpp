07f54e0776105ee21196fe4069c47d34c7e4c570
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-07 18:57:45 +0530

    Time: 11 ms (59.30%) | Memory: 16.4 MB (40.39%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int maj1;
+        int count1=0;
+
+        int maj2;
+        int count2=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==maj1)
+            {
+                count1++;
+            }
+            else if(nums[i]==maj2)
+            {
+                count2++;
+            }
+            else if(count1==0)
+            {
+                maj1=nums[i];
+                count1=1;
+            }
+            else if(count2==0)
+            {
+                maj2=nums[i];
+                count2=1;
+            }
+            else 
+            {
+                count1--;
+                count2--;
+            }
+
+        }
+
+        //now verify maj1 and maj2
+
+        int freq1=0,freq2=0;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==maj1)
+            {
+                freq1++;
+            }
+            if(nums[i]==maj2)
+            {
+                freq2++;
+            }
+        }
+
+        if(freq1>floor(nums.size()/3))
+        ans.push_back(maj1);
+
+        if(freq2>floor(nums.size()/3))
+        ans.push_back(maj2);
+        return ans;
+    }
+};
