0d90fd82a954735d4adf8fc8e79ea3d9925e30ce
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-13 18:39:35 +0530

    Time: 118 ms (55.29%) | Memory: 88.8 MB (70.34%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -7,7 +7,7 @@ public:
             res=res^nums[i];
         }
         int ans=0;
-        while(k!=0&&res!=0)
+        while(k!=0 || res!= 0)         //here instead of and , we use or 
         {
             if((k&1)!=(res&1))
             {
@@ -16,18 +16,18 @@ public:
             res=res>>1;
             k=k>>1;
         }
-        while(k!=0)
-        {
-            if((k&1)==1)
-            ans++;
-            k=k>>1;
-        }
-        while(res!=0)
-        {
-            if((res&1)==1)
-            ans++;
-            res=res>>1;
-        }
+        // while(k!=0)  //no use if we make or operation instead of and opreation
+        // {
+        //     if((k&1)==1)
+        //     ans++;
+        //     k=k>>1;
+        // }
+        // while(res!=0)
+        // {
+        //     if((res&1)==1)
+        //     ans++;
+        //     res=res>>1;
+        // }
         return ans;
 
 
