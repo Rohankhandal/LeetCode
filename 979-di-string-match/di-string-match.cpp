59fb0165ee73ae88b8203cf650bb26d951675dc5
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-14 16:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (84.95%) - LeetSync

--- 979-di-string-match/di-string-match.cpp
+++ 979-di-string-match/di-string-match.cpp
@@ -1,23 +1,46 @@
+// 942. DI String Match
+// https://leetcode.com/problems/di-string-match/
+// Difference between push_back and emplance_back
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
-        int n=s.size();
-        vector<int>ans;
-        int l=0,r=n;
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='I')
-            {
-                 ans.push_back(l);
-                 l++;
-            }
-            else
-            {
-            ans.push_back(r);
-            r--;
-            }
+        vector<int> result;
+        int n = s.size();
+        result.reserve(n + 1);
+        int l = 0, r = n;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'I') result.emplace_back(l++);
+            else result.emplace_back(r--);
         }
-        ans.push_back(l);
-        return ans;
+        // l is equal to r
+        result.emplace_back(l);
+
+        return result;
     }
 };
+
+// using push_back
+// class Solution {
+// public:
+//     vector<int> diStringMatch(string s) {
+//         int n=s.size();
+//         vector<int>ans;
+//         int l=0,r=n;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s[i]=='I')
+//             {
+//                  ans.push_back(l);
+//                  l++;
+//             }
+//             else
+//             {
+//             ans.push_back(r);
+//             r--;
+//             }
+//         }
+//         ans.push_back(l);
+//         return ans;
+//     }
+// };
