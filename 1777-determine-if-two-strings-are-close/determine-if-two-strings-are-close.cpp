ffba3c3dd74ab0c939f0de4ab27a7881b1ed432c
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-14 19:28:56 +0530

    Time: 70 ms (84.64%) | Memory: 20.8 MB (96.46%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -15,7 +15,7 @@ public:
             freq1[word1[i]-'a']++;
             freq2[word2[i]-'a']++;
         }
-
+//point1 :- jo char word1 me ha, wo char word2 me bhi  hona chaiya
         for(int i=0;i<26;i++)
         {
             if(freq1[i]!=0 && freq1[i]!=0) continue;
@@ -24,7 +24,7 @@ public:
 
             return false;
         }
-
+//point2:- match the freq
         sort(freq1.begin(),freq1.end());
         sort(freq2.begin(),freq2.end());
         
