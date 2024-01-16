a6499e87db50372a3d37f5e6cb766f60683718d9
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-16 23:37:43 +0530

    Time: 19 ms (87.17%) | Memory: 25.3 MB (84.49%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -7,7 +7,7 @@ public:
     // O(1)
     void rotate(vector<int>& nums, int k) {
         
-     k=k%nums.size();
+     k=k%nums.size();   //Imp part
         reverse(nums.begin(),nums.begin()+(nums.size()-k));
 
         reverse(nums.begin()+(nums.size()-k),nums.end());
