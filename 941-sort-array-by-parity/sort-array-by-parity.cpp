1b01cec8647b7bb939f865567c7c7053a5502ded
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-16 23:24:33 +0530

    Time: 13 ms (24.66%) | Memory: 16.5 MB (64.61%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,19 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         int sz = nums.size(), left = 0, right = sz-1;
+//         while(left < right){
+            
+//             while(left < right && nums[left] % 2 == 0){
+//                 left++;
+//             }
+//             while(right > left && nums[right] % 2 != 0){
+//                 right--;
+//             }
+//             if(left < right){
+//                 swap(nums[left], nums[right]);
+//             }
+//         }
+//         return nums;
+//     }
+// };
+
+
+// Using comparator Function
 class Solution {
 public:
+    static bool comparator(int &a  , int & b){
+      return a%2 < b%2 ;
+    }
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int sz = nums.size(), left = 0, right = sz-1;
-        while(left < right){
-            
-            while(left < right && nums[left] % 2 == 0){
-                left++;
-            }
-            while(right > left && nums[right] % 2 != 0){
-                right--;
-            }
-            if(left < right){
-                swap(nums[left], nums[right]);
-            }
-        }
+        int n = nums.size(); 
+        if(n==1) return nums;
+        sort(nums.begin() , nums.end() , comparator);
         return nums;
     }
+
 };
