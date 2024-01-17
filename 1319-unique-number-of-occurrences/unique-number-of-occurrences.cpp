99927e7a3563aa0cdd978378becd3f9c3341496f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-17 15:07:11 +0530

    Time: 8 ms (9.35%) | Memory: 9.1 MB (9.43%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,16 +1,62 @@
+// using constraints
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>mp;
-        for(auto it:arr)
+        vector<int>vec(2001,0);
+        for(int &x:arr)
         {
-             mp[it]++;
+            vec[x+1000]++;
         }
-       set<int>st;
-       for(auto it:mp)
-       {
-           st.insert(it.second);
-       }
-       return st.size()==mp.size()?true:false;
+
+        sort(vec.begin(),vec.end());
+
+        for(int i=0;i<2001;i++)
+        {
+            if(vec[i]!=0&&vec[i]==vec[i-1])
+            return false;
+        }
+        return true;
     }
 };
+
+
+//using unordered_set
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map<int,int>mp;
+//         for(auto it:arr)
+//         {
+//              mp[it]++;
+//         }
+//        unordered_set<int>st;
+//        for(auto &it:mp)
+//        {
+//            int freq=it.second;
+//            if(st.find(freq)!=st.end())
+//            {
+//                return false;
+//            }
+//            st.insert(freq);
+//        }
+//        return true;
+//     }
+// };
+
+// Method-1
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map<int,int>mp;
+//         for(auto it:arr)
+//         {
+//              mp[it]++;
+//         }
+//        set<int>st;
+//        for(auto it:mp)
+//        {
+//            st.insert(it.second);
+//        }
+//        return st.size()==mp.size()?true:false;
+//     }
+// };
