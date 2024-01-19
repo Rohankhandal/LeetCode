4070f254f5313e88657655e8991ac36691b08b53
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-19 17:45:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (5.27%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,42 +1,65 @@
 class Solution {
 public:
-void phoneNumber(vector<string>&ans,vector<string>&matching,string output,int i,string digits)
-{
-    if(i>=digits.size())
-    {
-        ans.push_back(output);
-        return ;
-    }
-    int intDigit=digits[i]-'0';
-    string temp=matching[intDigit];
-    for(int j=0;j<temp.size();j++)
-    {
-        char ch=temp[j];
-        phoneNumber(ans,matching,output+ch,i+1,digits);
+
+    void solve(vector<string> &ans,int index, string output, string digits,vector<string> &mapping) {
+        //base case
+        if(index >= digits.length()) {
+            ans.push_back(output);
+            return;
+        }
+
+        //1 case solve, baaki recursion
+        char digitCharacter = digits[index];
+
+        int digitInteger = digitCharacter -'0';
+
+        string value = mapping[digitInteger];
+
+        for(int i=0; i<value.length(); i++) {
+            char ch = value[i];
+
+            //include
+            //output.push_back(ch);
+            //recursive call
+            solve(ans, index+1, output + ch, digits, mapping);
+            //backtrack
+           // output.pop_back();
+        }
 
     }
-}
+ 
     vector<string> letterCombinations(string digits) {
-        vector<string>ans;
-        if(digits.size()==0)
-        {
+        vector<string> ans;
+        //empty string k liye answer empty array hoga 
+        if(digits.length() == 0)
             return ans;
-        }
-        vector<string>matching(10);
-        matching[2]="abc";
-        matching[3]="def";
-        matching[4]="ghi";
-        matching[5]="jkl";
-        matching[6]="mno";
-        matching[7]="pqrs";
-        matching[8]="tuv";
-        matching[9]="wxyz";
-
-        int i=0;
-        string output="";
-        phoneNumber(ans,matching,output,i,digits);
-        return ans;
         
-        }
-    
+        int index = 0;
+        string output = "";
+        vector<string> mapping(10);
+        mapping[2] = "abc";
+        mapping[3] = "def";
+        mapping[4] = "ghi";
+        mapping[5] = "jkl";
+        mapping[6] = "mno";
+        mapping[7] = "pqrs";
+        mapping[8] = "tuv";
+        mapping[9] = "wxyz";
+
+        // unordered_map<int, string> mapping;
+        // mapping[2] = "abc";
+        // mapping[3] = "def";
+        // mapping[4] = "ghi";
+        // mapping[5] = "jkl";
+        // mapping[6] = "mno";
+        // mapping[7] = "pqrs";
+        // mapping[8] = "tuv";
+        // mapping[9] = "wxyz";
+
+        solve(ans, index, output, digits, mapping);
+
+        return ans;
+
+        
+    }
 };
