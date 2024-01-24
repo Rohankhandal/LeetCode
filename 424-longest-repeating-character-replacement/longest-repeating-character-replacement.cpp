225fe724afe05b61f9ea67742eecc3900fc896bf
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-25 01:26:42 +0530

    Time: 7 ms (82.05%) | Memory: 8.3 MB (10.90%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -6,15 +6,15 @@ public:
         vector<int>hash(26,0);
         while(end<s.size())
         {
-            hash[s[end]-'A']++;
-            maxFreq=max(maxFreq,hash[s[end]-'A']);
-            if(end-start+1-maxFreq>k)
+            hash[s[end]-'A']++;  //count the frequency
+            maxFreq=max(maxFreq,hash[s[end]-'A']);  //compare with maxFreq
+            if(end-start+1-maxFreq>k)   //check whether window is valid after replacement of k char (or imagine after removing ) window is valid or not
             {
-                hash[s[start]-'A']--;
+                hash[s[start]-'A']--;  //if not valid then increment start pointer
                 start++;
                 maxFreq=*max_element(hash.begin(),hash.end());
             }
-            res=max(res,end-start+1);
+            res=max(res,end-start+1);   //find largest window
             end++;
         }
         return res;
