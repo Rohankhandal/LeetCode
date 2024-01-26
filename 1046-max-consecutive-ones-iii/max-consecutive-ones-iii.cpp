930101d6d60ff519edc841e7e25a66e6d2000267
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-26 15:58:10 +0530

    Time: 42 ms (77.62%) | Memory: 57.7 MB (11.99%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,23 +1,49 @@
+//Means atmost K 0 is possible in our window
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        int right=0,left=0;
-        int ans=0;
-        while(right<nums.size())
-        {
-            mp[nums[right]]++;
-            if(right-left+1-mp[1]>k)
-            {
-                mp[nums[left]]--;
-                left++;
-
-            }
-            
-            ans=max(right-left+1,ans);
-            right++;
-        }
-        return ans;
-
+      int left=0,right=0,maxOnes=0,zeroCount=0;
+      while(right<nums.size())
+      {
+          if(nums[right]==0)
+          {
+              zeroCount++;
+          }
+          while(zeroCount>k)
+          {
+              if(nums[left]==0)
+              {
+                  zeroCount--;
+              }
+              left++;
+          }
+          maxOnes=max(maxOnes,right-left+1);
+          right++;
+      }
+      return maxOnes;
     }
 };
+// T.C:-O(n) S.C :O(n)  sliding window
+// class Solution {
+// public:
+//     int longestOnes(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         int right=0,left=0;
+//         int ans=0;
+//         while(right<nums.size())
+//         {
+//             mp[nums[right]]++;
+//             if(right-left+1-mp[1]>k)
+//             {
+//                 mp[nums[left]]--;
+//                 left++;
+
+//             }
+            
+//             ans=max(right-left+1,ans);
+//             right++;
+//         }
+//         return ans;
+
+//     }
+// };
