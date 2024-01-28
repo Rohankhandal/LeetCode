fe86706f2917d0b3a2b55c742c24ce522deb5ad0
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-28 12:21:06 +0530

    Time: 3 ms (67.90%) | Memory: 14.7 MB (9.46%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
-        int preSum=0;
-        int index=-1;
-        int i;
-        for( i=0;i<nums.size()-1;i++)
+        int n=nums.size(),sum=0,lefts=0;
+        for(auto i:nums)
+        sum+=i;
+        for(int i=0;i<n;i++)
         {
-            int endSum=0;
-            for(int j=i+1;j<nums.size();j++)
-            {
-                endSum+=nums[j];
-            }
-            if(preSum==endSum)
-            {
-                index=i;
-                break;
-            }
-            preSum+=nums[i];
+            if(lefts==sum-nums[i])
+            return i;
+            lefts+=nums[i];
+            sum-=nums[i];
         }
-        if(i==nums.size()-1 && preSum==0)
-        {
-            index=i;
-        }
-        return index;
+        return -1;
     }
 };
+// Time complexity:-O(N)
+// class Solution {
+// public:
+//     int findMiddleIndex(vector<int>& nums) {
+//         int preSum=0;
+//         int index=-1;
+//         int i;
+//         for( i=0;i<nums.size()-1;i++)
+//         {
+//             int endSum=0;
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 endSum+=nums[j];
+//             }
+//             if(preSum==endSum)
+//             {
+//                 index=i;
+//                 break;
+//             }
+//             preSum+=nums[i];
+//         }
+//         if(i==nums.size()-1 && preSum==0)
+//         {
+//             index=i;
+//         }
+//         return index;
+//     }
+// };
