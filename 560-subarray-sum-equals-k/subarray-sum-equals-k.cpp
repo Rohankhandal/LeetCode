91c3d42d0b482dfb683e854dc1584e4796e1b839
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-29 15:42:51 +0530

    Time: 104 ms (30.13%) | Memory: 52.5 MB (6.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,20 +1,51 @@
-class Solution {
+// TC:-N*logN or N
+// if we are using an unordered_map data structure in C++ the time complexity will be O(N) but if we are using a map data structure, the time complexity will be O(N*logN). The least complexity will be O(N) as we are using a loop to traverse the arra 
+class Solution {   
+
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            int sum=0;
-            for(int j=i;j<n;j++)
-            {
-                sum+=nums[j];
-                if(sum==k)
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
+        int n = nums.size(); // size of the given array.
+    map<int,int> mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += nums[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
     }
 };
+
+
+// brute Force Method:
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             int sum=0;
+//             for(int j=i;j<n;j++)
+//             {
+//                 sum+=nums[j];
+//                 if(sum==k)
+//                 {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
