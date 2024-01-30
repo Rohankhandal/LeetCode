7462ceaa01221593856332c02638e39e109e3af3
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-30 10:24:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (7.49%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -9,31 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+ class Solution {
 public:
-bool solve(TreeNode* p, TreeNode* q)
-{
-    if(p==NULL && q == NULL){
-        return true;
-    }
-    if(p==NULL)
-    {
-        return false;
-    }
-    if(q==NULL)
-    {
-        return false;
-    }
-    if(p->val!=q->val)
-    {
-        return false;
-    }
-    bool res1=solve(p->left,q->left);
-    bool res2=solve(p->right,q->right);
-    return res1 && res2;
-}
+
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        bool result=solve(p,q);
-        return result;
+       if(!p && !q)   //when both are  equal to NULL
+       {
+           return true;
+       }
+       if(p && q)  //when both are not equal to NULL
+       {
+           return (p->val == q->val) && isSameTree(p->left,q->left)
+           && isSameTree(p->right,q->right);
+       }
+       return false;  //when p or q are NULL
     }
 };
+// class Solution {
+// public:
+// bool solve(TreeNode* p, TreeNode* q)
+// {
+//     if(p==NULL && q == NULL){
+//         return true;
+//     }
+//     if(p==NULL)
+//     {
+//         return false;
+//     }
+//     if(q==NULL)
+//     {
+//         return false;
+//     }
+//     if(p->val!=q->val)
+//     {
+//         return false;
+//     }
+//     bool res1=solve(p->left,q->left);
+//     bool res2=solve(p->right,q->right);
+//     return res1 && res2;
+// }
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         bool result=solve(p,q);
+//         return result;
+//     }
+// };
