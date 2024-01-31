49cc2681691b7b4b159060751fccafb824bd007b
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-01-31 19:15:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (10.21%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,24 +1,36 @@
-//Using binary search O(logN)
+
+//O(1)
 class Solution {
 public:
     int arrangeCoins(int n) {
-       long long left=0,high=n;
-       int ans=1;
-       while(left<=high)
-       {
-           long mid=left+(high-left)/2;
-           long long sum=mid*(mid+1)/2;
-           if(sum<=n)
-           {
-           ans=mid;
-           left=mid+1;
-           }
-           else
-           {
-               high=mid-1;
-           }
-
-       }
-       return ans;
+       return floor((-1 + sqrt(1 + (long)8 * n)) / 2); 
     }
 };
+
+
+
+
+//Using binary search O(logN)
+// class Solution {
+// public:
+//     int arrangeCoins(int n) {
+//        long long left=0,high=n;
+//        int ans=1;
+//        while(left<=high)
+//        {
+//            long mid=left+(high-left)/2;
+//            long long sum=mid*(mid+1)/2;
+//            if(sum<=n)
+//            {
+//            ans=mid;
+//            left=mid+1;
+//            }
+//            else
+//            {
+//                high=mid-1;
+//            }
+
+//        }
+//        return ans;
+//     }
+// };
