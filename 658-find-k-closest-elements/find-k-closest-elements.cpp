904e97ca72f0f68a5fc0989d28885bf5ddfaba41
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-06 19:43:09 +0530

    Time: 29 ms (72.22%) | Memory: 33.5 MB (49.65%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,29 +1,84 @@
 class Solution {
 public:
-vector<int> twoPtrMethod(vector<int>&arr, int k, int x) 
+int lowerBound(vector<int>&arr,int x)
 {
-    int l=0,h=arr.size()-1;
-    while(h-l>=k)
+    int start=0,end=arr.size()-1;
+    // int ans=-1;
+    int ans=end;
+    while(start<=end)
     {
-        if(x-arr[l] > arr[h] -x )  //comparsion respect of difference with x
+        int mid=(start+end)/2;
+        if(arr[mid]>=x)
         {
-            l++;  //shrink array if diff is greater than the h pointer element diff
+            ans=mid;
+            end=mid-1;
+        }
+        else if(x>arr[mid])
+        {
+            start=mid+1;
         }
         else {
-            h--;
+            end=mid-1;
         }
     }
-    // vector<int>ans;
-    // for(int i=l;i<=h;i++)
-    // {
-    //     ans.push_back(arr[i]);
-    // }
-    // return ans;
-    // or
+    return ans;
+}
+vector<int> bs_method(vector<int>&arr, int k, int x) 
+//e.g 12,16,22,30,35,39,42,45,48,50,53,55,56
+{
+    //lower bound
+    int h=lowerBound(arr,x);
+    int l=h-1;
+    while(k--)
+    {
+        //EDGE CASE:
+        if(l<0)
+        {
+            h++;
+        }
+        else if(h>=arr.size())
+        {
+            l--;
+        }
+        else if(x-arr[l]>arr[h]-x)
+        {
+            h++;
+        }
+        else {
+            l--;
+        }
+    }
+    return vector<int>(arr.begin()+l+1,arr.begin()+h);
+
 
     return vector<int>(arr.begin()+l,arr.begin()+h+1);
 }
+// vector<int> twoPtrMethod(vector<int>&arr, int k, int x) 
+// //e.g 12,16,22,30,35,39,42,45,48,50,53,55,56
+// {
+//     int l=0,h=arr.size()-1;
+//     while(h-l>=k)
+//     {
+//         if(x-arr[l] > arr[h] -x )  //comparsion respect of difference with x
+//         {
+//             l++;  //shrink array if diff is greater than the h pointer element diff
+//         }
+//         else {
+//             h--;
+//         }
+//     }
+//     // vector<int>ans;
+//     // for(int i=l;i<=h;i++)
+//     // {
+//     //     ans.push_back(arr[i]);
+//     // }
+//     // return ans;
+//     // or
+
+//     return vector<int>(arr.begin()+l,arr.begin()+h+1);
+// }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPtrMethod(arr,k,x);
+        // return twoPtrMethod(arr,k,x);
+        return bs_method(arr,k,x);
     }
 };
