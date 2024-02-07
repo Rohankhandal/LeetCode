8d2948b16ac69920e1a9d13c35f151f942827af0
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-08 01:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (28.98%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,58 +1,89 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
-
-
- //Time Complexity: O(L)+O(L-N), We are calculating the length of the linked list
- // and then iterating up to the (L-N)th node of the linked list, where L is the total length of the list.
-
-// Space Complexity:  O(1), as we have not used any extra space.
 class Solution
 {
     public:
-        int getLength(ListNode *head)
+        ListNode* removeNthFromEnd(ListNode *head, int n)
         {
-            int len = 0;
-            ListNode *temp = head;
-            while (temp != NULL)
-            {
-                len++;
-                temp = temp->next;
-            }
-            return len;
-        }
-    ListNode* removeNthFromEnd(ListNode *head, int n)
-    {
-        int len = getLength(head);
-        if (n == len)
-        {
-            ListNode *newhead = head->next;
-            delete(head);
-            return newhead;
-        }
+           	// Create two pointers, fastp and slowp
+            ListNode *fastp = head;
+            ListNode *slowp = head;
 
-        int rem = len - n;
-        ListNode *temp = head;
-        while (temp != NULL)
-        {
-            rem--;
-            if (rem == 0)
+           	// Move the fastp pointer N nodes ahead
+            for (int i = 0; i < n; i++)
+                fastp = fastp->next;
+
+           	// If fastp becomes NULL,
+           	// the Nth node from the end is the head
+            if (fastp == NULL)
+                return head->next;
+
+           	// Move both pointers until fastp reaches the end
+            while (fastp->next != NULL)
             {
-                break;
+                fastp = fastp->next;
+                slowp = slowp->next;
             }
-            temp = temp->next;
+
+           	// Delete the Nth node from the end
+            ListNode *delNode = slowp->next;
+            slowp->next = slowp->next->next;
+            delete delNode;
+            return head;
         }
-       	// Delete the Nth node from the end
-        ListNode *delNode = temp->next;
-        temp->next = temp->next->next;
-        delete(delNode);
-        return head;
-    }
 };
+
+//Time Complexity: O(L)+O(L-N), We are calculating the length of the linked list
+// and then iterating up to the (L-N)th node of the linked list, where L is the total length of the list.
+
+// Space Complexity:  O(1), as we have not used any extra space.
+// class Solution
+// {
+//     public:
+//         int getLength(ListNode *head)
+//         {
+//             int len = 0;
+//             ListNode *temp = head;
+//             while (temp != NULL)
+//             {
+//                 len++;
+//                 temp = temp->next;
+//             }
+//             return len;
+//         }
+//     ListNode* removeNthFromEnd(ListNode *head, int n)
+//     {
+//         int len = getLength(head);
+//         if (n == len)
+//         {
+//             ListNode *newhead = head->next;
+//             delete(head);
+//             return newhead;
+//         }
+
+//         int rem = len - n;
+//         ListNode *temp = head;
+//         while (temp != NULL)
+//         {
+//             rem--;
+//             if (rem == 0)
+//             {
+//                 break;
+//             }
+//             temp = temp->next;
+//         }
+//        	// Delete the Nth node from the end
+//         ListNode *delNode = temp->next;
+//         temp->next = temp->next->next;
+//         delete(delNode);
+//         return head;
+//     }
+// };
