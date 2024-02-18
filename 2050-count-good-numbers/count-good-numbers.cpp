1309216301f36474ab0113c43ac9dac4692f3208
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-18 16:10:15 +0530

    Time: 3 ms (35.84%) | Memory: 7.2 MB (25.90%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,23 +1,43 @@
-#define mod 1000000007
+#define M 1000000007
 class Solution {
 public:
-    long long power(long long x,long long y)   //T.c:-O(logN)  S.c:-O(logN) (recusion call stack)
+    long long power(long long x,long long y)
     {
-        if(y==0) return 1;
+        long long ans=1;
+        while(y>0)
+        {
+            if(y&1)  //odd number
+            {
+                ans=(ans*x)%M; 
+            }
+            x=(x*x)%M;
 
-        long long ans=power(x,y/2);
-        ans*=ans;  //ans=(x,y/2)*(x,y/2)
-        ans%=mod;
-
-        if(y%2) ans*=x; //(x,7) =(x,3)*(x,3)*x
-        ans%=mod;
-        return  ans;
+            y>>=1;  //y=y/2 right shift
+        }
+        return ans%M;
     }
+    
     int countGoodNumbers(long long n) {
         long long odd=n/2;
         long long even=n/2+n%2;
 
-        return (power(5,even)* power(4,odd))%mod;  //we have define our own power function 
+        return (power(5,even)* power(4,odd))%M;  //we have define our own power function 
         // otherwise , we get TLE
     }
 };
+
+
+
+
+// long long power(long long x,long long y)   //T.c:-O(logN)  S.c:-O(logN) (recusion call stack)
+//     {
+//         if(y==0) return 1;
+
+//         long long ans=power(x,y/2);
+//         ans*=ans;  //ans=(x,y/2)*(x,y/2)
+//         ans%=mod;
+
+//         if(y%2) ans*=x; //(x,7) =(x,3)*(x,3)*x
+//         ans%=mod;
+//         return  ans;
+//     }
