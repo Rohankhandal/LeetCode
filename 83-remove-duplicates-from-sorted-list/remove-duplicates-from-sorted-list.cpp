4dd416e10489d20f84b15d2489282aac16a0222e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-19 09:43:26 +0530

    Time: 12 ms (17.88%) | Memory: 15.1 MB (45.68%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,26 +11,70 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-      if(head==NULL || head->next==NULL)
+        if(head==NULL || head->next==NULL)
+            return head;
+      ListNode*prev=head;
+      ListNode*temp=prev->next;
+
+      while(temp!=NULL)
       {
-          return head;
-      }   
-      ListNode*temp=head;
-      while(temp!=NULL && temp->next!=NULL)
-      {
-          while(temp!=NULL && temp->next!=NULL && temp->val==temp->next->val)
+          if(temp->val==prev->val)
           {
-              ListNode*del=temp->next;
-              temp->next=temp->next->next;
-              //update temp
-              //   temp=temp->next;
-              //delete the node
-              delete del;
-          }
-          
               temp=temp->next;
-          
+              continue;
+          }
+          prev->next=temp;
+          prev=temp;
+          temp=temp->next;
       }
+
+      prev->next=NULL;  //IMP at last if temp goes to NULL , then loop is end
+      //without updating the prev
+      //e.g 1->2->2->3->4->4->4->NULL
       return head;
     }
 };
+
+
+
+
+
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//       if(head==NULL || head->next==NULL)
+//       {
+//           return head;
+//       }   
+//       ListNode*temp=head;
+//       while(temp!=NULL && temp->next!=NULL)
+//       {
+//           while(temp!=NULL && temp->next!=NULL && temp->val==temp->next->val)
+//           {
+//               ListNode*del=temp->next;
+//               temp->next=temp->next->next;
+//               //update temp
+//               //   temp=temp->next;  //we can't update until all matching number are not delete (if all matching numbers are in sequence)
+//               //delete the node
+//               delete del;
+//           }
+          
+//               temp=temp->next;
+          
+//       }
+//       return head;
+//     }
+// };
