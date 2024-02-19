1d198da4be02f61af368cf9408243f63491064fc
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-19 09:25:17 +0530

    Time: 12 ms (86.21%) | Memory: 18.5 MB (30.25%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -11,38 +11,84 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
+        //create a dummmy node , and add head to dummy node so that ,
+        //we can delete nodes from starting also
         if(head==NULL)
-          return head;
-        while(head!=NULL&&head->val==val)
+         return head;
+        ListNode*dummy=new ListNode(-1);
+        dummy->next=head;
+
+        ListNode*curr=dummy;
+        while(curr->next!=NULL)
         {
-            ListNode*del=head;
-            head=head->next;
-            del->next=NULL;
-            delete del;
-        }
-        if(head==NULL)
-          return head;
-        ListNode*temp=head;
-        ListNode*prev=NULL;
-        while(temp!=NULL)
-        {
-            if(temp->val==val )
+            //check if value matches
+            if(curr->next->val==val)
             {
-                ListNode*del=temp;
-                
-                prev->next=temp->next;
-                
-                temp=temp->next;
-                del->next=NULL;
-                delete del;
+                curr->next=curr->next->next;
             }
             else
             {
-                prev=temp;
-                temp=temp->next;
+                curr=curr->next;
             }
-            
-        }
-        return head;
+        } 
+
+        return dummy->next;
+        //return the head node;
     }
 };
+
+
+
+
+
+
+// Good but have messy code
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         if(head==NULL)
+//           return head;
+//         while(head!=NULL&&head->val==val)  //removing the nodes in starting 
+//         {
+//             ListNode*del=head;
+//             head=head->next;
+//             del->next=NULL;
+//             delete del;
+//         }
+//         if(head==NULL)
+//           return head;
+//         ListNode*temp=head;
+//         ListNode*prev=NULL;
+
+//         while(temp!=NULL)  //deleting  nodes 
+//         {
+//             if(temp->val==val )
+//             {
+//                 ListNode*del=temp;
+                
+//                 prev->next=temp->next;
+                
+//                 temp=temp->next;
+//                 del->next=NULL;
+//                 delete del;
+//             }
+//             else
+//             {
+//                 prev=temp;
+//                 temp=temp->next;
+//             }
+            
+//         }
+//         return head;
+//     }
+// };
