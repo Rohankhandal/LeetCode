b8b665ec10a939e81c09176e1402c2808a336c85
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-22 19:22:24 +0530

    Time: 4 ms (31.73%) | Memory: 9.8 MB (54.77%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -6,47 +6,43 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+//  Solution 2: Using a single stack:
+// Time Complexity: O(2N).
 
- //Iterative method using 2 stacks
-// Solution 1: Using two stacks
-
-// Approach: 
-
-// The algorithm approach can be stated as:
-
-// We take two explicit stacks S1 and S2.
-// We insert our node to S1(if it’s not pointing to NULL).
-// We will set up a while loop to run till S1 is non-empty.
-// In every iteration, we pop out the top of S1 and then push this popped node to S2. Moreover we will push the left child and right child of this popped node to S1.(If they are not pointing to NULL).
-// Then we start the next iteration with the next node as top of S1.
-// We stop the iteration when S1 becomes empty.
-// At last we start popping at the top of S2 and print the node values, we will get the postorder traversal.
+// Space Complexity: O(N)
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(!root) return ans;
-        stack<TreeNode*>s1;
-        stack<TreeNode*>s2;
-        s1.push(root);
-        while(!s1.empty())
-        {
-            TreeNode*top=s1.top();
-            s1.pop();
-            s2.push(top);
-            if(top->left) s1.push(top->left);
-            if(top->right) s1.push(top->right); 
-        }
+        TreeNode*cur=root;
+        vector<int> postOrder;
+        if (cur == NULL)
+            return postOrder;
 
-        //now pop all nodes from s2 and insert data in ans 
-        while(!s2.empty())
-        {
-            ans.push_back((s2.top())->val);
-            s2.pop();
+        stack<TreeNode*> st;
+        while (cur != NULL || !st.empty()) {
+
+            if (cur != NULL) {
+                st.push(cur);
+                cur = cur->left;
+            } else {
+                TreeNode* temp = st.top()->right;
+                if (temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    postOrder.push_back(temp->val);
+                    while (!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        postOrder.push_back(temp->val);
+                    }
+                } else
+                    cur = temp;
+            }
         }
-        return ans;
+        return postOrder;
     }
 };
