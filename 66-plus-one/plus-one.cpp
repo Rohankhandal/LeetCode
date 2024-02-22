8cf95c341e951ce59da500de91f30b9b716e2747
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-23 00:19:43 +0530

    Time: 5 ms (21.33%) | Memory: 10.1 MB (49.14%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n=digits.size();
-        int carry=1;
-        int i=n-1;
-        while(carry&&i>=0)
-        {
-            int sum=digits[i]+carry;
-            digits[i]=sum%10;
-            carry=sum/10;
+        int n = digits.size();
 
-            i--;
-        }
-        if(carry)
-        {
-            vector<int>ans(n+1);
-            ans[0]=carry;
-            for(int i=1;i<n+1;i++)
-            {
-                ans[i]=digits[i-1];
+        for(int i =n-1; i>=0; i--){
+            digits[i]++;
+            if(digits[i]<10){
+                return digits;  //if function don't return value here,means array size must be increase bcz new one digit is come
+                //e.g arr=[9];  or arr=[9,9,9,9]
+                // 9+1=10 so, arr=[1,0];
             }
-            return ans;
+
+            digits[i]=0;  
+            
         }
+
+        // digits.insert(digits.begin(),1);  //insert  a new digit in begin
+                    //  or
+        digits.push_back(0);
+        digits[0]=1;
         return digits;
     }
 };
