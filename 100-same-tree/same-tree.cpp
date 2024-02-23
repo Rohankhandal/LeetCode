599a5739bcea3bd2aaecf29a6e3762de466c4f8b
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-23 22:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (44.33%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -9,48 +9,14 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- class Solution {
+class Solution {
 public:
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-       if(!p && !q)   //when both are  equal to NULL
-       {
-           return true;
-       }
-       if(p && q)  //when both are not equal to NULL
-       {
-           return (p->val == q->val) && isSameTree(p->left,q->left)
-           && isSameTree(p->right,q->right);
-       }
-       return false;  //when p or q are NULL
+        if(!p && !q) return true;
+        if(p && q && p->val==q->val){
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        }
+        else
+        return false;
     }
 };
-// class Solution {
-// public:
-// bool solve(TreeNode* p, TreeNode* q)
-// {
-//     if(p==NULL && q == NULL){
-//         return true;
-//     }
-//     if(p==NULL)
-//     {
-//         return false;
-//     }
-//     if(q==NULL)
-//     {
-//         return false;
-//     }
-//     if(p->val!=q->val)
-//     {
-//         return false;
-//     }
-//     bool res1=solve(p->left,q->left);
-//     bool res2=solve(p->right,q->right);
-//     return res1 && res2;
-// }
-//     bool isSameTree(TreeNode* p, TreeNode* q) {
-//         bool result=solve(p,q);
-//         return result;
-//     }
-// };
