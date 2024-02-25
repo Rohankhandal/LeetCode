d763e5412a6bde7788b88bf8129f6dc38f03c22f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-26 03:50:11 +0530

    Time: 5 ms (9.48%) | Memory: 9.8 MB (28.16%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,22 +9,74 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Morris Traversal
+//  Iterative Approach
+// T.c:-O(N);
+// S.c:_O(1);
 class Solution {
 public:
-void helper(TreeNode* root,vector<int> &ans)
-{
-    if(root==NULL)
-    {
-        return ;
-    }
-    helper(root->left,ans);
-ans.push_back(root->val);
-    helper(root->right,ans);
-}
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        helper(root,ans);
+        vector<int>inorder;
+        TreeNode*curr=root;
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode*prev=curr->left;
+                while(prev->right && prev->right!=curr)
+                {
+                    prev=prev->right;
+                }
 
-        return ans;
+                if(prev->right==NULL)
+                {
+                    prev->right=curr;  //connect the right pointer
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;  //when we again traverse same node , at that time 
+                    //remove the right pointer , go to right side instead to left side
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
+
+
+
+
+// //  Iterative Approach
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         stack<TreeNode*>st;
+//         vector<int>inOrder;
+//         TreeNode*node=root;
+//         if(!root) return inOrder;
+//         while(true)
+//         {
+//             if(node!=NULL)
+//             {
+//                 st.push(node);
+//                 node=node->left;
+//             }
+//             else
+//             {
+//                 if(st.empty()==true) break;
+
+//                 node=st.top();
+//                 st.pop();
+//                 inOrder.push_back(node->val);
+//                 node=node->right;  //go to right
+//             }
+//         }
+//         return inOrder;
+//     }
+// };
