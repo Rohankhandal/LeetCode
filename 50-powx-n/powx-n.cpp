341c351d979b9924d2afbaaa7753509e73ba993d
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-26 14:51:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.08%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    double power(double x, long long n) { // Using long long for larger values of n
-        double ans = 1;
-        while (n) {
-            if (n & 1) {
-                ans *= x;
-            }
-            x *= x; // Storing x * x for optimization
-            n >>= 1;
-        }
-        return ans;
-    }
-
     double myPow(double x, int n) {
-        if (n == 0) return 1; // Handle n == 0 case
-        if (n < 0) {
+        
+        if(n < 0) {
             x = 1 / x;
-            // n = -n;
+        } 
+        
+        long num = labs(n);
+        
+        double pow = 1;
+        
+        while(num){ // equivalent to while(num != 0)
+            if(num & 1) { // equivalent to if((num & 1) != 0)
+                pow *= x;
+            }
+            
+            x *= x;
+            num >>= 1;
         }
-        return power(x, abs(n));
+        
+        return pow;
     }
 };
