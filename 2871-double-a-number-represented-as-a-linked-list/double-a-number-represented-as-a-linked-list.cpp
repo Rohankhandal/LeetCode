1cc20c1f4bb91f2e6b3660fde8bbee5d5e89a1fa
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-02-28 18:24:49 +0530

    Time: 201 ms (34.58%) | Memory: 136.4 MB (10.26%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* reverse(ListNode* head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    ListNode* prev=NULL;
+    ListNode* curr=head;
+    ListNode* forward=curr->next;
+    while(curr)
+    {
+        forward=curr->next;
+        curr->next=prev;
+
+        prev=curr;
+        curr=forward;
+    }
+    return prev;
+    
+}
+    ListNode* doubleIt(ListNode* head) {
+        if(head==NULL)
+        {
+            return head;
+        }
+        
+        ListNode*temp=reverse(head);
+        int carry=0;
+        ListNode*dummy=new ListNode(-1);
+        ListNode*tail=dummy;
+        while(temp)
+        {
+            int sum=temp->val*2+carry;
+            int digit=sum%10;
+            carry=sum/10;
+
+            //insert new node in head of ans;
+            ListNode*newNode=new ListNode(digit);
+            newNode->next=tail->next;
+            tail->next=newNode;
+
+            temp=temp->next;
+        }
+        if(carry)
+        {
+            ListNode*newNode=new ListNode(carry);
+            newNode->next=tail->next;
+            tail->next=newNode;
+        }
+
+        return dummy->next;
+    }
+};
