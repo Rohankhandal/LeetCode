6fb017e34a8cfa6b9b88ccbf6bd38cc9cbe51b69
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-11 12:15:42 +0530

    Time: 3 ms (50.27%) | Memory: 8.3 MB (30.10%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,38 +1,109 @@
+// https://github.com/MAZHARMIK/Interview_DS_Algo/blob/master/HashMap/Custom%20Sort%20String.cpp
+
+// INTUITION : 
+// 	The question is actually testing if we know about comparators in sorting.
+// 	It just wants us to sort the string "str" but the ordering of characters must be in order
+// 	of their indices they occur in string "order"
+	
+// 	It's similar to what I have shown below example :
+// 	Example : 
+// 	order = "cba"
+// 	str      = "abcd"
+	
+// 	Assign numbers to characters of "order"
+// 	c -> 0
+// 	b -> 1
+// 	a  -> 2
+	
+// 	Now, put those numbers in characters in "str"
+// 	We get,
+// 	a   b   c    d
+// 	2   1   0   INF   (If a char is not present in "order" assign it the largest value)
+	
+// 	Now , we are sorting {2, 1, 0, INF} in ascending order
+// 	{0, 1, 2, INF}
+// 	correspnding characters are :
+// 	{c, b, a, d}
+	
+// 	This is just the intuition about the approach.
+// 	The actualy solution is just imply applying the comparator and it will be done.
+
+
+//Approach-1 (Just making use of frequency of each char) : 
+//T.C : O(str.length())
+//S.C : O(26) ~ O(1)
+// class Solution {
+// public:
+//     string customSortString(string order, string str) {
+//         int count[26] = {0};
+        
+//         for(char &x : str)
+//             count[x-'a']++;
+        
+//         string result = "";
+//         for(char &ch : order) {
+//             while(count[ch-'a']--) {
+//                 result.push_back(ch);
+//             }
+//         }
+        
+//         //Add the characters which are not present in 'order'
+//         for(char &ch : str) {
+//             if(count[ch-'a'] > 0) {
+//                 result.push_back(ch);
+//             }
+//         }
+        
+//         return result;
+//     }
+// };
+
+//Approach-2 (Using comparator)
+//T.C : O(nlogn)
+//S.C : O(26) ~ O(1)
 class Solution {
 public:
-    string customSortString(string order, string s) {
-        string ans="";
-        unordered_map<char,int>mp1;
-        for(auto &it:order)
-        {
-            mp1[it]++;
-        }
-        //store s in mp2 map
-        unordered_map<char,int>mp2;
-        for(auto &it:s)
-        {
-            mp2[it]++;
-        }
-        //find order string characters correspoinding to s string
-        // and into ans string;
-        for(auto &it:order)
-        {
-            if(mp2.find(it)!=mp2.end())
-            {
-                while(mp2[it]--)
-                  ans+=it;
-                //remove occurrence of it element in mp2
-                    mp2.erase(it);
-              
-            }
-        }
-        //now add all remaming elements of mp2 map in ans string
-        for(auto &it:mp2)
-        {
-            while(it.second--)
-              ans+=(it.first);
-        }
-        return ans;
-
+    string customSortString(string order, string str) {
+        vector<int> index(26, -1);
+        
+        for(int i = 0; i<order.length(); i++)
+            index[order[i]-'a'] = i;
+        
+        
+        auto lambda = [&index](char &ch1, char &ch2) {  //lambda comparator
+            return index[ch1-'a'] < index[ch2-'a'];
+        };
+        
+        sort(begin(str), end(str), lambda);
+        return str;
+        
     }
 };
+
+/*What was actually asked in Facebook :
+FOLLOW UP NOTE : The original questions that was asked by Facebook was similar to
+this but there was one small variation that "The characters which don't
+occur in "str" must be sorted as per English Alphabet order".  In that case
+one slight change will do the job in comparator function below :
+*/
+// class Solution {
+// public:
+//     string customSortString(string order, string str) {
+//         vector<int> index(26, INT_MAX);
+        
+//         for(int i = 0; i<order.length(); i++)
+//             index[order[i]-'a'] = i;
+        
+        
+//         auto lambda = [&](char &ch1, char &ch2) {
+//             if(index[ch1-'a'] == index[ch2-'a'])
+//                 return ch1 < ch2;
+            
+//             return index[ch1-'a'] < index[ch2-'a'];
+//         };
+        
+//         sort(begin(str), end(str), lambda);
+//         return str;
+        
+//     }
+// };
