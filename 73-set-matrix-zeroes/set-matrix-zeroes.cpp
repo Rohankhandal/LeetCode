d50bbf32b905c79626a6ee399a4a2583c50ebdbb
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-12 10:25:39 +0530

    Time: 12 ms (39.45%) | Memory: 15.9 MB (81.49%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -12,7 +12,8 @@ public:
         bool firstRow = false;
 
         // set markers in first row and first column
-        
+        // step 1: Traverse the matrix and
+    // mark 1st row & col accordingly:
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
@@ -27,6 +28,7 @@ public:
             }
         }
         // replace inner matrix
+        
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
