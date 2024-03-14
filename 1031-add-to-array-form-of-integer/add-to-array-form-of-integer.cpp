13f4e882981d01042af1568dddd0ec7a6566ecf3
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-14 15:56:54 +0530

    Time: 21 ms (40.46%) | Memory: 31.2 MB (28.20%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -1,64 +1,57 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        int n=num.size();
-        int carry=0;
-        int sum=0;
-       reverse(num.begin(),num.end());
         vector<int>ans;
-        int i=0;
-        while(k)
-        {
-            if(i!=n)
-            {
-                sum=num[i++]+(k%10)+carry;
-                ans.emplace_back(sum%10);
-                carry=sum/10;
-                k/=10;
+        int carry=0;
+        for(int i=num.size()-1; i>=0 || k>0 ; i--){      //if nums=[0] and k=23 => output:-[2,3]
+            int sum=0;
+            if(i>=0){
+               sum = num[i];
             }
-            else{
-                sum=(k%10)+carry;
-                ans.emplace_back(sum%10);
-                carry=sum/10;
-                k/=10;
-            }
-        }
-        while(carry)
-        {
-            if(i!=n)
-            sum=num[i++]+carry;
-            else
-            sum=carry;
-            ans.emplace_back(sum%10);
+            sum =  sum + carry + k%10;
+            ans.push_back(sum%10);
             carry=sum/10;
+            k/=10;
         }
-        while(i!=n)
-        {
-            ans.emplace_back(num[i++]);
+        if(carry){
+            ans.push_back(carry);
         }
         reverse(ans.begin(),ans.end());
         return ans;
-
-
-
-        // for(int i=n-1;i>=0;i--)
-        // {
-        //     sum=num[i]+(k%10)+carry;
-        //     k=k/10;
-        //     carry=sum/10;
-        //     num[i]=sum%10;
-        // }
-        // while()
-        // vector<int>ans(n+1);
-        // if(carry){
-        //     ans[0]=carry;
-        //     int i=1;
-        //    for(auto &it:num)
-        //    {
-        //     ans[i++]=it;
-        //    }
-        //    return ans;
-        // }
-        // return num;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> addToArrayForm(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         int i=n-1;
+//         int carry=0;
+//         vector<int> ans;
+//         while(i>=0)
+//         {
+//             int dig=k%10;
+//             k/=10;
+//             int sum=nums[i]+dig+carry;
+//             ans.push_back(sum%10);
+//             carry=sum/10;
+            
+//             i--;
+//         }
+//         while(k)   //if nums=[0] and k=23 => output:-[2,3]
+//         {
+//             int dig=k%10;
+//             k/=10;
+//             int sum=dig+carry;
+//             ans.push_back(sum%10);
+//             carry=sum/10;
+//         }
+//         if(carry>0)
+//         {
+//             ans.push_back(carry);
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
