0cdf74f6b2295e3052d152e005828fa76c013603
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-16 00:51:40 +0530

    Time: 7 ms (98.68%) | Memory: 26.2 MB (86.12%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,40 +1,89 @@
+// //Approach-1 (Using Division)
+// //T.C : O(n)
+// //S.C : O(1)
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int product = 0;
+//         int product_without_zero = 1;
+//         for(int i: nums) {
+//             if(i == 0)
+//                 product++;
+//             else
+//                 product_without_zero *= i;
+//         }
+        
+//         vector<int> result(nums.size());
+//         for(int i = 0; i<nums.size(); i++) {
+//             if(nums[i] != 0) {
+//                 if(product > 0)
+//                     result[i] = 0;
+//                 else
+//                     result[i] = product_without_zero/nums[i];
+//             }
+//             else {
+//                 if(product > 1)
+//                     result[i] = 0;
+//                 else
+//                     result[i] = product_without_zero;
+//             } 
+//         }
+//         return result;
+//     }
+// };
+
+
+// //Approach-2 (Using extra space)
+// //T.C : O(n)
+// //S.C : O(n)
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> left(n);
+//         vector<int> right(n);
+//         left[0] = 1;
+//         right[n-1] = 1;
+        
+//         for(int i = 1; i<nums.size(); i++) {
+//             left[i] = left[i-1]*nums[i-1];
+//         }
+        
+//         for(int i = n-2; i>=0; i--) {
+//             right[i] = right[i+1]*nums[i+1];
+//         }
+        
+//         vector<int> result(n);
+//         for(int i = 0; i<n; i++) {
+//             result[i] = left[i]*right[i];
+//         }
+        
+//         return result;
+//     }
+// };
+
+//Approach-3 (Using constant space)
+//T.C : O(n)
+//S.C : O(1)
+
+//use resultant space as left product and then get ans using left product * right product(calculate while getting ans)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans(n,0);
-        int prod=1;
-        vector<int>prefix(n,0);
-        vector<int>suffix(n,0);
-        prefix[0]=1;
-        prefix[1]=nums[0];
-        suffix[n-1]=1;
-        suffix[n-2]=nums[n-1];
-        for(int i=2;i<n;i++)
-        {
-            prefix[i]=prefix[i-1]*nums[i-1];
-        }
-        for(int i=n-3;i>=0;i--)
-        {
-            suffix[i]=suffix[i+1]*nums[i+1];
-        }
-        for(auto &it:prefix )
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
-        for(auto &it:suffix )
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
+        int n = nums.size();
+        vector<int> result(n);
+        result[0] = 1;
         
-        for(int i=0;i<n;i++)
-        {
-              ans[i]=prefix[i]*suffix[i];
+        for(int i = 1; i<nums.size(); i++) {
+            result[i] = result[i-1]*nums[i-1];
         }
-        return ans;
-
-
+        
+        int right = 1;
+        for(int i = n-1; i>=0; i--) {
+            result[i] = result[i]*right;
+            right *= nums[i];
+        }
+        
+        return result;
     }
 };
