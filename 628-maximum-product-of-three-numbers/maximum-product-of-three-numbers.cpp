e071ff16376f577dae866e5d6805ec4180aa1257
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-16 15:39:02 +0530

    Time: 25 ms (90.30%) | Memory: 29.9 MB (92.63%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,16 +1,55 @@
 class Solution {
 public:
-    static bool cmp(int a,int b)
-    {
-        return a>b;
-    }
     int maximumProduct(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       int n=nums.size();
+      int n=nums.size();
+      int firstMin=INT_MAX,secondMin=INT_MAX;
+      int firstMax=INT_MIN,secondMax=INT_MIN,thirdMax=INT_MIN;
+      for(int i=0;i<n;i++)
+      {
+        if(nums[i]>firstMax)
+        {
+            thirdMax=secondMax;
+            secondMax=firstMax;
+            firstMax=nums[i];
+        }
+        else if(nums[i]>secondMax)
+        {
+            thirdMax=secondMax;
+            secondMax=nums[i];
+        }
+        else if(nums[i]>thirdMax)
+        {
+            thirdMax=nums[i];
+        }
+
+
+        if(nums[i]<firstMin)
+        {
+            secondMin=firstMin;
+            firstMin=nums[i];
+        }
+        else if(nums[i]<secondMin)
+        {
+            secondMin=nums[i];
+        }
        
-       //starting two negative and last one ele
-       int max1=nums[0]*nums[1]*nums[n-1];
-       int max2=nums[n-1]*nums[n-2]*nums[n-3];
-       return max(max1,max2);
+      }
+    //   cout<<firstMin<<" "<<secondMin<<endl;
+      int max1=firstMin*secondMin*firstMax;
+      int max2=firstMax*secondMax*thirdMax;
+    return max(max1,max2);
     }
+      
 };
+// class Solution {
+// public:
+//     int maximumProduct(vector<int>& nums) {
+//        sort(nums.begin(),nums.end());
+//        int n=nums.size();
+       
+//        //starting two negative and last one ele
+//        int max1=nums[0]*nums[1]*nums[n-1];
+//        int max2=nums[n-1]*nums[n-2]*nums[n-3];
+//        return max(max1,max2);
+//     }
+// };
