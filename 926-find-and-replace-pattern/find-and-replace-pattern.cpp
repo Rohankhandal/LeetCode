a0c246087e061f3391b31bc352e93e79538090fd
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-18 10:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (37.17%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.cpp
+++ 926-find-and-replace-pattern/find-and-replace-pattern.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
-bool matches(string word,string pattern)
-{
-    vector<int>wordToPattern(26,0);  //we need to map for check in both direction mapping 
-    // e.g word="abb" pattern="ccc"  //we can't use two map , then result is mapping is correct 
-    //which is wrong
-    vector<int>patternToWord(26,0);
-
-    for(int index=0;index<word.size();index++)
-    {
-        char wordChar=word[index];
-        char patternChar=pattern[index];
-
-        if(patternToWord[patternChar-'a']==0)
-        {
-            patternToWord[patternChar-'a']=wordChar;
-        }
-        if(wordToPattern[wordChar-'a']==0)
-        {
-            wordToPattern[wordChar-'a']=patternChar;
-        }
-
-        //check for mapping e.g word="abb" pattern="ccc"
-        if(patternToWord[patternChar-'a']!=wordChar || wordToPattern[wordChar-'a']!=patternChar)
-        {
-            return false;
-        }
-    }
-    return true;
-
-
-}
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string>ans;
-        for(auto &str:words)
+        for(auto &word:words)
         {
-            if(matches(str,pattern))
+            int i=0;
+             unordered_map<char,char>forward;
+            unordered_map<char,char>reverse;
+            for(i=0;i<word.size();i++)
             {
-                ans.push_back(str);
-            }
+                if(forward.find(pattern[i])!=forward.end() && forward[pattern[i]]!=word[i])
+                {
+                    break;
+                }
+                if(reverse.find(word[i])!=reverse.end() && reverse[word[i]]!=pattern[i])
+                {
+                    break;
+                }
+                forward[pattern[i]]=word[i];
+                reverse[word[i]]=pattern[i];
+
+            } 
+            if(i==word.size()) ans.push_back(word);
         }
         return ans;
+
+               
     }
 };
