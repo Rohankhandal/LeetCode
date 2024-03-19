2f8024c8588302c3b067acb2116c899bbc918531
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-20 03:21:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (51.62%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -48,6 +48,30 @@ int solveUsingBottomUp(vector<int>&nums)
         return dp[n-1];
         
 }
+
+//Space optimization
+int spaceOpt(vector<int>&nums)
+    {
+        int n=nums.size();
+        
+        int prev2=0;
+        int prev1=nums[0];
+        int curr=0;
+
+        for(int i=1;i<=n;i++)
+        {
+            int temp=0;
+            if(i-2>=0)
+                temp=prev2;
+            int include=temp+nums[i];
+            int exclude=0+prev1;
+            curr=max(include,exclude);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+        
+}
     
     int rob(vector<int>& nums) {
         // vector<int>dp(nums.size()+1,-1);  //at index i how much maximum amount we can robb
