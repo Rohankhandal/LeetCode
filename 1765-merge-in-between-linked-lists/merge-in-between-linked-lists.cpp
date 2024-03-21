af50541c899878ada33c00a3095a419c751aedd2
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-21 08:56:43 +0530

    Time: 176 ms (68.38%) | Memory: 97.9 MB (97.74%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -1,41 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+//T.C : O(m+n)
+//S.C : O(1)
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        if(list1==NULL) return list2;
-        ListNode* dummyNode=new ListNode(-1);
-        dummyNode->next=list1;
-        ListNode*temp=dummyNode;
-
-        while(a>0)
-        {
-            temp=temp->next;
-            a--;
-        }
-        ListNode*forward=list1;
-        while(b>0)
-        {
-            forward=forward->next;
-            b--;
-        }
-        // cout<<temp->val<<" "<<forward->val<<endl;
-        ListNode*tail2=list2;
-        while(tail2->next!=NULL)
-        {
-            tail2=tail2->next;
+        ListNode* left  = NULL;
+        ListNode* right = list1;
+        
+        for(int i = 0; i <= b; i++) {
+            if(i == a-1) {
+                left = right;
+            }
+            right = right->next;
         }
         
-        temp->next=list2;
-        tail2->next=forward->next;
-        return list1;
+        left->next = list2;
+        
+        ListNode* temp = list2;
+        while(temp && temp->next) {
+            temp = temp->next;
+        }
+        
+        temp->next = right;
+        
+        return list1;  
     }
 };
