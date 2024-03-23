70e0da51e9969ddd1d29455acb319316b37b6b50
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-23 15:01:07 +0530

    Time: 12 ms (66.59%) | Memory: 11.3 MB (38.09%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,13 +1,34 @@
+//Approach-1 (Straight forward count)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     long long countSubstrings(string s, char c) {
-        int n=s.size();
-        long long  freq=0;
-        for(auto &it:s) 
-        {
-            if(it==c) freq++;
-        }
-        long long ans=freq*(freq+1)/2;
-        return ans ;
+        long long count = 0;   //it count the no. of occurrence of char c
+        long long substrings = 0;
+        
+
+        for(char &ch : s) {
+            if(ch == c) {
+                substrings += (1 + count);   //1(bcz indivdual char is also substring of itself)+count(in past how 
+                //how many occurrence we see to make a substring)
+                count++;
+            }
+        } 
+        return substrings;
     }
 };
+
+// class Solution {
+// public:
+//     long long countSubstrings(string s, char c) {
+//         int n=s.size();
+//         long long  freq=0;
+//         for(auto &it:s) 
+//         {
+//             if(it==c) freq++;
+//         }
+//         long long ans=freq*(freq+1)/2;
+//         return ans ;
+//     }
+// };
