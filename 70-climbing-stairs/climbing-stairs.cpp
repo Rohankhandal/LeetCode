8418b5d33de7ae271e3ed98c5949d8c007310895
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-24 10:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (11.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,10 +4,6 @@ public:
     {
         dp[0]=1;
         dp[1]=1;
-        if(n==0 || n==1)
-        {
-            return dp[n];
-        }
         if(dp[n]!=-1)
         return dp[n];
 
@@ -15,9 +11,22 @@ public:
 
         return dp[n];
     }
+    int solveUsingTab(int n)
+    {
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return  dp[n];
+    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(n,dp);
+
+        return solveUsingTab(n);
        
     }
 };
