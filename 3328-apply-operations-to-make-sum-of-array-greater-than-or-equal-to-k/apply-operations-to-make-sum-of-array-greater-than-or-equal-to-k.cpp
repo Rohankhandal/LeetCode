c0f85d66a2409f6b6bbb4b9be5bf92d97da5783e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-25 02:26:43 +0530

    Time: 6 ms (50.00%) | Memory: 7.3 MB (55.56%) - LeetSync

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -1,21 +1,57 @@
-class Solution {
+class Solution {      //T.C:-O(K)
 public:
     int minOperations(int k) {
-        int mini=INT_MAX;
+        //first we have to increment as possible ,then doubled
         if(k==1) return 0;
-        // for(int i=1;i<=k;i++)
-        // {
-        //      int minOp=0;
-        //      minOp+=(k/i)+(k%i!=0);
-        //      mini=min(minOp,mini);
-        // }
-        for(int i=2;i<=k;i++)
+
+        int minOp=INT_MAX;
+        for(int i=0;i<k;i++)
         {
-             int minOp=0;
-             minOp+=(i-1);
-             minOp=minOp+(k/i)-(k%i==0);
-             mini=min(minOp,mini);
+            //i means adding is 'i' times
+            int curValue=1+i;
+
+            int div=(k+curValue-1)/curValue -1;   //ceil value of(k/currValue)-1
+             
+             minOp=min(minOp,i+div);
         }
-        return mini;
+        return minOp;
     }
 };
+
+
+//MAth solution:-
+// https://www.youtube.com/watch?v=_5fM6Q_qZ4U&ab_channel=AryanMittal
+// T.C;-O(1)
+// class Solution {      //T.C:-O(K)
+// public:
+//     int minOperations(int k) {
+        
+//     }
+// };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int minOperations(int k) {
+//         int mini=INT_MAX;
+//         if(k==1) return 0;
+//         // for(int i=1;i<=k;i++)
+//         // {
+//         //      int minOp=0;
+//         //      minOp+=(k/i)+(k%i!=0);
+//         //      mini=min(minOp,mini);
+//         // }
+//         for(int i=2;i<=k;i++)
+//         {
+//              int minOp=0;
+//              minOp+=(i-1);
+//              minOp=minOp+(k/i)-(k%i==0);
+//              mini=min(minOp,mini);
+//         }
+//         return mini;
+//     }
+// };
