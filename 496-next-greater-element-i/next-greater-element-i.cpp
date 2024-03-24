e6935ed81c1289ad0de3edf6b0e1a7e113e92fd5
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-24 21:10:40 +0530

    Time: 7 ms (42.05%) | Memory: 12.2 MB (27.58%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,46 +1,48 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int>st;
         unordered_map<int,int>mp;
-        vector<int>ans(nums1.size());
-        for(int i=nums2.size()-1;i>=0;i--)
+        stack<int>st;
+        int n=nums2.size();
+        for(int i=n-1;i>=0;i--)
         {
             if(st.empty())
             {
                mp[nums2[i]]=-1;
-                st.push(nums2[i]);
+               st.push(nums2[i]);
             }
             else if(st.top()>nums2[i])
             {
                 mp[nums2[i]]=st.top();
                 st.push(nums2[i]);
             }
-            else //st.top()<=nums2[i]
+            else   //st.top()<nums2[i]
             {
-                while(!st.empty()&&st.top()<=nums2[i])
+                while(!st.empty() && st.top()<=nums2[i])
                 {
                     st.pop();
                 }
                 if(st.empty())
                 {
-                     mp[nums2[i]]=-1;
+                    mp[nums2[i]]=-1;
                     st.push(nums2[i]);
                 }
                 else
                 {
-                mp[nums2[i]]=st.top();
-                st.push(nums2[i]);
+                    mp[nums2[i]]=st.top();
+                    st.push(nums2[i]);
                 }
-                
             }
         }
-        
-        
-        //now traverse in nums2 and check whose next greater element need
-        for(int j=0;j<nums1.size();j++)
+        //for store ans 
+        for(auto &it:mp)
         {
-              ans[j]=mp[nums1[j]];    
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        vector<int>ans(nums1.size());
+        for(int i=0;i<nums1.size();i++)
+        {
+            ans[i]=mp[nums1[i]];
         }
         return ans;
     }
