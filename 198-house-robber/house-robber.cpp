2193497b2753605a4240bb3abecd784f518bfe70
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-26 08:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.72%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -27,10 +27,32 @@ public:
         dp[i]=max(include,exclude);
         return dp[i];
     }
+    int solveUsingTab(vector<int>&nums,int i)
+    {
+        vector<int>dp(nums.size()+1,0);
+        int n=nums.size();
+        dp[n]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            int include=nums[i];
+            if(i+2<=n)
+            {
+                include+=dp[i+2];
+            }    
+            int exclude=0+dp[i+1];
+
+            dp[i]=max(include,exclude);
+        }
+
+        return dp[0];
+    }
     int rob(vector<int>& nums) {
         // return solve(nums,0);
 
-        vector<int>dp(nums.size()+1,-1);
-        return solveUsingMem(nums,0,dp);
+        // vector<int>dp(nums.size()+1,-1);
+        // return solveUsingMem(nums,0,dp);
+        int n=nums.size();
+        return solveUsingTab(nums,n-1);
     }
 };
