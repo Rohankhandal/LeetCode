9e8b5e81fd61d287c8f92e51c1af63b7635c0cab
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-03-27 02:16:47 +0530

    Time: 141 ms (59.02%) | Memory: 98.2 MB (26.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -27,6 +27,33 @@ bool solveMem(vector<int>&nums,int target,int i,vector<vector<int>>&dp)
     return dp[i][target];
 
 }
+bool solveTab(vector<int>&nums,int target)
+{
+    vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,0));
+
+    for(int i=0;i<nums.size();i++)
+    {
+        dp[i][0]=1;
+    }
+
+    for(int i=nums.size()-1;i>=0;i--)
+    {
+        for(int t=1;t<=target;t++)
+        {
+            bool include=0;
+            if(t-nums[i]>=0)
+            {
+                include=dp[i+1][t-nums[i]];
+            }
+            bool exclude=dp[i+1][t];
+
+            dp[i][t]=include||exclude;
+        }
+    }
+
+    return dp[0][target];
+
+}
     bool canPartition(vector<int>& nums) {
         int sum=0;
         for(auto &it:nums)
@@ -36,9 +63,17 @@ bool solveMem(vector<int>&nums,int target,int i,vector<vector<int>>&dp)
         if(sum%2!=0) return false;
         
         // return solve(nums,sum/2,0);
+
+        // using MEM
         int n=nums.size();
         int target=sum/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return solveMem(nums,target,0,dp);
+        // vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        // return solveMem(nums,target,0,dp);
+
+
+        //using bottomm
+        return solveTab(nums,target);
     }
+
+
 };
