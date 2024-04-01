3a85f5820d49a7f48d00c3d6970b592d1f14d127
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-01 14:39:32 +0530

    Time: 90 ms (54.64%) | Memory: 82.6 MB (20.62%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,25 +1,33 @@
+//Approach-1 (Brute Force)
+//Find all subarrays and check if min is minK and max is maxK
+//Code will be provided soon for brute force
+
+//Approach-2 (Optimal O(n)) - Sliding Window
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long res = 0;
-        int bad_idx = -1, left_idx = -1, right_idx = -1;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (!(minK <= nums[i] && nums[i] <= maxK)) {
-                bad_idx = i;
-            }
-
-            if (nums[i] == minK) {
-                left_idx = i;
-            }
-
-            if (nums[i] == maxK) {
-                right_idx = i;
-            }
-
-            res += max(0, min(left_idx, right_idx) - bad_idx);
+        long long ans = 0;
+        
+        int minPosition = -1;
+        int maxPosition = -1;
+        int leftBound   = -1;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            
+            if(nums[i] < minK || nums[i] > maxK)
+                leftBound = i;
+            
+            if(nums[i] == minK)
+                minPosition = i;
+            if(nums[i] == maxK)
+                maxPosition = i;
+            
+            int count = min(maxPosition, minPosition) - leftBound;
+            
+            ans += (count <= 0) ? 0 : count;
+            
         }
-
-        return res;
+        
+        return ans;
     }
 };
