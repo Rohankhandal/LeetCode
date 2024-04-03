17d3e2858da6ab23571104b467a51c12db4197ca
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-03 14:10:33 +0530

    Time: 9 ms (47.16%) | Memory: 12.3 MB (24.43%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -45,10 +45,47 @@ public:
         return dp[i][j];
 
     }
+    int solveTab(string a,string b)
+    {   int len1=a.size();
+        int len2=b.size();
+        vector<vector<int>>dp(a.size()+1,vector<int>(b.size()+1,0));
+        for(int i=0;i<b.size();i++)
+        {
+            dp[a.size()][i]=b.size()-i;
+        }
+        for(int i=0;i<a.size();i++)
+        {
+            dp[i][b.size()]=a.size()-i;
+        }
+        int ans=0;
+        for(int i=len1-1;i>=0;i--)
+        {
+            for(int j=len2-1;j>=0;j--)
+            {
+                if(a[i]==b[j]){
+                 ans=0+dp[i+1][j+1];
+                }
+                else 
+                {
+                    int insert=1+dp[i][j+1];
+                    int deleted=1+dp[i+1][j];
+                    int replace=1+dp[i+1][j+1];
+
+                    ans=min(insert,min(deleted,replace));
+                }
+                dp[i][j]=ans;
+            }
+        }
+        
+        return dp[0][0];
+
+    }
     int minDistance(string a, string b) {
     //    return  solve(a,b,0,0);
 
-        vector<vector<int>>dp(a.size(),vector<int>(b.size(),-1));
-        return solveTop(a,b,0,0,dp);
+        // vector<vector<int>>dp(a.size(),vector<int>(b.size(),-1));
+        // return solveTop(a,b,0,0,dp);
+
+        return solveTab(a,b);
     }
 };
