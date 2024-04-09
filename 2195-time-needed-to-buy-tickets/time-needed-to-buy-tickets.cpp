c68db709eb13252a20b82b4516f5fdd09e9f0347
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-09 08:07:46 +0530

    Time: 4 ms (41.19%) | Memory: 9.4 MB (67.56%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,47 +1,25 @@
-
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-       int n=tickets.size();
-       int total=tickets[k];
+        int n=tickets.size();
+        int time=0;
+        for(int i=0;i<=k;i++){
+            if(tickets[i]<=tickets[k])
+            {
+                time+=tickets[i];
+            }
+            else
+            {
+                time+=tickets[k];
+            }
+        }
+        for(int i=k+1;i<n;i++)
+        {
+            if(tickets[i]<tickets[k]) time+=tickets[i];
+            else time+=tickets[k]-1;
+            
+        }
 
-        for(int i=0;i<k;i++){
-            total+=min(tickets[k],tickets[i]);
-        }
-        for(int i=k+1;i<n;i++){
-            total+=min(tickets[k]-1,tickets[i]);
-        }
-        return total;
+        return time;
     }
 };
-// class Solution {
-// public:
-//     int timeRequiredToBuy(vector<int>& tickets, int k) {
-//         int needTicket=tickets[k];
-//         int n=tickets.size();
-//         int time=0;
-//         for(int i=0;i<=k;i++)
-//         {
-//             if(tickets[i]>=needTicket)
-//             {
-//                 time+=needTicket;
-//             }
-//             else
-//             {
-//                 time+=tickets[i];
-//             }
-//         }
-//         for(int i=k+1;i<n;i++)
-//         {
-//             if(tickets[i]>=needTicket)
-//             {
-//                 time+=needTicket-1;
-//             }
-//             else
-//             {
-//                 time+=tickets[i];
-//             }
-//         }
-//         return time;
-//     }
-// };
