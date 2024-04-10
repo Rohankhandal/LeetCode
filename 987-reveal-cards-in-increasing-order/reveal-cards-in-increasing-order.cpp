0f19e62224e3e09f10d46626e1c73bad58f65c1d
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-10 23:31:44 +0530

    Time: 3 ms (67.33%) | Memory: 10.8 MB (67.85%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,28 +1,75 @@
+//Approach-1 (Simulation) 
+//T.C : O(nlogn)
+//S.C : O(1)
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+//         int n = deck.size();
+//         vector<int> result(n, 0);
+        
+//         bool skip = false;
+        
+//         int i = 0; //deck
+//         int j = 0; //result
+        
+//         sort(begin(deck), end(deck));
+        
+//         while(i < n) {
+            
+//             if(result[j] == 0) { //khali hai
+                
+//                 if(skip == false) {
+//                     result[j] = deck[i];
+//                     i++;
+//                 }
+                
+//                 skip = !skip; //alternate
+                
+//             }
+            
+//             j = (j+1)%n;
+            
+//         }
+        
+//         return result;
+//     }
+// };
+
+
+
+//Approach-2 (Simulation using Queue) 
+//T.C : O(nlogn)
+//S.C : O(n)
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n = deck.size();
         
-        int i=0; //deck
-        int j=0; //ans
-        int n=deck.size();
-        vector<int>ans(n,0);
-        sort(deck.begin(),deck.end());
-        bool skip=true;
-        while(i<n)
-        {
-            if(ans[j]==0)  //if answer is not inserted in vector ans
-            {
-                if(skip)
-                {
-                    ans[j]=deck[i];
-                    i++;
-                }
-                skip=!skip;
-            }
-
-
-            j=(j+1)%n;
+        queue<int> que;
+        vector<int> result(n);
+        
+        for(int i = 0; i < n; i++) {
+            que.push(i);
         }
-        return ans;
+        
+        sort(begin(deck), end(deck));
+        
+        for(int i = 0; i < n; i++) {
+            
+            int idx = que.front();
+            que.pop();
+            
+            result[idx] = deck[i];
+            
+            if(!que.empty()) {
+                que.push(que.front());
+                que.pop();
+            }
+            
+        }
+        
+        return result;
     }
 };
+
+
