da5c6333b9481878b77a79e1e9342a8f7ce42f16
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-13 10:49:06 +0530

    Time: 10 ms (17.33%) | Memory: 14.5 MB (53.17%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void solve(vector<string>&ans,string temp,string s,int i)
+    {
+        if(i>=s.size())
+        {
+            ans.push_back(temp);
+            return ;
+        }
+        if(isalpha(s[i]))
+        {
+            //not_take(means not change in capital letter)
+            temp.push_back(s[i]);
+            solve(ans,temp,s,i+1);
+            temp.pop_back();
+
+            //take
+            if(s[i]>='a' && s[i]<='z') temp.push_back(s[i]-32);
+            if(s[i]>='A' && s[i]<='Z') temp.push_back(s[i]+32);
+            solve(ans,temp,s,i+1);
+            temp.pop_back();
+        }
+        else
+        {
+            temp.push_back(s[i]);
+            solve(ans,temp,s,i+1);
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        string temp="";
+        int i=0;
+        vector<string>ans;
+        solve(ans,temp,s,i);
+        return ans;
+    }
+};
