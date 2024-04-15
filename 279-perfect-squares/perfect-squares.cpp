1c5bfe3f0617e5ff622e4c27b7e0da6bf023936c
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-15 16:53:20 +0530

    Time: 30 ms (90.67%) | Memory: 10.6 MB (63.40%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -24,10 +24,29 @@ public:
         }
         return dp[n]=ans;
     }
+    int solveTab(int n)
+    {
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        
+      
+        for(int i=1;i<=n;i++)
+        {
+              int ans=INT_MAX;
+            for(int j=1;j*j<=i;j++)
+            {
+                ans=min(ans,1+dp[i-j*j]);
+            }
+            dp[i]=ans;
+        }
+        return dp[n];
+    }
     int numSquares(int n) {
         // return solve(n)-1;
 
-        vector<int>dp(n+1,-1);
-        return solve(n,dp)-1;
+        // vector<int>dp(n+1,-1);
+        // return solve(n,dp)-1;
+
+        return solveTab(n)-1;
     }
 };
