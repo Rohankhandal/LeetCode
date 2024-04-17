fdd36d8c6d3fcee6d817fa469b0ff07ce9ea1abb
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-17 20:52:55 +0530

    Time: 11 ms (7.35%) | Memory: 22.8 MB (5.74%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void solve(vector<string>&ans,string temp,int open,int close,int n)
     {
-         if(close>open) return ;
+        
         if(temp.size()==2*n)
         {
             // cout<<temp<<" "<<open<<" "<<close<<endl;
@@ -10,15 +10,15 @@ public:
             ans.push_back(temp);
             return;
         }
-
+        if(close>open) return ;
         temp.push_back('(');
         solve(ans,temp,open+1,close,n);
         temp.pop_back();
 
-        // if(close<open)
-        // {
+        if(close<open)
+        {
             
-        // }
+        }
         temp.push_back(')');
         solve(ans,temp,open,close+1,n);
         temp.pop_back();
