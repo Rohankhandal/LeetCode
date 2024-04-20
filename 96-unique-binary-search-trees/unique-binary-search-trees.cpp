d4386cb5183e0b7bdcf397c6cd39fdd54f582f3f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-20 12:09:53 +0530

    Time: 2 ms (60.02%) | Memory: 7.2 MB (17.84%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -32,13 +32,37 @@ public:
         }
         return dp[n]=ans;
     }
+
+
+    //3.Tabluation
+    int solveTab(int n)
+    {
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;  
+
+        int ans=0;
+        //think i as a number of nodes
+        for(int i=2;i<=n;i++)
+        {
+            //think j as a root node
+            for(int j=1;j<=i;j++)
+            {
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+    }
     int numTrees(int n) {
         
         // return solve(n);
 
 
         //2.Memoization
-        vector<int>dp(n+1,-1);
-        return solveMem(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solveMem(n,dp);
+
+        //3.Tabluation;
+        return solveTab(n);
     }
 };
