997659119b71ca29a4a69fe67af3134f7008143d
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-20 12:02:19 +0530

    Time: 2 ms (60.02%) | Memory: 7.1 MB (83.17%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -14,8 +14,31 @@ public:
         }
         return ans;
     }
+
+    //2.Memoization
+    int solveMem(int n,vector<int>&dp)
+    {
+        if(n==0 || n==1)
+        {
+            return 1;
+        }   
+        if(dp[n]!=-1) return dp[n];
+
+        int ans=0;
+        //think i as a root node
+        for(int i=1;i<=n;i++)
+        {
+            ans+=solveMem(i-1,dp)*solveMem(n-i,dp);
+        }
+        return dp[n]=ans;
+    }
     int numTrees(int n) {
         
-        return solve(n);
+        // return solve(n);
+
+
+        //2.Memoization
+        vector<int>dp(n+1,-1);
+        return solveMem(n,dp);
     }
 };
