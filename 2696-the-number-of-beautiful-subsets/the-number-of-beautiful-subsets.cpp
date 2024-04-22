ef239437ddcb4f3eec92ee5f5c12b5f010489c8b
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-22 20:11:40 +0530

    Time: 778 ms (18.19%) | Memory: 40.4 MB (18.99%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -4,26 +4,26 @@ class Solution {
 public:
     int solve(vector<int>&nums,int k,int i,unordered_map<int,int>&mp)
     {
-        if(i==nums.size()) return 1;
+        if(i==nums.size()) return 0;
 
         //take
         int take=0;
         if(!mp[nums[i]-k] && !mp[nums[i]+k])
         {
             mp[nums[i]]++;
-            take=solve(nums,k,i+1,mp);
+            take=1+solve(nums,k,i+1,mp);
             //backtracking
             mp[nums[i]]--;
         }
 
         //not-take
-        int noTake=solve(nums,k,i+1,mp);
+        int noTake=0+solve(nums,k,i+1,mp);
 
         return take+noTake;
     }
     int beautifulSubsets(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
         int ans=solve(nums,k,0,mp);
-        return ans-1;   //bcz there is one empty subset is also included
+        return ans;   //bcz there is one empty subset is also included
     }
 };
