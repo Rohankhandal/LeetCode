0bdbc235267fa566e40f85382b95f265afd81df4
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-23 20:22:21 +0530

    Time: 91 ms (37.38%) | Memory: 95.7 MB (94.01%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,40 @@
+//Recursive Approach
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int minValue=INT_MAX,maxi=INT_MIN;
-        int n=prices.size();
-        for(int i=0;i<n;i++)
-        {
-            if(prices[i]<minValue) minValue=prices[i];
+    void findMaxProfit(vector<int>&prices,int i,int &minValue,int &maxiProfit)
+    {
+        if(i>=prices.size()) return ;
 
-            int todayProfit=prices[i]-minValue;
-            if(maxi<todayProfit) maxi=todayProfit;
-        }
-        return maxi;
+        if(prices[i]<minValue) minValue=prices[i];
+
+        int todayProfit=prices[i]-minValue;
+        if(maxiProfit<todayProfit) maxiProfit=todayProfit;
+
+        findMaxProfit(prices,i+1,minValue,maxiProfit);
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int minValue=INT_MAX,maxiProfit=INT_MIN;
+        int n=prices.size();
+        findMaxProfit(prices,0,minValue,maxiProfit);
+        return maxiProfit;
     }
 };
+
+//ITERATIVE APPROACH
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int minValue=INT_MAX,maxi=INT_MIN;
+//         int n=prices.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(prices[i]<minValue) minValue=prices[i];
+
+//             int todayProfit=prices[i]-minValue;
+//             if(maxi<todayProfit) maxi=todayProfit;
+//         }
+//         return maxi;
+//     }
+// };
