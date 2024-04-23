3dfc1d56a667ef6c43e6fd43bf158a1eff81ca31
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-23 18:10:44 +0530

    Time: 84 ms (36.64%) | Memory: 9.4 MB (35.01%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,68 +1,39 @@
 class Solution {
 public:
     int solve(int start,int end)
-    {   if(start>=end) return 0;
-
+    {
+        if(start>=end)
+        {
+            return 0; //we choose the right number and win
+        }
         int ans=INT_MAX;
-        
         for(int i=start;i<=end;i++)
         {
-            ans=min(ans,i+ max(solve(start,i-1),solve(i+1,end)));
+            ans=min(ans,i+max(solve(1,i-1),solve(i+1,end)));
         }
         return ans;
 
     }
-    int solveTop(int start,int end,vector<vector<int>>&dp)
-    {   
-        if(start>=end) return 0;
+    int solveMem(int start,int end,vector<vector<int>>&dp)
+    {
+        if(start>=end)
+        {
+            return 0; //we choose the right number and win
+        }
         if(dp[start][end]!=-1) return dp[start][end];
-
         int ans=INT_MAX;
-        
         for(int i=start;i<=end;i++)
         {
-            ans=min(ans,i+ max(solveTop(start,i-1,dp),solveTop(i+1,end,dp)));
+            ans=min(ans,i+max(solveMem(start,i-1,dp),solveMem(i+1,end,dp)));
         }
-        dp[start][end]=ans;
-        return dp[start][end];
-
-    }
-    int solveTab(int n)
-    {   
-
-        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
-
-        
-        for(int start=n;start>=1;start--)
-        {
-            for(int end=1;end<=n;end++)
-            {
-                if(start>=end){
-                    continue;
-                }
-                else
-                {
-                    int ans=INT_MAX;
-                    for(int i=start;i<=end;i++)
-                    {
-                        ans=min(ans,i+ max(dp[start][i-1],dp[i+1][end]));
-                    }
-                    dp[start][end]=ans;
-                    
-                }
-
-            }
-        }
-        return dp[1][n];
+        return dp[start][end]=ans;
 
     }
     int getMoneyAmount(int n) {
-        // return solve(0,n);
+    //    return  solve(1,n);
 
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        // return solveTop(0,n,dp);
-
-
-        return solveTab(n);
+    //2.Memoization
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solveMem(1,n,dp);
     }
 };
