94a27fd52e5e4b26e9af76a35133d5fbfdb1db36
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-25 23:31:14 +0530

    Time: 329 ms (37.39%) | Memory: 183.2 MB (35.71%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -13,6 +13,9 @@ public:
         int ans2=0+solve(i+1,s,last,k);
         return max(ans1,ans2);
     }
+
+//     Time complexity: O(n∗27)
+//      Space complexity:O(n∗27) to O(26)
     int solveMem(int i,string &s,int last,int k,vector<vector<int>>&dp)
     {
         if(i>=s.size()) return 0;
@@ -28,12 +31,40 @@ public:
         int ans2=0+solveMem(i+1,s,last,k,dp);
         return dp[i][last]=max(ans1,ans2);
     }
+
+
+    //3.Tabluation:-
+    int solveTab(string &s,int k)
+    {
+         vector<vector<int>>dp(s.size()+2,vector<int>(27,0));
+        int n=s.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int last=26;last>=0;last--)
+            {
+                int ans1=0;
+                char ch=last+'a';  //IMP 
+                if(last==26 || abs(s[i]-ch)<=k)
+                {
+                    ans1=1+dp[i+1][s[i]-'a'];  //include
+                }
+                //exclude
+                int ans2=0+dp[i+1][last];
+                dp[i][last]=max(ans1,ans2);
+            }
+        }
+        return dp[0][26];
+        
+    }
     int longestIdealString(string s, int k) {
          //char last='{';  //suppose any character
         //    return solve(0,s,last,k);
 
         //2.Memoziation
-        vector<vector<int>>dp(s.size()+2,vector<int>(27,-1));
-        return solveMem(0,s,26,k,dp);
+        // vector<vector<int>>dp(s.size()+2,vector<int>(27,-1));
+        // return solveMem(0,s,26,k,dp);
+
+        //3/Tabluation
+        return solveTab(s,k);
     }
 };
