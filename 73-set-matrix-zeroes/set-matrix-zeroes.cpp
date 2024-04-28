f7b1b8f9010039268134fec28d6f866179eb72f6
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-04-28 14:21:19 +0530

    Time: 6 ms (87.41%) | Memory: 16.4 MB (26.30%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,47 +1,116 @@
-
+//TC:-O(n*m)
+//S.C:-O(1)
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
-        vector<int>row(r,-1);
-        vector<int>col(c,-1);
-        for(int i=0;i<r;i++ )
+        int firstCol=1;
+        for(int i=0;i<r;i++)
         {
-            for(int j=0;j<c;j++)
+            if(matrix[i][0]==0){firstCol=0; break;}
+        } 
+        for(int j=0;j<c;j++)
+        {
+            if(matrix[0][j]==0){matrix[0][0]=0; break;} //first row
+        } 
+       
+
+        for(int i=1;i<r;i++ )
+        {
+            for(int j=1;j<c;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    row[i]=1;
+                    matrix[i][0]=0;  //for row
                     break;
                 }
             }
         }
-        for(int j=0;j<c;j++ )
+        for(int j=1;j<c;j++ )
         {
-            for(int i=0;i<r;i++)
+            for(int i=1;i<r;i++)
             {
                 if(matrix[i][j]==0)
                 {
-                    col[j]=1;
+                    matrix[0][j]=0;  //for col
                     break;
                 }
             }
         }
         //mark elements
-        for(int i=0;i<r;i++)
+        for(int i=1;i<r;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=1;j<c;j++)
             {
-                if(row[i]==1 || col[j]==1)
+                if( matrix[i][0]==0 || matrix[0][j]==0)
                 {
                     matrix[i][j]=0;
                 }
             }
         }
+
+        //first change first row
+        if(matrix[0][0]==0) 
+        {
+            for(int j=0;j<c;j++) matrix[0][j]=0;
+        }
+        //change first column
+        cout<<firstCol<<endl;
+        if(firstCol==0)
+        {
+            for(int i=0;i<r;i++) matrix[i][0]=0;
+        }
         return ;
     }
 };
+
+//T.C:-O(n*m);
+//S.C:_O(N);
+
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int r = matrix.size();
+//         int c = matrix[0].size();
+//         vector<int>row(r,-1);
+//         vector<int>col(c,-1);
+//         for(int i=0;i<r;i++ )
+//         {
+//             for(int j=0;j<c;j++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     row[i]=1;
+//                     break;
+//                 }
+//             }
+//         }
+//         for(int j=0;j<c;j++ )
+//         {
+//             for(int i=0;i<r;i++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     col[j]=1;
+//                     break;
+//                 }
+//             }
+//         }
+//         //mark elements
+//         for(int i=0;i<r;i++)
+//         {
+//             for(int j=0;j<c;j++)
+//             {
+//                 if(row[i]==1 || col[j]==1)
+//                 {
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+//         return ;
+//     }
+// };
 
 
 
