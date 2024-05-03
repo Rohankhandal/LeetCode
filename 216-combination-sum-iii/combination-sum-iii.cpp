1a8a60ce26912dcaa8f2354b24fb2429d2603f9e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-03 12:12:55 +0530

    Time: 2 ms (49.17%) | Memory: 13.1 MB (9.58%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,26 +1,67 @@
+//T.c:-O(2^n)
+//S.c:-O(1)
 class Solution {
-    void solve(int k, int n, vector<vector<int>> & answer ,vector<int>ans , int index){
-        // base case 
-        if(n==0 && ans.size()==k){
-            answer.push_back(ans);
+    void solve(int i,int k, int n, vector<vector<int>> & ans,vector<int>temp, int sum){
+        if(k==0)
+        {
+            if(sum==n)
+            {
+                ans.push_back(temp);
+            }
             return ;
         }
+        if(sum>n) return ;
+        if(i>=10) return ;
 
-        for(int i=index;i<=9;i++){
-            if(i<=n){
-                ans.push_back(i);
-                solve(k,n-i,answer,ans,i+1);
-                 ans.pop_back();
-            }
-               
-        }
+        //pick
+        temp.push_back(i);
+        solve(i+1,k-1,n,ans,temp,sum+i);
+        temp.pop_back();
+
+
+        //not pick
+        solve(i+1,k,n,ans,temp,sum);
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> answer;
-        vector<int>ans;
-
-        solve(k,n,answer,ans,1);
-        return answer;
+        vector<vector<int>> ans;
+        vector<int>temp;
+        int i=1;
+        int sum=0;
+        solve(i,k,n,ans,temp,sum);
+        return ans;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+//     void solve(int k, int n, vector<vector<int>> & answer ,vector<int>ans , int index){
+//         // base case 
+//         if(n==0 && ans.size()==k){
+//             answer.push_back(ans);
+//             return ;
+//         }
+
+//         for(int i=index;i<=9;i++){
+//             if(i<=n){
+//                 ans.push_back(i);
+//                 solve(k,n-i,answer,ans,i+1);
+//                  ans.pop_back();
+//             }
+               
+//         }
+//     }
+// public:
+//     vector<vector<int>> combinationSum3(int k, int n) {
+//         vector<vector<int>> answer;
+//         vector<int>ans;
+
+//         solve(k,n,answer,ans,1);
+//         return answer;
+//     }
+// };
