85136d0880fe3c72cf914b44d9305703b4cfc011
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-05 11:49:32 +0530

    Time: 3 ms (43.54%) | Memory: 7.9 MB (54.25%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -26,12 +26,36 @@ public:
         }
         return dp[tar]=ans;
     }
+
+    //3.Tabluation
+    int solveTab(vector<int>&nums,int t)
+    {
+        vector<int>dp(t+1,0);
+        int n=nums.size();
+        dp[0]=1;
+        
+        for(int tar=1;tar<=t;tar++)
+        {
+            long long ans=0;      //IMP (long long imp)
+            for(int i=n-1;i>=0;i--)
+            {
+                if(tar-nums[i]>=0)
+                ans+=dp[tar-nums[i]];
+            }
+            dp[tar]=ans;
+        }
+        return dp[t];
+    }
     int combinationSum4(vector<int>& nums, int target) {
         // int i=0;
         // return solve(nums,target);
 
         //2.Memoziation
-        vector<int>dp(target+1,-1);
-        return solveMem(nums,target,dp);
+        // vector<int>dp(target+1,-1);
+        // return solveMem(nums,target,dp);
+
+
+        //3.Tabluation
+        return solveTab(nums,target);
     }
 };
