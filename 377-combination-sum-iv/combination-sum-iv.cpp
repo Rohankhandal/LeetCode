767ab26493bf4f1da4c802f3a065eff8a64638e3
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-05 11:41:09 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (54.25%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -2,55 +2,36 @@ class Solution {
 public:
     int solve(vector<int>&nums,int tar)
     {
+        int n=nums.size();
         if(tar==0) return 1;
         if(tar<0) return 0;
         int ans=0;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            ans+=solve(nums,tar-nums[i]);   
+            ans+=solve(nums,tar-nums[i]);
         }
         return ans;
     }
-    int solveTop(vector<int>&nums,int tar,vector<int>&dp)
+     int solveMem(vector<int>&nums,int tar,vector<int>&dp)
     {
+        int n=nums.size();
         if(tar==0) return 1;
         if(tar<0) return 0;
-        if(dp[tar]!=-1) return dp[tar];
 
+        if(dp[tar]!=-1) return dp[tar];
         int ans=0;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            ans+=solveTop(nums,tar-nums[i],dp);   
+            ans+=solveMem(nums,tar-nums[i],dp);
         }
         return dp[tar]=ans;
     }
-    int solveBottom(vector<int>&nums,int tar)
-    {
-
-        vector<unsigned int>dp(tar+1,0); //Must Use unsigned int
-        dp[0]=1;
-
-       
-        for(int i=1;i<=tar;i++)
-        {
-            for(int j=0;j<nums.size();j++)
-            {
-                
-                if(i-nums[j]>=0)
-                {
-                    dp[i]+=dp[i-nums[j]];   //ERROR WHY
-                }
-                   
-            }    
-        }
-        return dp[tar];
-    }
     int combinationSum4(vector<int>& nums, int target) {
+        // int i=0;
         // return solve(nums,target);
 
-        // vector<int>dp(target+1,-1);
-        // return solveTop(nums,target,dp);
-
-        return solveBottom(nums,target);
+        //2.Memoziation
+        vector<int>dp(target+1,-1);
+        return solveMem(nums,target,dp);
     }
 };
