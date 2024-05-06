79fd9ac5f0f1bcb7959b22a7b80deaaebaf5cc6f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-06 08:11:14 +0530

    Time: 256 ms (83.95%) | Memory: 159.7 MB (90.66%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -8,108 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-ListNode* reverse(ListNode* head)
-{
-    ListNode*curr=head;
-    ListNode*prev=NULL;
-    ListNode*forward=curr->next;
-    while(curr!=NULL)
+    ListNode* reverse(ListNode* head)
     {
-        forward=curr->next;
-        curr->next=prev;
-
-        prev=curr;
-        curr=forward;
-
-
-    }
-    return prev;
-}
-    ListNode* removeNodes(ListNode* head) {
-        if(head==NULL)
-        {
-            return NULL;
-        }
-        head=reverse(head);
-        ListNode*curr=head;
-        
-
-        int maxi=curr->val;
-        ListNode*prev=NULL;
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* forward=head->next;
         while(curr!=NULL)
         {
-            if(curr->val<maxi)
-            {
-                //remving the node
-                ListNode* del=curr;
+            forward=curr->next;
+            curr->next=prev;
 
-                prev->next=curr->next;
-                curr=curr->next;
-
-                del->next=NULL;
-                delete del;
-                
-            }
-            else{
-                //update maximum value
-                maxi=curr->val;
-                prev=curr;
-                curr=curr->next;
-            }
+            prev=curr;
+            curr=forward;
         }
+        return prev;
+    }
+    ListNode* removeNodes(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
         head=reverse(head);
-        return head;
-
+        int max=head->val;
+        ListNode* temp=head;
+        while(temp!=NULL && temp->next!=NULL)
+        {
+            if(temp->next->val<max)
+            {
+                temp->next=temp->next->next;
+            }
+            else
+            {
+                temp=temp->next;
+                 max=temp->val;
+            } 
+        }
+        return head=reverse(head);
 
     }
+
 };
-
-
-
-//  T.c:-O(N)
-//  S.c:-O(N)
-// class Solution {
-// public:
-// void storeNodes(stack<ListNode*>&st,ListNode* head)
-// {
-//     ListNode* temp=head;
-//     while(temp!=NULL)
-//     {
-//         st.push(temp);
-//         temp=temp->next;
-
-//     }
-
-// }
-//     ListNode* removeNodes(ListNode* head) {
-//         stack<ListNode*>st;
-//         storeNodes(st,head);
-//         ListNode*prev=st.top();
-//         st.pop();
-//         int maxi=prev->val;
-//         while(!st.empty())
-//         {
-//             ListNode*front=st.top();
-//             st.pop();
-//             if(front->val<maxi)
-//             {
-//                 if(!st.empty()) 
-//                 {
-//                     ListNode*temp=st.top();
-//                     temp->next=prev;
-//                     delete front;
-//                 }
-//             }
-//             else{
-//                 prev=front;
-//                 maxi=front->val;
-//             }
-//         }
-
-//         return prev;
-        
-//     }
-// };
