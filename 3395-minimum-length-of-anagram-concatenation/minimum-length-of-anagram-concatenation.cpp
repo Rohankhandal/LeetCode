0a7ce240babebf64f8833fa63f3e6a31f0e5095b
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-08 01:24:28 +0530

    Time: 54 ms (45.48%) | Memory: 30.8 MB (35.08%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
@@ -1,35 +1,29 @@
+// t.c:-O(nLogn)
 class Solution {
 public:
-    bool check(string s,int k)
-    {
-        int n=s.size();
-        char cnt1[26]={0};
-        for(int i=0;i<k;i++)
-        {
-            cnt1[s[i]-'a']++;
+    bool check(string s, int k) {  //O(n+k)=>O(N)
+        int n = s.size();
+        char firstFreq[26] = {0};
+        for (int i = 0; i < k; i++) {  //o(K)
+            firstFreq[s[i] - 'a']++;
         }
-       
-        for(int i=k;i<n;i+=k)
-        {
-            char cnt2[26]={0};
-            for(int j=i;j<k+i;j++)
-            {
-                cnt2[s[j]-'a']++;
-            }
-            for(int i=0;i<26;i++)
-            {
-                if(cnt1[i]!=cnt2[i]) return false;
-            }
 
-        }
+        for (int i = k; i < n; i += k) {  //O(n/k)
+            char rangeFreq[26] = {0};
+            for (int j = i; j < k + i; j++) {  //O(K)
+                rangeFreq[s[j] - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) { //O(26)
+                if (firstFreq[i] != rangeFreq[i])
+                    return false;
+            }
+        }//O(N)
         return true;
     }
     int minAnagramLength(string s) {
-        int n=s.size();
-        for(int i=1;i<=n;i++)
-        {
-            if(n%i==0 && check(s,i))
-            {
+        int n = s.size();
+        for (int i = 1; i <= n; i++) {  //O(~logN)
+            if (n % i == 0 && check(s, i)) {  
                 return i;
             }
         }
