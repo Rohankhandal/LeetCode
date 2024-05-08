592bf298c137c5ca35caab72d1901484a18a5580
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-08 18:35:38 +0530

    Time: 8 ms (83.67%) | Memory: 32.3 MB (77.82%) - LeetSync

--- 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.cpp
+++ 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.cpp
@@ -1,44 +1,87 @@
 class Solution {
 public:
-    bool isOk(vector<int>&nums1,vector<int>&nums2,int i,int j,int& diff)
+    bool isOk(vector<int>&nums1,vector<int>&nums2,int& diff)
     {
-        int n = nums1.size();
-        int m = nums2.size();
-        for (int k = 0, l = 0; k < n && l < m;) {
-            if (k == i || k==j) {
-                k++;
-                continue;
+        int cnt=0;
+         int n=nums1.size();
+        int m=nums2.size();
+        int j=0;
+        for(int i=0;i<n&&j<m;i++)
+        {
+            int diffVal=nums2[j]-nums1[i];
+            if(diffVal==diff)
+            {
+                j++;
             }
-           
-            int val = nums2[l] - nums1[k];
-            if (diff == INT_MAX) {
-                diff = val;
-            } else if (diff != val) {
-                return false;
-            }
-            k++;
-            l++;
+            else{ cnt++;}
         }
-        return true;
+        return (cnt<=2)?true:false;
+       
     }
-    int minimumAddedInteger(vector<int>& nums1, vector<int>& nums2) {
-        int ans=0;
-         sort(nums1.begin(),nums1.end());
+    int minimumAddedInteger(vector<int>& nums1, vector<int>& nums2) {  //T.C:-O(N^3)
+        
+        sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         int n=nums1.size();
         int m=nums2.size();
         int mini=INT_MAX;
-        for(int i=0;i<n-1;i++)  //remove i and j index element
+        for(int i=0;i<n;i++)  
         {
-            for(int j=i+1;j<n;j++)
+            int diff=nums2[0]-nums1[i];
+            if(isOk(nums1,nums2,diff))
             {
-               int diff=INT_MAX;
-               if(isOk(nums1,nums2,i,j,diff))
-               {
-                mini=min(mini,diff);
-               }
+                mini=min(diff,mini);
             }
         }
         return mini;
     }
 };
+
+
+
+
+//T.C:-O(N^3) =>Brute Force
+// class Solution {
+// public:
+//     bool isOk(vector<int>&nums1,vector<int>&nums2,int i,int j,int& diff)
+//     {
+//         int n = nums1.size();
+//         int m = nums2.size();
+//         for (int k = 0, l = 0; k < n && l < m;) {
+//             if (k == i || k==j) {
+//                 k++;
+//                 continue;
+//             }
+           
+//             int val = nums2[l] - nums1[k];
+//             if (diff == INT_MAX) {
+//                 diff = val;
+//             } else if (diff != val) {
+//                 return false;
+//             }
+//             k++;
+//             l++;
+//         }
+//         return true;
+//     }
+//     int minimumAddedInteger(vector<int>& nums1, vector<int>& nums2) {  //T.C:-O(N^3)
+//         int ans=0;
+//          sort(nums1.begin(),nums1.end());
+//         sort(nums2.begin(),nums2.end());
+//         int n=nums1.size();
+//         int m=nums2.size();
+//         int mini=INT_MAX;
+//         for(int i=0;i<n-1;i++)  //remove i and j index element
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//                int diff=INT_MAX;
+//                if(isOk(nums1,nums2,i,j,diff))
+//                {
+//                 mini=min(mini,diff);
+//                }
+//             }
+//         }
+//         return mini;
+//     }
+// };
