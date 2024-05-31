137cdd6e3dde2c71fbfac54a43d2c2e442f81066
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-05-31 06:54:44 +0530

    Time: 5 ms (30.43%) | Memory: 10.2 MB (31.25%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        int maxEle=*max_element(nums.begin(),nums.end());
-        for(int i=0;i<=maxEle;i++)
+        int n=nums.size();  //answer not greater than n (Observation)
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<=n;i++)
         {
-            int cnt=0;
-            for(int j=0;j<nums.size();j++)
-            {
-                
-                if(nums[j]>=i) cnt++;
-            }
-            if(cnt==i) return i;
+           
+            int idx=lower_bound(nums.begin(),nums.end(),i)-nums.begin();
+            // cout<<idx<<endl;
+
+            if(i==(n-idx)) return i;
         }
         return -1;
     }
