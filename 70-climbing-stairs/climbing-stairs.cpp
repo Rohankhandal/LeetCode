472b3f8a76b179ffbb2376234027bda0fb4234c4
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-06-26 16:27:05 +0530

    Time: 2 ms (49.64%) | Memory: 6.9 MB (99.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp)
+    int dp[46];
+
+    int solve(int n)
     {
-        dp[0]=1;
-        dp[1]=1;
-        if(dp[n]!=-1)
-        return dp[n];
-
-        dp[n]=solve(n-1,dp) +solve(n-2,dp);
-
-        return dp[n];
+        if(n==0 || n==1) return 1;
+        return solve(n-1)+solve(n-2);
     }
-    int solveUsingTab(int n)
+    //2.Memomization
+    int solveMem(int n)
     {
-        vector<int>dp(n+1,0);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return  dp[n];
+        if(n==0 || n==1) return dp[n]=1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solveMem(n-1)+solveMem(n-2);
     }
     int climbStairs(int n) {
-        // vector<int>dp(n+1,-1);
-        // return solve(n,dp);
-
-        return solveUsingTab(n);
-       
+        memset(dp,-1,sizeof(dp));
+        return solveMem(n);
     }
 };
