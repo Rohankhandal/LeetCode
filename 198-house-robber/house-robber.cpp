5514ad6e1b54597b40c17a01d4fa7f48955755a8
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-06-28 21:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (94.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,80 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i)
-    {
-        if(i>=nums.size())
-        {
-            return 0;
-        }
-        int include=nums[i]+solve(nums,i+2);
-        int exclude=0+solve(nums,i+1);
+    int dp[101];
+     int solve(vector<int>&nums,int i)
+     {
+        if(i>=nums.size()) return 0;
 
-        return max(include,exclude);
-    }
-    int solveUsingMem(vector<int>&nums,int i,vector<int>&dp)
-    {
-        if(i>=nums.size())
-        {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int include=nums[i]+solveUsingMem(nums,i+2,dp);
-        int exclude=0+solveUsingMem(nums,i+1,dp);
+        int take=nums[i]+solve(nums,i+2);
+        int notake=0+solve(nums,i+1);
+        return max(take,notake);
+     }
+     //2.Memoziation
+     int solveMem(vector<int>&nums,int i)
+     {
+        if(i>=nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
 
-        dp[i]=max(include,exclude);
-        return dp[i];
-    }
-    int solveUsingTab(vector<int>&nums,int i)
-    {
-        vector<int>dp(nums.size()+1,0);
-        int n=nums.size();
-        dp[n]=0;
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            int include=nums[i];
-            if(i+2<=n)
-            {
-                include+=dp[i+2];
-            }    
-            int exclude=0+dp[i+1];
+        int take=nums[i]+solveMem(nums,i+2);
+        int notake=0+solveMem(nums,i+1);
 
-            dp[i]=max(include,exclude);
-        }
-
-        return dp[0];
-    }
-    int space(vector<int>&nums,int i)
-    {
-        int n=nums.size();
-        int prev2=0;
-        int prev1=nums[n-1];
-        int curr=0;
-        for(int i=n-2;i>=0;i--)
-        {
-            int include=nums[i];
-            if(i+2<=n)
-            {
-                include+=prev2;
-            }    
-            int exclude=0+prev1;
-
-            curr=max(include,exclude);
-
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
-    }
+        return dp[i]=max(take,notake);
+     }
     int rob(vector<int>& nums) {
-        // return solve(nums,0);
-
-        // vector<int>dp(nums.size()+1,-1);
-        // return solveUsingMem(nums,0,dp);
-        int n=nums.size();
-        return space(nums,n-1);
+        memset(dp,-1,sizeof(dp));
+        return solveMem(nums,0);
     }
 };
