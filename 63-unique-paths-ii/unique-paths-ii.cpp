6823b99cfb18ddd55d49ebf801e66add35bb76ad
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-06-29 23:30:46 +0530

    Time: 3 ms (53.65%) | Memory: 10.2 MB (35.09%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -33,14 +33,40 @@ public:
 
         return dp[i][j]=val1+val2;
     }
+
+    //3.Tabluation
+    int solveTab(vector<vector<int>>& grid, int m, int n) {
+    vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, 0));
+    dp[m - 1][n - 1] = 1;
+
+    for (int i = m - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (i == m - 1 && j == n - 1) continue;
+
+            long long val1 = 0;
+            if (i + 1 < m && grid[i + 1][j] != 1)
+                val1 = dp[i + 1][j]; // down
+
+            long long val2 = 0;
+            if (j + 1 < n && grid[i][j + 1] != 1)
+                val2 = dp[i][j + 1]; // right
+
+            dp[i][j] = val1 + val2;
+        }
+    }
+
+    return dp[0][0];
+}
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        if(obstacleGrid[0][0]==1) return 0;
+        if(obstacleGrid[0][0]==1) return 0;  //IMP BASE CASE
         // return solve(obstacleGrid,0,0,m,n);
 
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solveMem(obstacleGrid,0,0,m,n,dp);
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        // return solveMem(obstacleGrid,0,0,m,n,dp);
+
+        return solveTab(obstacleGrid,m,n);
 
 
 
