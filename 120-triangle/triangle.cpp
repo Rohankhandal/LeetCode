4e013a67126aab99655a31cb8d423a8e74d067a8
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-02 09:10:41 +0530

    Time: 6 ms (35.30%) | Memory: 11.6 MB (10.79%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -29,6 +29,32 @@ public:
         
         return dp[i][j]=ans;
     }
+    
+    //3.Tabluation
+    int solveTab(vector<vector<int>>& triangle)
+    {
+       
+        int n=triangle.size();
+      
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        
+        int ans=INT_MAX;
+        for(int i=n-1;i>=0;i--)
+        {
+            int m=triangle[i].size();
+            for(int j=0;j<m;j++)
+            {
+                 ans=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+                 dp[i][j]=ans;
+            }
+           
+        }
+        
+       
+        
+        return dp[0][0];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
@@ -36,7 +62,10 @@ public:
         // return solve(triangle,n,0,0);
         
         
-        return solveMem(triangle,n,0,0,dp);
+        // return solveMem(triangle,n,0,0,dp);
+        
+        
+        return solveTab(triangle);
     }
 };
 
