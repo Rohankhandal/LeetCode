f88601f8921773518a7c6c04d632c23d06b537fd
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-24 15:45:33 +0530

    Time: 112 ms (29.16%) | Memory: 27.3 MB (30.25%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int res = 0;
-        string top, bot;
-        int top_score, bot_score;
+        int n = s.size();
+        string first = (x > y) ? "ab" : "ba";
+        string second = (x > y) ? "ba" : "ab";
+        int fhigh = (x > y) ? x : y;
+        int shigh = (x > y) ? y : x;
+        int ans = 0;
 
-        if (y > x) {
-            top = "ba";
-            top_score = y;
-            bot = "ab";
-            bot_score = x;
-        } else {
-            top = "ab";
-            top_score = x;
-            bot = "ba";
-            bot_score = y;
-        }
-
-        // Removing first top substrings cause they give more points
-        vector<char> stack;
-        for (char ch : s) {  // Changed 'char' to 'ch'
-            if (ch == top[1] && !stack.empty() && stack.back() == top[0]) {
-                res += top_score;
-                stack.pop_back();
+        stack<char> st;
+        // Process the string to maximize the removal of first pattern
+        for (int i = 0; i < n; ++i) {
+            if (!st.empty() && st.top() == first[0] && s[i] == first[1]) {
+                st.pop();
+                ans += fhigh;
             } else {
-                stack.push_back(ch);
+                st.push(s[i]);
             }
         }
 
-        // Removing bot substrings cause they give less or equal amount of scores
-        vector<char> new_stack;
-        for (char ch : stack) {  // Changed 'char' to 'ch'
-            if (ch == bot[1] && !new_stack.empty() && new_stack.back() == bot[0]) {
-                res += bot_score;
-                new_stack.pop_back();
+        // Now process the remaining string to maximize the removal of second pattern
+        string temp;
+        while (!st.empty()) {
+            temp.push_back(st.top());
+            st.pop();
+        }
+        reverse(temp.begin(), temp.end());
+
+        for (int i = 0; i < temp.size(); ++i) {
+            if (!st.empty() && st.top() == second[0] && temp[i] == second[1]) {
+                st.pop();
+                ans += shigh;
             } else {
-                new_stack.push_back(ch);
+                st.push(temp[i]);
             }
         }
 
-        return res;
+        return ans;
     }
 };
