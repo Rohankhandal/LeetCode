deeebbf16184ea682c4d6d760076500ed3338d9a
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-24 19:46:59 +0530

    Time: 23 ms (72.52%) | Memory: 30.7 MB (22.70%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -19,33 +19,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// head =
-// [2,2,1]
-// root =
-// [2,null,2,null,2,null,1] OUTPUT:-true
-
-// head =
-// [1,10]
-// root =
-// [1,null,1,10,1,9]     Output=>false
-
-//  T.c:-O(N*height of Tree)
-//  S.c:-(N)
 class Solution {
 public:
-   bool isPath(ListNode* head, TreeNode* root){
-        if(!head)
-            return true;
+    bool isPath(ListNode*head,TreeNode*root)
+    {
+        if(head==NULL) return true;
+        if(root==NULL) return false;
+
+         if (head->val != root->val) return false; // Values don't match
+
+        bool left=isPath(head->next,root->left);
+        bool right= isPath(head->next,root->right);
+
+        return left||right;
+
+    }
+    bool solve(ListNode*head,TreeNode*root)
+    {
+        if(root==NULL) return false;
         
-        if(!root)
-            return false;
-        
-        return (root->val == head->val) && (isPath(head->next, root->left) || isPath(head->next, root->right));
+        if(head->val==root->val && isPath(head,root)) return true;
+
+        bool left=solve(head,root->left);
+        bool right=solve(head,root->right);
+
+        return left||right;
+
+       
     }
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if(!root)
-            return false;
-        
-        return (root->val == head->val && isPath(head, root)) || isSubPath(head, root->left) || isSubPath(head, root->right);
+        return solve(head,root);
     }
 };
