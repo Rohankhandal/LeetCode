b9c358fb0c67b82689f50f0722f7df535ea4a7c0
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-25 19:03:56 +0530

    Time: 17 ms (93.22%) | Memory: 16.5 MB (67.07%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -20,15 +20,43 @@ public:
         return dp[amount]=ans;
 
     }
+
+
+    //2.Tabluation
+    int solveTab(vector<int>&coins,int amt)
+    {
+        vector<int>dp(amt+1,0);
+
+        for(int amount=1;amount<=amt;amount++)
+        {
+            int ans=INT_MAX;
+            for(int i=0;i<coins.size();i++)
+            {
+                int res=INT_MAX;
+
+                if(amount-coins[i]>=0)
+                    res=dp[amount-coins[i]];
+
+                if(res!=INT_MAX)
+                    ans=min(ans,1+res);
+            }
+            dp[amount]=ans;
+        }
+        
+
+        return dp[amt]!=INT_MAX?dp[amt]:-1;
+
+    }
     int coinChange(vector<int>& coins, int amount) {
         // int res=solve(coins,amount);   
         // return res!=INT_MAX?res:-1;
 
-        vector<int>dp(amount+1,-1);
-        int res=solveMem(coins,amount,dp);   
-        return res!=INT_MAX?res:-1;
+        // vector<int>dp(amount+1,-1);
+        // int res=solveMem(coins,amount,dp);   
+        // return res!=INT_MAX?res:-1;
 
 
+        return solveTab(coins,amount);
         
     }
 };
