9d5bdd85b2af931b1b276aea3aa6e2437e29525f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-27 14:25:24 +0530

    Time: 19 ms (89.82%) | Memory: 29.9 MB (23.08%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -17,7 +17,7 @@ public:
         }
         return min({skip1,skip2});
     }
-
+    // 2.Memoziation
     int solveMem(string &s,int l,int r,vector<vector<int>>&dp)
     {
         if (l >= r) return 0; // Base case: if left index crosses right, no insertions are needed
@@ -34,11 +34,42 @@ public:
 
         return dp[l][r];
     }
+
+
+    //3.Tabluation
+    int solveTab(string &s)
+    {
+        int n=s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0)); 
+
+        for(int l=n-1;l>=0;l--)
+        {
+            for(int r=0;r<n;r++)
+            {
+                if (l >= r) continue; 
+
+                if (s[l] == s[r]) {
+                    dp[l][r] = dp[l + 1][r - 1]; // No insertion needed if characters match
+                } else {
+                    int insertLeft = 1 + dp[l + 1][ r];
+                    int insertRight = 1 + dp[l][r - 1]; 
+                    dp[l][r] = min(insertLeft, insertRight);
+                }
+            }
+        }
+        return dp[0][n-1];
+
+        
+    }
     int minInsertions(string s) {
         // return solve(s,0,s.size()-1);
 
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1)); // Memoization table
-        return solveMem(s, 0, n - 1, dp);
+        // int n = s.size();
+        // vector<vector<int>> dp(n, vector<int>(n, -1)); // Memoization table
+        // return solveMem(s, 0, n - 1, dp);
+
+
+        // 3.Tabluation
+        return solveTab(s);
     }
 };
