9a135efd3f8a7fcc973c8b3afd083d095edfbfa2
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-28 19:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (18.73%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,140 @@
+class Solution {
+public:
+    // buy=>1 means you can buy
+    // buy=>0 means you can sell
+
+    int solve(vector<int>&prices,int i,int buy)
+    {
+        if(i>=prices.size()) return 0;  //profit=sell-buy => sell+(-buy)
+
+        int ans=0;
+        if(buy) //you can buy
+        {
+            int take=-prices[i]+solve(prices,i+1,0);
+            int skip=0+solve(prices,i+1,buy);
+
+            ans=max(take,skip);
+        }
+        else
+        {
+            int take=prices[i]+solve(prices,i+2,1);   //change here from Part 2 Question
+            int skip=0+solve(prices,i+1,buy);
+
+            ans=max(take,skip);
+        }
+
+        return ans;
+    }
+
+    //2.Memoziation
+    int solveMem(vector<int>&prices,int i,int buy,vector<vector<int>>&dp)
+    {
+        if(i>=prices.size()) return 0;  //profit=sell-buy => sell+(-buy)
+
+        if(dp[i][buy]!=-1) return dp[i][buy];
+
+        int ans=0; 
+        if(buy) //you can buy
+        {
+            int take=-prices[i]+solveMem(prices,i+1,0,dp);
+            int skip=0+solveMem(prices,i+1,buy,dp);
+
+            ans=max(take,skip);
+        }
+        else
+        {
+            int take=prices[i]+solveMem(prices,i+2,1,dp);
+            int skip=0+solveMem(prices,i+1,buy,dp);
+
+            ans=max(take,skip);
+        }
+
+        return dp[i][buy]=ans;
+    }
+
+
+    //3.Tabluation
+    int solveTab(vector<int>&prices)
+    {
+        int n=prices.size();
+         vector<vector<int>>dp(n+1,vector<int>(2,0));
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                 int ans=0; 
+                if(buy) //you can buy
+                {
+                    int take=-prices[i]+dp[i+1][0];
+                    int skip=0+dp[i+1][buy];
+
+                    ans=max(take,skip);
+                }
+                else
+                {
+                    int take=prices[i]+dp[i+2][1];
+                    int skip=0+dp[i+1][buy];
+
+                    ans=max(take,skip);
+                }
+                 dp[i][buy]=ans;
+            }
+        }
+        return dp[0][1];
+        
+    }
+
+    //4.space
+    int space(vector<int>&prices)
+    {
+        int n=prices.size();
+        //  vector<vector<int>>dp(n+1,vector<int>(2,0));
+        vector<int>curr(2,0);
+        vector<int>next(2,0);
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                 int ans=0; 
+                if(buy) //you can buy
+                {
+                    int take=-prices[i]+next[0];
+                    int skip=0+next[buy];
+
+                    ans=max(take,skip);
+                }
+                else
+                {
+                    int take=prices[i]+next[1];
+                    int skip=0+next[buy];
+
+                    ans=max(take,skip);
+                }
+                 curr[buy]=ans;
+            }
+            next=curr;
+        }
+        return next[1];
+        
+    }
+    int maxProfit(vector<int>& prices) {
+        // int buy=1;  //first buy it and then sell it
+        // return solve(prices,0,buy);
+
+        // 2.Memozaiton
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solveMem(prices,0,1,dp);
+
+
+        // 3.Memoziation
+        // return solveTab(prices);
+
+        
+
+         // 4.space
+        // return space(prices);
+    }
+};
