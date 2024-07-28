7b4bbf9fbb0c21d88c267f88f3073014de518f33
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-28 10:41:39 +0530

    Time: 4 ms (50.47%) | Memory: 7.7 MB (29.95%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    void deleteNum(vector<int>&num,int n,int i,int k,int count)
-    {
-        if(num.size()==1)
-        {
-            return ;
-        }
-        if(count==k-1)
-        {
-            count=0;
-            num.erase(num.begin()+i);
-        }
-
-        deleteNum(num,n,(i+1)%num.size(),k,count+1);
-    }
-
     int findTheWinner(int n, int k) {
-        if(k==1) return n;
-        vector<int>num;
-        for(int i=1;i<=n;i++)
+        vector<int>arr(n,0);
+        for(int i=0;i<n;i++)
         {
-            num.push_back(i);
+            arr[i]=i+1;
         }
-        int count=0;
-         deleteNum(num,n,0,k,count);
-         return num[0];
+        int index=0;
+        while(arr.size()>1)
+        {
+           // Calculate the index to remove
+            index = (index + k - 1) % arr.size();
+            // Erase the element
+            arr.erase(arr.begin() + index);
+        }
+         return arr[0];
     }
+   
 };
