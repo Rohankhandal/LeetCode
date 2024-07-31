da1ff982c2c19d8920625d3d5991296027eb311e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-07-31 17:42:11 +0530

    Time: 91 ms (45.85%) | Memory: 104.3 MB (29.06%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -57,6 +57,31 @@ public:
 
         return dp[i][prev+1]=max(take, noTake);
     }
+
+
+    // 3.Tabluation
+    // Recursive function to find the longest chain ending at word i
+    int solveTab(vector<string>& words) {
+        int n = words.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int prev=i-1;prev>=-1;prev--)
+            {
+                int take = 0;
+                if (prev == -1 || isSafe(words[i],words[prev])) {
+                    take = 1 + dp[i + 1][i+1];
+                }
+
+                int noTake = dp[i + 1][ prev+1];
+
+                dp[i][prev+1]=max(take, noTake);
+            }
+        }
+        return dp[0][0];
+        
+    }
      static bool myFunction(string& s1, string& s2) {
         return s1.length() < s2.length();
     }
@@ -67,8 +92,11 @@ public:
         
         // return solve(words, 0, -1);
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solveMem(words,0,-1,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // return solveMem(words,0,-1,dp);
+
+
+        return solveTab(words);
 
     }
 };
