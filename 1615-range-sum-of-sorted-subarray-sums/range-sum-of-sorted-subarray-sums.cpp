d58b2d8392063193759b9589c8e9b286cb99ad66
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-04 22:33:35 +0530

    Time: 75 ms (84.05%) | Memory: 11.4 MB (83.82%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,27 +1,46 @@
+//Approach-2 (How to find sorted subarray sums using Heap)
+//T.C : O(n^2 *n logn)
+//S.C : O(n)
 class Solution {
 public:
-    int M=1e9+7;
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
-        // int n=nums.size();
-        vector<int>arr;
+    typedef pair<int, int> P;
+    int M = 1e9+7;
 
-        for(int i=0;i<n;i++)
-        {
-            int sum=0;
-            for(int j=i;j<n;j++)
-            {
-                sum+=nums[j]%M;
-                arr.push_back(sum);
-            }
-                
-        }
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue<P, vector<P>, greater<P>> pq;
         
-        sort(arr.begin(),arr.end());
-        int ans=0;
-        for(int i=left;i<=right;i++)
-        {
-            ans=(ans%M+arr[i-1]%M)%M;
+        //pair<sum, i> -> (subarray sum, till index i)
+        for (int i = 0; i < n; i++) {
+            pq.push({nums[i], i});
         }
-        return ans;
+
+        int result = 0;
+
+        for (int count = 1; count <= right; count++) {
+            auto p = pq.top();
+            pq.pop();
+          
+          	int sum=p.first;
+            int idx=p.second;
+
+            // If the current index is greater than or equal to left, add the
+            // value to the answer.
+            if (count >= left) {
+                result = (result + sum) % M;
+            }
+
+            // If index is less than the last index, increment it and add its
+            // value
+            int new_idx=idx+1;
+          	P new_pair;
+          
+            if (new_idx < n ) {
+                new_pair.first=sum+nums[new_idx];
+                new_pair.second=new_idx;
+              
+                pq.push(new_pair);
+            }
+        }
+        return result;
     }
 };
