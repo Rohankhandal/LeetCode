2a8e5367eccbd2393b7190ac467ea757e3b9483e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-06 22:17:51 +0530

    Time: 8 ms (9.12%) | Memory: 10.1 MB (5.45%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+      
+        unordered_map<char,int>mp;
+        for(auto &it:word)
+        {
+            mp[it]++;
+        }
+
+        // vector<pair<char,int>>arr;
+        // for(auto&it:mp)
+        // {
+        //     arr.push_back({it.first,it.second});
+        // }
+        // auto cmp=[&](pair<char,int>&p1,pair<char,int>&p2)
+        // {
+        //     return p1.second>p2.second;
+        // };
+
+        // sort(arr.begin(),arr.end(),cmp);
+
+
+        priority_queue<int>pq;
+        for(auto &it:mp)
+        {
+            int freq=it.second;
+            pq.push(freq);
+        }
+
+        int cnt=1;
+        int ans=0;
+        int value=1;
+        while(!pq.empty())
+        {
+            int freq=pq.top();
+            pq.pop();
+            ans+=(freq*value);
+            cnt++;
+            if(cnt==9)
+            {
+                cnt=1;
+                value++;
+            }
+        }
+        return ans;
+    }
+};
+
