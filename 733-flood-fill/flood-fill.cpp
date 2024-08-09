6db9714c38ec9326b5921c6ea60628c238ae7e93
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-10 02:23:59 +0530

    Time: 6 ms (63.18%) | Memory: 17.5 MB (14.37%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -16,10 +16,40 @@ public:
         }
     }
 
+
+    void bfs(vector<vector<int>>& image, int i, int j, int startColor, int color) {
+
+        image[i][j]=color;
+        int n = image.size();
+        int m = image[0].size();
+
+        queue<pair<int,int>>q;
+        q.push({i,j});
+
+        vector<vector<int>> dir{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        while(!q.empty())
+        {
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            for(auto &d:dir)
+            {
+                int _x = d[0] + x;
+                int _y = d[1] + y;
+                 if (_x < 0 || _y < 0 || _x >= n || _y >= m || image[_x][_y] != startColor) continue;
+
+                image[_x][_y]=color;
+                q.push({_x,_y});
+            }
+        }
+       
+    }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int startColor = image[sr][sc];
         if (startColor != color) {
-            dfs(image, sr, sc, startColor, color);
+            bfs(image, sr, sc, startColor, color);
         }
         return image;
     }
