50fe20e72eedac7d744ce4c83a89a9f475067a7b
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-10 01:34:05 +0530

    Time: 172 ms (5.08%) | Memory: 56.1 MB (5.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    vector<pair<int,int>> dir{{1,0},{0,1},{-1,0},{0,-1}};
-    
-    void solve(vector<vector<char>>& grid, int i, int j, int n, int m) {
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') {
-        return;
-    }
-    
-    grid[i][j] = '$';  // Mark as visited
-    
-    for (pair<int,int>&p : dir) {
-        int newI = i + p.first;
-        int newJ = j + p.second;
-        
-            solve(grid, newI, newJ, n, m);  
-    }
-    }
-
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
+    void solve(vector<vector<char>>& grid,int n,int m,int i,int j)
+    {
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]!='1')
+        {
+            return ;
         }
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        int islandCount = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == '1') {
-                    solve(grid, i, j, n, m);
-                    islandCount++;
+
+        grid[i][j]='0';
+
+        vector<vector<int>>dir{{-1,0},{1,0},{0,-1},{0,1}};
+
+        for(auto &it:dir)
+        {
+            int _x=i+it[0];
+            int _y=j+it[1];
+
+           
+            solve(grid,n,m,_x,_y);
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    solve(grid,n,m,i,j);
+                    ans++;
                 }
             }
         }
-        
-        return islandCount;
+        return ans;
     }
 };
