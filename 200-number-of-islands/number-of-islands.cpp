e547b8c8013fdc4302392094ce2732bf242903dd
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-10 01:44:30 +0530

    Time: 35 ms (25.54%) | Memory: 23.1 MB (28.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,68 +1,96 @@
+//Approach-2 (BFS)
+//T.C : O(m*n)
+//S.C : O(1)
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int n,int m,int i,int j)
-    {
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]!='1')
-        {
-            return ;
-        }
-
-        grid[i][j]='0';
-
-        vector<vector<int>>dir{{-1,0},{1,0},{0,-1},{0,1}};
-
-        for(auto &it:dir)
-        {
-            int _x=i+it[0];
-            int _y=j+it[1];
-
-           
-            dfs(grid,n,m,_x,_y);
-        }
-    }
-    void bfs(vector<vector<char>>& grid,int n,int m,int i,int j)
-    {
-        grid[i][j]='0';
-
-        vector<vector<int>>dir{{-1,0},{1,0},{0,-1},{0,1}};
-
-        queue<pair<int,int>>q;
+    vector<pair<int,int>> dir{{1,0},{0,1},{-1,0},{0,-1}};
+    
+    void bfs(vector<vector<char>>& grid, int i, int j, int n, int m) {
+        queue<pair<int,int>> q;
         q.push({i,j});
-        while(!q.empty())
-        {
-            int x=q.front().first;
-            int y=q.front().second;
-
+        grid[i][j] = '$';  // Mark as visited
+        
+        while (!q.empty()) {
+            pair<int,int> p = q.front();
             q.pop();
-
-            for(auto &d:dir)
-            {
-                int _x=x+d[0];
-                int _y=y+d[1];
-                if(_x<0 || _x >=n || _y<0 || _y>=m || grid[_x][_y]!='1') continue;
-
-                grid[_x][_y]='0';
-                q.push({_x,_y});
-            }
-        }
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    // dfs(grid,n,m,i,j);
-                    bfs(grid,n,m,i,j);
-
-                    ans++;
+            
+            for (auto &d : dir) {
+                int newI = p.first + d.first;
+                int newJ = p.second + d.second;
+                
+                if (newI >= 0 && newI < n && newJ >= 0 && newJ < m && grid[newI][newJ] == '1') {
+                    grid[newI][newJ] = '$';  // Mark as visited before pushing to queue
+                    q.push({newI, newJ});
                 }
             }
         }
-        return ans;
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        int islandCount = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j, n, m);
+                    islandCount++;
+                }
+            }
+        }
+        
+        return islandCount;
     }
 };
+
+
+// DFS
+//Approach-1 (DFS)
+//T.C : O(m*n)
+//S.C : O(1)
+
+// class Solution {
+// public:
+//     vector<pair<int,int>> dir{{1,0},{0,1},{-1,0},{0,-1}};
+    
+//     void solve(vector<vector<char>>& grid, int i, int j, int n, int m) {
+//         if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') {
+//         return;
+//     }
+    
+//     grid[i][j] = '$';  // Mark as visited
+    
+//     for (pair<int,int>&p : dir) {
+//         int newI = i + p.first;
+//         int newJ = j + p.second;
+        
+//             solve(grid, newI, newJ, n, m);  
+//     }
+//     }
+
+//     int numIslands(vector<vector<char>>& grid) {
+//         if (grid.empty() || grid[0].empty()) {
+//             return 0;
+//         }
+        
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         int islandCount = 0;
+        
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = 0; j < m; ++j) {
+//                 if (grid[i][j] == '1') {
+//                     solve(grid, i, j, n, m);
+//                     islandCount++;
+//                 }
+//             }
+//         }
+        
+//         return islandCount;
+//     }
+// };
