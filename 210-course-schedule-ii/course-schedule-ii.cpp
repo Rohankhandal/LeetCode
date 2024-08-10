386676df50fac8c5d361ed8fd871d29aa89a7f44
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-10 10:17:08 +0530

    Time: 21 ms (19.77%) | Memory: 18 MB (31.54%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,63 +1,53 @@
 class Solution {
 public:
-    // Perform topological sorting using Kahn's algorithm
-    vector<int> topologicalSort(int n, vector<int>& indegree, unordered_map<int, vector<int>>& adj) {
-        queue<int> q; // Queue to process nodes with zero indegree
-        vector<int> ans; // Vector to store the topological order
-        int count = 0; // Count of nodes processed
-
-        // Add all nodes with zero indegree to the queue
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                ans.push_back(i);
-                count++;
+    vector<int> topologicalSort(unordered_map<int,vector<int>>&adj,vector<int>&inDegree,int n)
+    {
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(inDegree[i]==0) 
+            {
                 q.push(i);
+                ans.push_back(i);
             }
         }
 
-        // Process nodes in the queue
-        while (!q.empty()) {
-            int u = q.front();
+        while(!q.empty())
+        {
+            int u=q.front();
             q.pop();
 
-            // Visit all adjacent nodes of 'u'
-            for (int& v : adj[u]) {
-                indegree[v]--; // Decrement indegree of node 'v'
-                
-                // If indegree of 'v' becomes zero, add it to the queue
-                if (indegree[v] == 0) {
-                    ans.push_back(v);
-                    count++;
+            for(auto &v:adj[u])
+            {
+                inDegree[v]--;
+                if(inDegree[v]==0)
+                {
                     q.push(v);
+                    ans.push_back(v);
                 }
             }
         }
+        vector<int>emptyArr;
+        int size=ans.size();
+        return n==size?ans:emptyArr;
 
-        // If all nodes were processed, return the topological order
-        if (count == n) {
-            return ans;
-        }
-
-        // If there was a cycle, return an empty vector
-        return {};
     }
-
-    // Function to find the order in which courses should be taken
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adj; // Adjacency list
-        vector<int> indegree(numCourses, 0); // Indegree array
+        int n=numCourses;
+        unordered_map<int,vector<int>>adj;
+        vector<int>inDegree(n,0);
+        for(auto &it:prerequisites)
+        {
+            int u=it[1];
+            int v=it[0];
 
-        // Build the graph and indegree array from the prerequisites
-        for (auto& vec : prerequisites) {
-            int a = vec[0];
-            int b = vec[1];
+            adj[u].push_back(v);
 
-            // Edge b -> a (a depends on b)
-            adj[b].push_back(a);
-            indegree[a]++;
+            inDegree[v]++;
         }
 
-        // Perform topological sort and return the order of courses
-        return topologicalSort(numCourses, indegree, adj);
+        return topologicalSort(adj,inDegree,n);
+        
     }
 };
