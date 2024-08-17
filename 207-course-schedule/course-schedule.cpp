8855fdf89a0f21b3ffb06a70367cdb98514b1fff
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-17 12:29:09 +0530

    Time: 18 ms (42.42%) | Memory: 17.7 MB (44.26%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,117 +1,47 @@
-// //USING BFS
-
-// class Solution {
-// public:
-//     // Perform topological sorting using Kahn's algorithm
-//     bool topologicalSort(vector<int>& indegree, unordered_map<int, vector<int>>& adj, int n) {
-//         queue<int> que; // Queue to process nodes with zero indegree
-//         int count = 0; // Count of nodes processed
-
-//         // Add all nodes with zero indegree to the queue
-//         for (int i = 0; i < n; i++) {
-//             if (indegree[i] == 0) {
-//                 count++;
-//                 que.push(i);
-//             }
-//         }
-
-//         // Process nodes in the queue
-//         while (!que.empty()) {
-//             int u = que.front();
-//             que.pop();
-
-//             // Visit all adjacent nodes of 'u'
-//             for (int& v : adj[u]) {
-//                 indegree[v]--; // Decrement indegree of node 'v'
-                
-//                 // If indegree of 'v' becomes zero, add it to the queue
-//                 if (indegree[v] == 0) {
-//                     count++;
-//                     que.push(v);
-//                 }
-//             }
-//         }
-
-//         // If all nodes were processed, return true (no cycle)
-//         if (count == n) {
-//             return true; // All courses can be finished
-//         }
-
-//         return false; // There was a cycle, so not all courses can be finished
-//     }
-
-//     // Function to check if all courses can be finished given the prerequisites
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         unordered_map<int, vector<int>> adj; // Adjacency list
-//         vector<int> indegree(numCourses, 0); // Indegree array
-
-//         // Build the graph and indegree array from the prerequisites
-//         for (auto& vec : prerequisites) {
-//             int a = vec[0];
-//             int b = vec[1];
-
-//             // Edge b -> a (a depends on b)
-//             adj[b].push_back(a);
-//             indegree[a]++;
-//         }
-
-//         // Perform topological sort and check if all courses can be finished
-//         return topologicalSort(indegree, adj, numCourses);
-//     }
-// };
-
-
-
-//USING DFS
-
 class Solution {
 public:
-    bool isCycle(unordered_map<int, vector<int>>&adj,vector<bool>&inRecursion,vector<bool>&visited,int src)
+    bool topologicalSort(unordered_map<int,vector<int>>&adj, vector<int>&inDegree,int n)
     {
-        visited[src]=true;
-        inRecursion[src]=true;
-
-        for(int &v:adj[src])
-        {
-            if(!visited[v] && isCycle(adj,inRecursion,visited,v))
-            {
-                return true;
-            }
-            else if(visited[v] && inRecursion[v])
-            {
-                return true;
-            }
-
-        }
-
-        inRecursion[src]=false;
-        return false;
-    }
-
-    // Function to check if all courses can be finished given the prerequisites
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        unordered_map<int, vector<int>> adj; // Adjacency list
-        vector<bool>inRecursion(n,false);
-        vector<bool>visited(n,false);
-      
-
-        // Build the graph and indegree array from the prerequisites
-        for (auto& vec : prerequisites) {
-            int a = vec[0];
-            int b = vec[1];
-
-            // Edge b -> a (a depends on b)
-            adj[b].push_back(a);
-        }
-
+        queue<int>q;
+        int course=0;
         for(int i=0;i<n;i++)
         {
-            if(!visited[i] && isCycle(adj,inRecursion,visited,i))
+            if(inDegree[i]==0)
             {
-                return false;
+                q.push(i);
+                course++;
             }
         }
-        return true;
+
+        while(!q.empty())
+        {
+            int u=q.front();
+            q.pop();
+            for(auto &v:adj[u])
+            {
+                inDegree[v]--;
+                if(inDegree[v]==0)
+                {
+                    course++;
+                    q.push(v);
+                }
+            }
+        }
+
+        return course==n;
     }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>>adj;
+        vector<int>inDegree(numCourses+1,0);
+        for(auto &it:prerequisites)
+        {
+            int v=it[0];  //it[1] --> it[0]
+            int u=it[1];
+            adj[u].push_back(v);
+            inDegree[v]++;
+        }
+
+       return  topologicalSort(adj,inDegree,numCourses);
+    }
+
 };
