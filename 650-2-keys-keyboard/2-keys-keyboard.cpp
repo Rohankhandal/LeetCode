b644a3837ed1801aca7f0b4bba03690eabe2be6c
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-19 23:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (38.21%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,25 +1,53 @@
-#include <climits>
+//T.c:-O(2^N) and S.c:-O(N)
+// class Solution {
+// public:
+//     int solve(int n, int noOfA, int copyA) {
+//         if (noOfA > n) return 1000; // If noOfA exceeds n, return 1000 (invalid state)
+//         if (noOfA == n) return 0;      // Base case: when we have exactly n 'A's, no more steps needed
 
+//         // Try to paste the copied segment
+//         int paste =1+  solve(n, noOfA + copyA, copyA);
+     
+//         // Try to copy the current segment and then paste
+//         int copyPaste = 1 + 1 + solve(n, noOfA+noOfA, noOfA);
+//          // Adding 2 steps: one for copy and one for paste
+
+//         return min(paste, copyPaste); // Return minimum steps required
+//     }
+
+//     int minSteps(int n) {
+//         if (n == 1) return 0; // If n is 1, no steps are needed
+//         return 1+solve(n, 1, 1); // Start with 1 'A', no copy initially, +1 for first copy operation
+       
+//     }
+// };
+
+
+
+//Bottom UP Approach
 class Solution {
 public:
-    int solve(int n, int noOfA, int copyA) {
-        if (noOfA > n) return 1000; // If noOfA exceeds n, return INT_MAX (invalid state)
-        if (noOfA == n) return 0;      // Base case: when we have exactly n 'A's, no more steps needed
-
-        // Try to paste the copied segment
-        int paste =1+  solve(n, noOfA + copyA, copyA);
-     
-
-        // Try to copy the current segment and then paste
-        int copyPaste = 1 + 1 + solve(n, noOfA+noOfA, noOfA);
-         // Adding 2 steps: one for copy and one for paste
-
-        return min(paste, copyPaste); // Return minimum steps required
-    }
 
     int minSteps(int n) {
-        if (n == 1) return 0; // If n is 1, no steps are needed
-        return 1+solve(n, 1, 1); // Start with 1 'A', no copy initially, +1 for first copy operation
+        vector<int>dp(n+1,0);
+
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=i;
+        }
+
+        for(int i=4;i<=n;i++)
+        {
+            for(int j=2;j*j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    dp[i]=min(dp[i],i/j+dp[j]);
+                    dp[i]=min(dp[i],j+dp[i/j]);
+                }
+            }
+        }
+        return dp[n];
        
     }
 };
