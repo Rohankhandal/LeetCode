b6ad2ec798989154f9bfb7b337e6492057393d7c
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-22 23:46:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (90.78%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,63 +1,25 @@
-/*
-
-    Time Complexity : O(logN) where N is the number.
-
-    Space Complexity : O(logN), Hash Table(unordered_set(set)) space.
-
-    Solved using Math + Hash Table.
-
-*/
 class Solution {
 public:
-
     bool isHappy(int n) {
-        set<int>st;
-        while(true)
+        if(n==1) return true;
+        while(n)
         {
+            int num=n;
             int sum=0;
-            while(n)
+            while(num)
             {
-                int rem=n%10;
-                sum+=pow(rem,2);
-                n/=10;
+                int rem=(num%10);
+                sum+=rem*rem;
+                num/=10;
             }
-            // if sum is 1, return true;
-            if(sum==1)
-            {
-                return true;
-            }
-            //else , the new number is the current sum
             n=sum;
-            
-    //check if we have already encountered that number
-            if(st.count(n))
-                return false;
+            if(n<10)
+            {
+                if(n==1 || n==7) return true;
+                break;
+            } 
+        }  
 
-            st.insert(n);
-        }
         return false;
     }
 };
-
-//Simple solution:-
-// class Solution {
-// public:
-//     long long computeSum(int n){
-//         long long sum=0;
-//         while(n>0){
-//             int last=n%10;
-//             sum+=last*last;
-//             n/=10;
-//         }
-//     return sum;
-//     }
-
-//     bool isHappy(int n) {
-//         if(n<10){
-//             if(n==1 or n==7) return true;
-//         else return false;
-//         }
-//     long long sum=computeSum(n);
-//     return isHappy(sum);
-//     }
-// };
