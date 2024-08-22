1dcd56ecb82c23e5ea129ea9f061f4dd732b9b27
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-22 23:32:25 +0530

    Time: 415 ms (59.84%) | Memory: 11.8 MB (66.71%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int countPrimes(int n) {
-        if (n == 0 || n == 1)
-            return 0;
-        vector<bool> mark(n, true);
-        mark[0] = false;
-        mark[1] = false;
-        for (int i = 2; i <= sqrt(n); i++) {
-            if(mark[i]==true)  //IMP
+    vector<bool> checkPrime(int n)
+    {
+        vector<bool>prime(n,true);
+        prime[0]=false;
+        prime[1]=false;
+
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(prime[i])
             {
-                int j = i * i;
-                while (j < n) {
-                    mark[j] = false;
-                    j = j + i;
+                long long int j=i*i;
+                while(j<n)
+                {
+                    prime[j]=false;
+                    j+=i;
                 }
             }
-            
         }
-        int primeCount = 0;
-        for (int i = 0; i < n; i++) {
 
-            if (mark[i] == true) {
-                primeCount++;
-                // cout<<i<<endl;
-            }
-        }
-        return primeCount;
+        return prime;
+    }
+    int countPrimes(int n) {
+        if(n<=1) return 0;
+
+       vector<bool>prime=checkPrime(n);
+       int ans=0;
+       for(int i=0;i<n;i++){
+        if(prime[i]==true) ans++;
+       }   
+       return ans;
     }
 };
