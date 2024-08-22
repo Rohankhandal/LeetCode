bcad986ff735437be24ed4a564ad2e9ee7fdde0d
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-22 14:43:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (33.08%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,17 +1,29 @@
+//T.c:-O(log(num))
+// class Solution {
+// public:
+//     int findComplement(int num) {
+//         int i=0;
+//         int ans=0;
+//        while(num)
+//        {
+//            if((num & 1)==0)
+//            {
+//                ans+=(1<<i);
+//            }
+//            i++;
+//            num>>=1;
+//        }
+//        return ans;
+//     }
+// };
+
 class Solution {
 public:
     int findComplement(int num) {
-        int i=0;
-        int ans=0;
-       while(num)
-       {
-           if((num & 1)==0)
-           {
-               ans+=(1<<i);
-           }
-           i++;
-           num>>=1;
-       }
-       return ans;
+        int count=(int)(log2(num))+1;
+        long long mask=(1L<<count)-1;
+
+        long long ans=num^mask;
+        return ans;
     }
 };
