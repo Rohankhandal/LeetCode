71b1d48aebbd76b2d166c436e39da281fbb16280
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-25 11:39:25 +0530

    Time: 65 ms (95.08%) | Memory: 41.6 MB (89.64%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,60 +1,50 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int>rank;
-    int find(int num)
-    {
-        if(num==parent[num]) return num;
+    vector<int> parent;  // Stores parent of each node
+    vector<int> rank;    // Stores rank of each set for union by rank
 
-        return parent[num]=find(parent[num]);
+    // Find operation with path compression
+    int find(int x) {
+        if (x == parent[x]) 
+            return x;
+        return parent[x] = find(parent[x]);
     }
 
-    void Union(int x,int y)
-    {
-        int x_parent=find(x);
-        int y_parent=find(y);
-
-        if(x_parent!=y_parent)
-        {
-            if(rank[x_parent]>rank[y_parent])
-            {
-                parent[y_parent]=x_parent;
-            }
-            else if(rank[x_parent]<rank[y_parent])
-            {
-                parent[x_parent]=y_parent;
-            
-            }
-            else
-            {
-                parent[x_parent]=y_parent;
-                rank[y_parent]++;
-            }
+    // Union operation with rank
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if (x_parent == y_parent) 
+            return;
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
         }
     }
+    
     int makeConnected(int n, vector<vector<int>>& connections) {
-
+        // Check if there are enough cables
+        if (connections.size() < n-1)
+            return -1;
+        
+        // Initialize parent and rank vectors
         parent.resize(n);
-        rank.resize(n,0);
-        for(int i=0;i<n;i++)
-        {
-            parent[i]=i;
-        }
-        int components=n;  //no. of edges
-
-        if(connections.size()<n-1) return -1;
-        for(auto &it:connections)
-        {
-            int parent_x=find(it[0]);
-            int parent_y=find(it[1]);
-
-            if(parent_x!=parent_y)
-            {
-                Union(it[0],it[1]);
-                components--;
+        rank.resize(n, 0);
+        
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+        
+        int components = n;  // Start with n components
+        for (auto &vec : connections) {
+            if (find(vec[0]) != find(vec[1])) {
+                components--;  // Decrease component count when connecting two separate components
+                Union(vec[0], vec[1]);
             }
         }
-
-        return components-1;
+        return components - 1;  // Return number of operations needed to connect all components
     }
 };
