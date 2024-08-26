d56d78c2c4d9003a40dd06551fb40034f7d270f2
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-26 15:14:14 +0530

    Time: 4 ms (73.70%) | Memory: 11.8 MB (7.18%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,99 +1,40 @@
-// Bottom-Up
 class Solution {
 public:
-    int solve(vector<vector<int>>& triangle,int n,int i,int j)
+    int solve(vector<vector<int>>&triangle,int i,int j)
     {
-        if(i>=n) return 0;
-        
-        int m=triangle[i].size();
-        if(j>m) return 0;
-        int ans=INT_MAX;
-        
-        ans=triangle[i][j]+min(solve(triangle,n,i+1,j),solve(triangle,n,i+1,j+1));
-        
-        return ans;
-    }
-    
-    //2.Memoziation
-    int solveMem(vector<vector<int>>& triangle,int n,int i,int j, vector<vector<int>>&dp)
-    {
-        if(i>=n) return 0;
-        
-        int m=triangle[i].size();
-        if(j>m) return 0;
-        
-        if(dp[i][j]!=-1 ) return dp[i][j];
-        int ans=INT_MAX;
-        
-        ans=triangle[i][j]+min(solveMem(triangle,n,i+1,j,dp),solveMem(triangle,n,i+1,j+1,dp));
-        
-        return dp[i][j]=ans;
-    }
-    
-    //3.Tabluation
-    int solveTab(vector<vector<int>>& triangle)
-    {
-       
         int n=triangle.size();
-      
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        if(i>=n) return 0;
+        int m=triangle[i].size();
+        if(j>=m) return 0;
+
         
-        
-        int ans=INT_MAX;
-        for(int i=n-1;i>=0;i--)
-        {
-            int m=triangle[i].size();
-            for(int j=0;j<m;j++)
-            {
-                 ans=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
-                 dp[i][j]=ans;
-            }
-           
-        }
-        
+        int currIndex=triangle[i][j]+solve(triangle,i+1,j);
+
+        int nextIndex=triangle[i][j]+solve(triangle,i+1,j+1);
        
-        
-        return dp[0][0];
+        return min(currIndex,nextIndex);
+    }
+    int solveMem(vector<vector<int>>&triangle,int i,int j, vector<vector<int>>&dp)
+    {
+        int n=triangle.size();
+        if(i>=n) return 0;
+        int m=triangle[i].size();
+        if(j>=m) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int currIndex=triangle[i][j]+solveMem(triangle,i+1,j,dp);
+
+        int nextIndex=triangle[i][j]+solveMem(triangle,i+1,j+1,dp);
+       
+        return dp[i][j]=min(currIndex,nextIndex);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
+    //    return  solve(triangle,0,0);
+
+    //2.Memoziation
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        
-        // return solve(triangle,n,0,0);
-        
-        
-        // return solveMem(triangle,n,0,0,dp);
-        
-        
-        return solveTab(triangle);
+        return solveMem(triangle,0,0,dp);
     }
 };
-
-
-// 
-// Bottom-Up
-// class Solution {
-// public:
-//     int minimumTotal(vector<vector<int>>& triangle) {
-//         int height = triangle.size();
-        
-//         // Create a dp table with all elements initialized to 0
-//         vector<vector<int>> dp(height + 1, vector<int>(height + 1, 0));
-        
-//         // Initialize the last row of dp with the last row of the triangle
-//         for (int i = 0; i < height; i++) {
-//             dp[height - 1][i] = triangle[height - 1][i];
-//         }
-        
-//         // Fill the dp table from the second last row to the top
-//         for (int i = height - 2; i >= 0; i--) {
-//             int m = triangle[i].size();
-//             for (int j = 0; j < m; j++) {
-//                 dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
-//             }
-//         }
-        
-//         // The result is stored at the top of the triangle in dp[0][0]
-//         return dp[0][0];
-//     }
-// };
