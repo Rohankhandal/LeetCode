d63d61faff5334d377686ce22fbea3322a789447
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-26 23:04:08 +0530

    Time: 9 ms (61.85%) | Memory: 14.5 MB (38.20%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -101,7 +101,7 @@ public:
         // If (sum + target) is odd or target is impossible, return 0
         if ((sum + target) % 2 != 0 || sum < target) return 0;
 
-        if(sum + target<0) return 0;
+        if(sum + target<0) return 0; //e.g:- nums[100],and target=-100   (VERY IMP)
         int subsetSum = (sum + target) / 2;
         
         return solveTab(nums,n,subsetSum);
