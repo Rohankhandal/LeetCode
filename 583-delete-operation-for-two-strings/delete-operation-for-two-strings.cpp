2379223d5f704079e7537dbc30c53a70c961041c
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-29 00:12:08 +0530

    Time: 22 ms (12.56%) | Memory: 17.1 MB (15.17%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -38,6 +38,37 @@ public:
         return  dp[i][j]=ans;
     }
 
+
+    //3.Tabluation
+    int solveTab(string word1, string word2){
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int j=0;j<m;j++){
+            dp[n][j]=m-j;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][m]=n-i;
+        }
+       
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--){
+                int ans=INT_MAX;
+                if(word1[i]==word2[j])
+                {
+                    ans=dp[i+1][j+1];
+                }
+                else
+                {
+                    ans=1+min(dp[i+1][j],dp[i][j+1]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
@@ -45,7 +76,11 @@ public:
         // return solve(word1,word2,0,0);
 
         //2.Memoziation
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solveMem(word1,word2,0,0,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return solveMem(word1,word2,0,0,dp);
+
+
+        //3.Tabluation
+        return solveTab(word1,word2);
     }
 };
