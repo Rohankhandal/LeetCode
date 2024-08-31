06a2931762d731465a0c3a36676b7ffc52c7a53e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-31 22:57:59 +0530

    Time: 8 ms (6.44%) | Memory: 11.9 MB (40.95%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,95 +1,53 @@
-//Approach-1 (DFS) Using vector as map
-// class Solution {
-// public:
-    
-//     void DFS(Node* node, Node* clone_node, vector<Node*>& visited) {
-//         visited[node->val] = clone_node;
-        
-//         for(Node* x : node->neighbors) {
-//             if(visited[x->val] == NULL) {
-//                 Node* clone = new Node(x->val);
-//                 clone_node->neighbors.push_back(clone);
-//                 DFS(x, clone, visited);
-//             } else {
-//                 clone_node->neighbors.push_back(visited[x->val]);
-//             }
-//         }
-//     }
-    
-//     Node* cloneGraph(Node* node) {
-//         if(!node)
-//             return NULL;
-        
-//         //cloned the given node
-//         Node* clone_node = new Node(node->val);
-        
-//         //Now, clone its neighbours and recursively their neighbours
-//         /*
-//             But if a node reappears, then we need to access that cloned node
-//             So, store them in a vector to access
-//         */
-        
-//         vector<Node*> visited(101, NULL); //Using contraints given
-//         //We could use map also if constraints are not clear (i.e. unordered_map<Node*, Node*> visited;)
-//         visited[node->val] = clone_node;
-        
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
 
-//         DFS(node, clone_node, visited);
- 
-        
-        
-//         return clone_node;
-//     }
-// };
-
-
-//Approach-2 DFS (Using unordered_map)
 class Solution {
 public:
-    
-    unordered_map<Node*, Node*> mp;
-    
-    void DFS(Node* node, Node* clone_node) {
-        
-        for(Node* n : node->neighbors) {
-            
-            if(mp.find(n) == mp.end()) {
-                
-                Node* clone = new Node(n->val);
-                mp[n] = clone;
-                clone_node->neighbors.push_back(clone);
-                
-                DFS(n, clone);
-                
-            } else {
-                
-                clone_node->neighbors.push_back(mp[n]);
-                
+    void solve(unordered_map<Node*,Node*>&mapping,Node*&newNode,Node*node)
+    {
+        for(Node*temp:node->neighbors)
+        {
+            if(mapping.find(temp)!=mapping.end())
+            {
+                newNode->neighbors.push_back(mapping[temp]);
+                // solve(mapping,mapping[temp],temp);
+            }
+            else
+            {   
+                Node* newtemp=new Node(temp->val);
+                newNode->neighbors.push_back(newtemp);
+                mapping[temp]=newtemp;
+                solve(mapping,newtemp,temp);
             }
             
+            
         }
-        
+
     }
-    
     Node* cloneGraph(Node* node) {
-        if(!node)
-            return NULL;
-        
-        mp.clear();
-        
-        //cloned the given node
-        Node* clone_node = new Node(node->val);
-        
-        //Now, cloe its neighbours and recursively their newighbours
-        /*
-                But if a node reappears, then we need to access that cloned node
-                So, store them in a map <Node*, Node*>
-        */
-        
-        mp[node] = clone_node;
-        
-        DFS(node, clone_node);
-        
-        return clone_node;
+        if(node==NULL) return NULL;
+        unordered_map<Node*,Node*>mapping;
+        Node* newNode=new Node(node->val);
+        mapping[node]=newNode;
+        solve(mapping,newNode,node);
+        return newNode;
     }
 };
