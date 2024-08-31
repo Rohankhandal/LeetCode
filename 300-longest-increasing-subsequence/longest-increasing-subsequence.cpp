06eb1d9208d4de0a1028920f62353fb118cde362
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-08-31 14:17:43 +0530

    Time: 264 ms (32.67%) | Memory: 294.9 MB (19.60%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -16,9 +16,40 @@ public:
 
         return dp[i][prev+1]=max(take,skip);
     }
+
+
+    //2.Tabluation
+    int solveTab(vector<int>&nums)
+    {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int prev=i-1;prev>=-1;prev--)
+            {
+                int take=INT_MIN;
+                if(prev==-1 || nums[prev]<nums[i])
+                {
+                    take=1+dp[i+1][i+1];
+                }
+
+                int skip=dp[i+1][prev+1];
+
+                dp[i][prev+1]=max(take,skip);
+            }
+        }
+
+        return dp[0][-1+1];
+        
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(nums,0,-1,dp);
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return solve(nums,0,-1,dp);
+
+
+        return solveTab(nums);
     }
 };
