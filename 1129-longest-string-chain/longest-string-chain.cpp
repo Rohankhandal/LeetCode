b3fcde14fe298bf55aa2a32a470ae3f7bccd11c7
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-09-03 22:28:49 +0530

    Time: 50 ms (88.31%) | Memory: 17.2 MB (62.77%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,135 +1,58 @@
 class Solution {
 public:
-    // Helper function to determine if words[i] can be a valid extension of
-    // words[prev]
-    bool isSafe( const string &s1, const string &s2) {
-        if (s1.size() != s2.size() + 1)
-            return false;
-
-        int first = 0;
-        int second = 0;
-
-        while (first < s1.size()) {
-            if (second < s2.size() && s1[first] == s2[second]) {
-                first++;
-                second++;
-            } else
-                first++;
-        }
-        if (first == s1.size() && second == s2.size())
-            return true;
-        else
-            return false;
-    }
-
-    // Recursive function to find the longest chain ending at word i
-    int solve(vector<string>& words, int i, int prev) {
-        int n = words.size();
-        if (i >= n)
-            return 0;
-
-        int take = 0;
-        if (prev == -1 || isSafe(words[i],words[prev])) {
-            take = 1 + solve(words, i + 1, i);
-        }
-
-        int noTake = solve(words, i + 1, prev);
-
-        return max(take, noTake);
-    }
-
-
-    // 2.Memoziation
-    // Recursive function to find the longest chain ending at word i
-    int solveMem(vector<string>& words, int i, int prev, vector<vector<int>>&dp) {
-        int n = words.size();
-        if (i >= n)
-            return 0;
-
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-
-        int take = 0;
-        if (prev == -1 || isSafe(words[i],words[prev])) {
-            take = 1 + solveMem(words, i + 1, i,dp);
-        }
-
-        int noTake = solveMem(words, i + 1, prev,dp);
-
-        return dp[i][prev+1]=max(take, noTake);
-    }
-
-
-    // 3.Tabluation
-    // Recursive function to find the longest chain ending at word i
-    int solveTab(vector<string>& words) {
-        int n = words.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int prev=i-1;prev>=-1;prev--)
-            {
-                int take = 0;
-                if (prev == -1 || isSafe(words[i],words[prev])) {
-                    take = 1 + dp[i + 1][i+1];
-                }
-
-                int noTake = dp[i + 1][ prev+1];
-
-                dp[i][prev+1]=max(take, noTake);
-            }
-        }
-        return dp[0][0];
-        
-    }
-
-
-    //GOOD APPROACH 
-    int f(vector<string>&words)
+    bool isSequence(string&prev,string&curr)
     {
-        int n = words.size();
-        vector<int> dp(n, 1);
+        int n=prev.size();
+        int m=curr.size();
+        if(n+1!=m) return false;
+        int i=0,j=0;
+        int cnt=0;
+        while(i<n && j<m)
+        {
 
-        for (int i = 0; i < n; i++) {
-        
-            for (int prev = 0; prev < i; prev++) {
-                if (isSafe(words[i],words[prev])) {
-                    if (1 + dp[prev] > dp[i]) {
-                        dp[i] = 1 + dp[prev];
-                    }
+            if(prev[i]!=curr[j])
+            {
+                cnt++;j++;continue;
+            }
+            if(cnt>=2) return false; 
+ 
+
+            i++;
+            j++;
+        }
+       if(i==n && j==m) return true;
+       return (cnt==0);
+    }
+    int longestStrChain(vector<string>& words) {
+        auto comp=[&](string &a,string &b)
+        {
+            if(a.size()==b.size())
+            {
+                return a<b;
+            }
+            return a.size()<b.size();
+        };
+        sort(words.begin(),words.end(),comp);
+        int n=words.size();
+        for(int i=0;i<n;i++)
+        {
+            cout<<words[i]<<endl;
+           
+        }
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(isSequence(words[j],words[i]) && 1+dp[j]>dp[i])
+                {
+                    dp[i]=1+dp[j];
+                    maxi=max(maxi,dp[i]);
                 }
             }
         }
-
-        int maxi = INT_MIN;
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            if (maxi < dp[i]) {
-                maxi = dp[i];
-                index = i;
-            }
-        }
-
+      
         return maxi;
-    }
-     static bool myFunction(string& s1, string& s2) {
-        return s1.length() < s2.length();
-    }
-    // Main function to sort the words and initiate the recursive solution
-    int longestStrChain(vector<string>& words) {
-        int n=words.size();
-        sort(words.begin(), words.end(),myFunction);
-        
-        // return solve(words, 0, -1);
-
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        // return solveMem(words,0,-1,dp);
-
-
-        // return solveTab(words);
-
-
-        return f(words);
-
     }
 };
