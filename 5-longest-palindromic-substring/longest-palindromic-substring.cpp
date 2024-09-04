eb04bb262176e75b65a2bc6063672bd1f4f20ff6
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-09-04 21:58:29 +0530

    Time: 149 ms (34.32%) | Memory: 12.7 MB (67.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     int t[1001][1001];
-    bool isPalindrome(string&s,int i,int j)
-    {
-        if(i>j) return true;
-
-        if(t[i][j]!=-1) return t[i][j];
-        
-        if(s[i]==s[j]){
-            return t[i][j]=isPalindrome(s,i+1,j-1);
-        }
-        return t[i][j]=false;
-    }
     string longestPalindrome(string s) {
         int n=s.size();
-        int maxLen=INT_MIN;
-        int startIndex=0;
-        memset(t,-1,sizeof(t));
-        
-        for(int i=0;i<n;i++)
+        int maxLen=1;
+        int startingIndex=0;
+        memset(t,0,sizeof(t));
+        for(int L=1;L<=n;L++)
         {
-            for(int j=i;j<n;j++)
+            for(int i=0;i+L-1<n;i++)
             {
-                if(isPalindrome(s,i,j))
+                int j=i+L-1;
+
+                if(i==j)  //1 length string
                 {
-                    // cout<<s.substr(i,(j-i+1))<<endl;;
+                    t[i][j]=1;
+                }
+                else if(s[i]==s[j] && L==2) //2 length string
+                {
+                    t[i][j]=1;
+                    maxLen=2;
+                    startingIndex=i;
+                }
+                else if(s[i]==s[j] && t[i+1][j-1]==true){
+                    t[i][j]=1;
                     if(maxLen<(j-i+1))
                     {
                         maxLen=(j-i+1);
-                        startIndex=i;
+                        startingIndex=i;
                     }
+                }
+                else
+                {
+                    t[i][j]=0;
                 }
             }
         }
-        return s.substr(startIndex,maxLen);
+        return s.substr(startingIndex,maxLen);
     }
 }; 
