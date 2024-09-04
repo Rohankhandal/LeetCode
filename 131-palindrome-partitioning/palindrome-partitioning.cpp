5442a5d039191260039aa083129d66a7756f4081
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-09-04 22:10:54 +0530

    Time: 73 ms (86.16%) | Memory: 52.8 MB (98.35%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,97 +1,40 @@
-//Approach-2 (Using DP + Backtracking)
-//T.C : O(2^n)
-//S.C : O(n^2)
 class Solution {
 public:
-    // Backtracking function to generate all palindrome partitions
-    void solve(string &s, int i, vector<string>& currPartition, vector<vector<bool>>& t, vector<vector<string>>& result) {
-        if(i == s.length()) { // Base case: successfully partitioned the entire string
-            result.push_back(currPartition);
-            return;
+    bool isPalindrome(string &s)
+    {
+        int l=0,r=s.size()-1;
+        while(l<=r)
+        {
+           if(s[l]!=s[r]) return false;
+           l++;r--;
         }
-        for(int j = i; j < s.length(); j++) {
-            if(t[i][j] == true) { // If s[i...j] is a palindrome
-                currPartition.push_back(s.substr(i, j-i+1));
-                solve(s, j+1, currPartition, t, result); // Recurse for the rest of the string
-                currPartition.pop_back(); // Backtrack
+        return true;
+
+    }
+    void solve(int i,string &s,vector<vector<string>>&ans,vector<string>&temp)
+    {
+        int n=s.size();
+        if(i>=s.size())
+        {
+            ans.push_back(temp);
+            return ;
+        }
+        string str="";
+        for(int idx=i;idx<n;idx++)
+        {
+            str+=s[idx];
+            if(isPalindrome(str))
+            {
+                temp.push_back(str);
+                solve(idx+1,s,ans,temp);
+                temp.pop_back();
             }
         }
     }
-
     vector<vector<string>> partition(string s) {
-        int n = s.length();
-       
-        vector<vector<bool>> t(n, vector<bool>(n, false));
-      	 // Initialize the DP table for palindromic substrings
-        //t[i][j] = true -> s[i...j] is a palindrome  
-      
-        // Initialize: single characters are palindromes
-        for (int i = 0; i < n; ++i) {
-            t[i][i] = true;
-        }
-        
-        // Fill the DP table
-        for (int L = 2; L <= n; ++L) {
-            for (int i = 0; i < n - L + 1; ++i) {
-                int j = i + L - 1;
-                if (s[i] == s[j]) {
-                    if (L == 2) {
-                        t[i][j] = true;
-                    } else {
-                        t[i][j] = t[i + 1][j - 1];
-                    }
-                }
-            }
-        }
-
-        vector<vector<string>> result;
-        vector<string> currPartition;
-        solve(s, 0, currPartition, t, result);
-        return result;
+        vector<vector<string>>ans;
+        vector<string>temp;
+        solve(0,s,ans,temp);
+        return ans;
     }
 };
-
-
-
-
-//T.c:- O(n*2^n)
-// class Solution {
-// public:
-//     bool isPalindrome(string&s,int l,int r)
-//     {
-//         cout<<s<<endl;
-//         while(l<r)
-//         {
-//             if(s[l]!=s[r]) return false;
-//             l++;r--;
-//         }
-//         return true;
-//     }
-//     void solve(vector<vector<string>>&ans,vector<string>temp,int idx,string&s)
-//     {
-//         if(idx>=s.size())
-//         {
-//             ans.push_back(temp);
-//             return ;
-//         }
-//         for(int i=idx;i<s.size();i++)
-//         {
-//             // string str=s.substr(i,i-idx+1);
-
-//             if(isPalindrome(s,idx,i))
-//             {
-//                 temp.push_back(s.substr(idx,i-idx+1));
-//                 solve(ans,temp,i+1,s);
-//                 temp.pop_back();
-//             }
-//         }
-        
-//     }
-//     vector<vector<string>> partition(string s) {
-//         vector<vector<string>>ans;
-//         vector<string>temp;
-//         int i=0;
-//         solve(ans,temp,i,s);
-//         return ans;
-//     }
-// };
