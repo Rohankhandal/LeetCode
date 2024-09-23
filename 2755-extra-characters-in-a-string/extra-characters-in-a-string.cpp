182b4dfb33c76618502f4813d48e680951dbb547
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-09-23 11:35:46 +0530

    Time: 141 ms (48.82%) | Memory: 88.5 MB (44.60%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -24,13 +24,43 @@ public:
         }
         return dp[i]=min(op1,op2);
     }
+
+    int solveTab(string &s,unordered_map<string,int>&mp)
+    {
+        int n=s.size();
+        vector<int>dp(s.size()+1,0);
+
+       
+        for(int i=n-1;i>=0;i--)
+        {
+             int op1=INT_MAX,op2=INT_MAX;
+            for(int l=1;l<=s.size()-i;l++)
+            {
+                string temp=s.substr(i,l);
+                if(mp.find(temp)!=mp.end())
+                {
+                    op1=min(op1,dp[i+l]);
+                }
+                else
+                {
+                    op2=min(op2,l+dp[i+l]);  //not match
+                }
+            }
+            dp[i]=min(op1,op2);
+        }
+        return dp[0];
+        
+    }
     int minExtraChar(string s, vector<string>& dictionary) {
         unordered_map<string,int>mp;
         for(auto &it:dictionary)
         {
             mp[it]++;
         }
-        vector<int>dp(s.size(),-1);
-        return solve(0,s,mp,dp);
+        // vector<int>dp(s.size(),-1);
+        // return solve(0,s,mp,dp);
+
+
+        return solveTab(s,mp);
     }
 };
