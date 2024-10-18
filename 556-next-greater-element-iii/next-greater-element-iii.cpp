eea8c3c5c6ae7b24e2b6019e96f9c30ed6416044
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-10-18 21:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (99.60%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -6,32 +6,36 @@ public:
         int sz=s.size();
         bool flag=false;
         int idx=0;
-        for(int i=sz-2;i>=0;i--)
-        {
-            if(s[i]<s[i+1])
-            {
-                flag=true;
-                idx=i;
+        // Find the first digit from the right that is smaller than the next digit
+        for (int i = sz - 2; i >= 0; i--) {
+            if (s[i] < s[i + 1]) {
+                flag = true;
+                idx = i;
+                break;          // Break when the first smaller element is found
+            }
+        }
+
+        if (!flag) return -1;    // If no such digit is found, return -1
+        
+        // Find the smallest element larger than s[idx] from the right side
+        int j = 0;
+        for (j = sz - 1; j > idx; j--) {
+            if (s[j] > s[idx]) {
                 break;
             }
         }
-        if(!flag) return -1;
-      
-        //find next greater element from right
-        int j=0;
-        for(j=sz-1;j>idx;j--)
-        {
-            if(s[j]>s[idx])
-            {
-                break;
-            }
-        }
-        swap(s[idx],s[j]);
-        reverse(s.begin()+idx+1,s.end());
 
-        long long temp=stoll(s);
+        // Swap the two elements
+        swap(s[idx], s[j]);
+        
+        // Reverse the elements to the right of idx to get the next permutation
+        reverse(s.begin() + idx + 1, s.end());
 
-        if(temp>INT_MAX) return -1;
+        // Convert the string back to a number
+        long long temp = stoll(s);
+        
+        // If the number exceeds INT_MAX, return -1, otherwise return the number
+        if (temp > INT_MAX) return -1;
         else return temp;
     }
 };
