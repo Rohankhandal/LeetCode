5276a761edeb5ffa9119b53d62a02ef45785e2e5
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-10-18 19:21:39 +0530

    Time: 5 ms (99.95%) | Memory: 28.1 MB (10.09%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,26 +4,39 @@ public:
         vector<int>ans;
         stack<int>st;
         int n=nums.size();
-        //for circular array ,we add array nums in exist array nums
-        // e.g nums=[1,2,1] after add array itself
-        // nums=[1,2,1,1,2,1]
-        //we thing virtual of cirular array
-        for(int i=2*n-1;i>=0;i--)
-        {
-            while(!st.empty() && st.top()<=nums[i%n]) //IMP
-            {
-                st.pop();
-            }
-            //if index i is in range of 0 to n
-            if(i<n)
-            {
-                int temp=st.empty()?-1:st.top();
-                ans.push_back(temp);
-            }
-            st.push(nums[i%n]);
 
+        for(int i=n-1;i>=0;i--)
+        {
+            st.push(nums[i]);
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.empty())
+            {
+                ans.push_back(-1);
+                st.push(nums[i]);
+            }
+            else
+            {
+                while(!st.empty() && st.top()<=nums[i])
+                {
+                    st.pop();
+                }
+                if(st.empty())
+                {
+                    ans.push_back(-1);
+                     
+                }
+                else
+                {
+                    ans.push_back(st.top());
+                }
+                st.push(nums[i]);
+            }
         }
         reverse(ans.begin(),ans.end());
+
         return ans;
     }
 };
