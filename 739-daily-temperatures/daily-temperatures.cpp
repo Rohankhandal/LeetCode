e8fdb1265f83d463df19a3de55748d5e520dc31e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-10-28 18:46:07 +0530

    Time: 38 ms (37.89%) | Memory: 106.5 MB (25.28%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int>ans;
-        stack<pair<int,int>>st;
-        int n=temperatures.size();
-        for(int i=n-1;i>=0;i--)
-        {
-            if(st.empty())
-            {
+        vector<int>ans;  // Vector to store the result
+        stack<pair<int,int>>st;  // Stack stores {temperature, count} pairs
+        int n = temperatures.size();
+        
+        // Process from right to left
+        for(int i = n-1; i >= 0; i--) {
+            // If stack is empty, no warmer day ahead
+            if(st.empty()) {
                 ans.push_back(0);
-                st.push({temperatures[i],1});
+                st.push({temperatures[i], 1});  // Push current temp with count 1
             }
-            else
-            {
-                int cnt=1;
-                while(!st.empty() && st.top().first<=temperatures[i])
-                {
-                    cnt+=st.top().second;
-                    st.pop();
+            else {
+                int cnt = 1;  // Initialize count for current temperature
+                
+                // While we find temperatures <= current temperature
+                // Add their counts to get total days to next warmer temperature
+                while(!st.empty() && st.top().first <= temperatures[i]) {
+                    cnt += st.top().second;  // Add the count from stack top
+                    st.pop();  // Remove the smaller/equal temperature
                 }
-                if(st.empty())
-                {
+                
+                // If stack becomes empty, no warmer day ahead
+                if(st.empty()) {
                     ans.push_back(0);
-                    st.push({temperatures[i],1});
+                    st.push({temperatures[i], 1});
                 }
-                else
-                {
+                else {
+                    // Found a warmer day, push the count of days
                     ans.push_back(cnt);
-                    //push the element
-                st.push({temperatures[i],cnt});
+                    // Push current temperature with its count
+                    st.push({temperatures[i], cnt});
                 }
-                
-                
             }
         }
 
-        reverse(ans.begin(),ans.end());
+        // Since we processed right to left, reverse to get correct order
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
