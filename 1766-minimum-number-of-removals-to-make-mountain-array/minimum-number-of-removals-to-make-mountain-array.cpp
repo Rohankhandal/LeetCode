321a8db707367c35e146016a33caeccf6710062a
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-10-30 13:54:14 +0530

    Time: 69 ms (31.82%) | Memory: 14.8 MB (67.26%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
-       int n=nums.size();
-        vector<int>dp1(n,1); 
-        vector<int>dp2(n,1);
+       int n = nums.size();
+       
+       // dp1[i] stores length of longest increasing subsequence ending at index i
+       vector<int> dp1(n, 1); 
+       // dp2[i] stores length of longest decreasing subsequence starting from index i
+       vector<int> dp2(n, 1);
 
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(nums[j]<nums[i])
-                {
-                    dp1[i]=max(dp1[i],1+dp1[j]);
-                }
-            }
-        }
+       // Fill dp1 for LIS ending at each index
+       for (int i = 1; i < n; i++) {
+           for (int j = 0; j < i; j++) {
+               if (nums[j] < nums[i]) {
+                   dp1[i] = max(dp1[i], 1 + dp1[j]);
+               }
+           }
+       }
 
-        //check increasing subsequency from last
-        for(int i=n-2;i>0;i--)
-        {
-            for(int j=n-1;j>i;j--)
-            {
-                if(nums[j]<nums[i])
-                {
-                    dp2[i]=max(dp2[i],1+dp2[j]);
-                }
-            }
-        }
-        for(auto &it:dp1)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
-        for(auto &it:dp2)
-        {
-            cout<<it<<" ";
-        }
-        int maxMountainSeq=0;
-        for(int i=0;i<n;i++)
-        {
-            if(dp1[i]!=1 && dp2[i]!=1)
-                 maxMountainSeq=max(dp1[i]+dp2[i]-1,maxMountainSeq);
-        }
+       // Fill dp2 for LIS starting from each index, working backwards
+       for (int i = n - 2; i > 0; i--) {
+           for (int j = n - 1; j > i; j--) {
+               if (nums[j] < nums[i]) {
+                   dp2[i] = max(dp2[i], 1 + dp2[j]);
+               }
+           }
+       }
+       
+       int maxMountainSeq = 0;
+       // Calculate the maximum valid mountain sequence length
+       for (int i = 0; i < n; i++) {
+           // A valid peak must have both an increasing and a decreasing sequence
+           if (dp1[i] != 1 && dp2[i] != 1) {
+               maxMountainSeq = max(dp1[i] + dp2[i] - 1, maxMountainSeq);
+           }
+       }
 
-        return n-maxMountainSeq;
+       // Minimum removals to get the mountain array
+       return n - maxMountainSeq;
     }
 };
