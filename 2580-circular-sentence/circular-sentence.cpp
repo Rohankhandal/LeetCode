c949661457159b7ebbcedb37403074b5b82fbfaf
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-02 11:13:04 +0530

    Time: 4 ms (5.45%) | Memory: 8.2 MB (59.78%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,28 +1,59 @@
+//T.C:-O(N)
+// class Solution {
+// public:
+//     bool isCircularSentence(string sentence) {
+//         stringstream str(sentence);
+//         string tokenStr;
+//         char firstChar=sentence[0];
+//         char last='0';
+//         char start='0';
+//         while(getline(str,tokenStr,' '))
+//         {
+//             if(last=='0')  // it means this is first string
+//             {
+//                 last=tokenStr[tokenStr.size()-1];
+//                 continue;
+//             }
+//             start=tokenStr[0];
+
+//             if(start!=last) return false;
+
+//             last=tokenStr[tokenStr.size()-1];
+//         }
+
+//         if(last!=firstChar) return false;
+
+//         return true;
+
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        stringstream str(sentence);
-        string tokenStr;
         char firstChar=sentence[0];
+        int n=sentence.size();
         char last='0';
         char start='0';
-        while(getline(str,tokenStr,' '))
+        char prev='0';
+        for(int i=0;i<n;i++)
         {
-            if(last=='0')  // it means this is first string
+            char ch=sentence[i];
+            if(ch==' ')  //it means next char is first char of next string
             {
-                last=tokenStr[tokenStr.size()-1];
-                continue;
+                start=sentence[i+1];
+                last=sentence[i-1];
+                if(start!=last) return false;
+
             }
-            start=tokenStr[0];
-
-            if(start!=last) return false;
-
-            last=tokenStr[tokenStr.size()-1];
+            prev=ch;
         }
 
-        if(last!=firstChar) return false;
+        if(firstChar!=prev) return false;
 
         return true;
-
     }
 };
