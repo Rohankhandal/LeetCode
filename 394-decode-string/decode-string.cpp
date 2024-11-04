39a5832dabb753372fe6c39994834ce5a7afeb6c
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-04 11:45:19 +0530

    Time: 5 ms (9.97%) | Memory: 13.2 MB (5.44%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -3,10 +3,12 @@ public:
     string decodeString(string s) {
         stack<char> st;
         int n = s.size();
+
         for (int i = 0; i < n; i++) {
             char ch = s[i];
             if (ch == ']') {
                 string temp = "";
+
                 while (!st.empty() && st.top() != '[') {
                     temp = st.top() + temp;
                     st.pop();
@@ -16,21 +18,21 @@ public:
 
                 string numStr = "";
                 while (!st.empty() && isdigit(st.top())) {
-                    numStr = st.top() + numStr; // Collect digits in reverse
+                    numStr = st.top() + numStr;  // Collect digits in reverse
                     st.pop();
                 }
 
                 int rep = stoi(numStr);
-                
+
                 string d = "";
                 for (int j = 0; j < rep; j++) {
                     d += temp;
                 }
+
                 for (char it : d) {
-                     st.push(it);
-                 }
-            } else
-            {
+                    st.push(it);
+                }
+            } else {
                 st.push(ch);
             }
         }
@@ -40,50 +42,8 @@ public:
             ans = st.top() + ans;
             st.pop();
         }
+
         return ans;
     }
 };
 
-// stack<char> st;
-//         int n = s.size();
-
-//         for (int i = 0; i < n; i++) {
-//             char ch = s[i];
-//             if (ch == ']') {
-//                 string temp = "";
-
-//                 while (!st.empty() && st.top() != '[') {
-//                     temp = st.top() + temp;
-//                     st.pop();
-//                 }
-
-//                 st.pop();
-
-//                 string numStr = "";
-//                 while (!st.empty() && isdigit(st.top())) {
-//                     numStr = st.top() + numStr;  // Collect digits in reverse
-//                     st.pop();
-//                 }
-
-//                 int rep = stoi(numStr);
-
-//                 string d = "";
-//                 for (int j = 0; j < rep; j++) {
-//                     d += temp;
-//                 }
-
-//                 for (char it : d) {
-//                     st.push(it);
-//                 }
-//             } else {
-//                 st.push(ch);
-//             }
-//         }
-
-//         string ans = "";
-//         while (!st.empty()) {
-//             ans = st.top() + ans;
-//             st.pop();
-//         }
-
-//         return ans;
