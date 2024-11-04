8df224908f4ab923f6bc5ab18cf11a369f53fb54
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-04 23:41:39 +0530

    Time: 31 ms (5.15%) | Memory: 11.8 MB (41.37%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,29 +1,68 @@
+// class Solution {
+// public:
+//     string simplifyPath(string path) {
+//     stack<string> dirs;
+//     stringstream ss(path);
+//     string part;
+
+//     // Split path by '/' and process each part
+//     while (getline(ss, part, '/')) {
+//         if (part == "" || part == ".") {
+//             // Skip empty or current directory
+//             continue;
+//         } else if (part == "..") {
+//             // Go up one directory if possible
+//             if (!dirs.empty()) {
+//                 dirs.pop();
+//             }
+//         } else {
+//             // Push the valid directory name onto the stack
+//             dirs.push(part);
+//         }
+//     }
+
+//     // Construct the simplified path
+//     string result;
+//     while (!dirs.empty()) {
+//         result = "/" + dirs.top() + result;
+//         dirs.pop();
+//     }
+
+//     // If the result is empty, it means we're at root
+//     return result.empty() ? "/" : result;
+//     }
+// };
+
 class Solution {
 public:
     string simplifyPath(string path) {
-        stringstream ss(path);           
-        string tokken;
-        vector<string>temp;
-        while(getline(ss,tokken,'/'))   //tokenize words on the basis of '/'
-        {
-            if(tokken=="." || tokken=="")
-            {
+        stack<string> st;
+        int i = 0;
+        while (i < path.size()) {
+            int start = i;
+            int end = i + 1;
+            while (end < path.size() && path[end] != '/') {
+                ++end;
+            }
+
+            string minPath = path.substr(start, end - start);
+            i = end;
+            cout << minPath << endl; // check what store in stack
+
+            if (minPath == "/" || minPath == "/.")
                 continue;
-            }
-            if(tokken!="..")
-            {
-                temp.push_back(tokken);
-            }
-            else if(!temp.empty())
-            {
-                temp.pop_back();
+
+            if (minPath != "/..") {
+                st.push(minPath);
+            } else if (!st.empty()) {
+                st.pop();
             }
         }
-        string ans="";
-        if(temp.size()==0) return "/";
-        for(auto &it:temp)
-        {
-            ans+="/"+it;
+
+        string ans = st.empty() ? "/" : ""; // e.g:- /../
+        while (!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
         }
         return ans;
     }
