a9d101b08fe84aabd8eedefc179f6817caf695b2
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-14 15:52:21 +0530

    Time: 23 ms (91.39%) | Memory: 87.3 MB (34.13%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int maxi=*max_element(quantities.begin(),quantities.end());
-    
-        int l=1,r=maxi;
-        int ans=0;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            //check is mid is correct for all products
-            int i=1;
-            for(auto it:quantities)
-            {
-                while(it>0)
-                {
-                    it-=mid;
-                    i++;
-                }
+        int left = 1, right = *max_element(quantities.begin(), quantities.end());
+        int ans = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int requiredStores = 0;
+
+            for (int quantity : quantities) {
+                requiredStores += (quantity + mid - 1) / mid; // calculates ceiling of quantity/mid
             }
-            // cout<<mid<<" "<<i<<endl;
-            if(i<=n+1)
-            {
-                ans=mid;
-                r=mid-1;
+
+            if (requiredStores <= n) {
+                ans = mid;    // possible solution, try for a smaller max quantity
+                right = mid - 1;
+            } else {
+                left = mid + 1; // too few stores, increase mid to reduce the load
             }
-            else l=mid+1;
         }
+
         return ans;
     }
 };
