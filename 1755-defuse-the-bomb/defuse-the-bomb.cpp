528fbe2ec77834cce5e7e6e3deb7e25796e7ad21
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-18 20:28:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (17.93%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    void forward(vector<int>&code,int i,vector<int>&ans,int k)
-    {
-        int cnt=0;
-        int sum=0;
-        int n=code.size();
-        while(cnt<k)
-        {
-
-            sum+=code[(i+1)%n];
-            i++;
-            cnt++;
-        }
-        ans.push_back(sum);
-
-    }
-    
-    void backward(vector<int>&code,int i,vector<int>&ans,int k)
-    {
-        int cnt=0;
-        int sum=0;
-        int n=code.size();
-        while(cnt<abs(k))
-        {
-            if(i==0) i=n;
-
-            sum+=code[(i-1)%n];
-            i--;
-            cnt++;
-        }
-        ans.push_back(sum);
-
-    }
     vector<int> decrypt(vector<int>& code, int k) {
         vector<int>ans;
+        int start=1,end=k;
+        int sum=0;
         int n=code.size();
-
-        for(int i=0;i<n;i++)
+        if(k<0)
         {
-            if(k>0) forward(code,i,ans,k);
-            else if(k<0) backward(code,i,ans,k);
-            else ans.push_back(0);
+            start=n-abs(k);
+            end=n-1;
+        }   
+        
+        for(int i=start;i<=end;i++)
+        {
+            sum+=code[i];
+        }   
+        ans.push_back(sum);
+
+        for(int i=1;i<n;i++)
+        {
+            sum-=code[(start)%n];
+            // cout<<sum<<" ";
+            end++;
+            sum+=code[end%n];
+            // cout<<sum<<endl;
+            start++;
+            ans.push_back(sum);
+
         }
         return ans;
     }
