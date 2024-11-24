0961b8e6f244c624fbcb15b994e472540b98b647
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-24 17:01:23 +0530

    Time: 131 ms (34.53%) | Memory: 245 MB (5.07%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        map<int, int> pointToCount;
+        vector<int>range(1e6+2,0);
+        int n=intervals.size();
 
-        // Mark the starting and ending points in the map
-        for (vector<int> interval : intervals) {
-            pointToCount[interval[0]]++;
-            pointToCount[interval[1] + 1]--;
+        for(int i=0;i<n;i++)
+        {
+            int l=intervals[i][0];
+            int r=intervals[i][1];
+            range[l]++;
+            range[r+1]--;
         }
 
-        int concurrentIntervals = 0;
-        int maxConcurrentIntervals = 0;
-        // Iterate over the numbers in ascending order
-        for (pair<int, int> p : pointToCount) {
-            // Add the currently active intervals
-            concurrentIntervals += p.second;
-
-            // Update the maximum active intervals at any time
-            maxConcurrentIntervals =
-                max(maxConcurrentIntervals, concurrentIntervals);
+        int maxCollision=0;
+        for(int i=1;i<1e6+2;i++)
+        {
+            range[i]=range[i]+range[i-1];
+            maxCollision=max(maxCollision,range[i]);
         }
+        return maxCollision;
 
-        return maxConcurrentIntervals;
     }
 };
