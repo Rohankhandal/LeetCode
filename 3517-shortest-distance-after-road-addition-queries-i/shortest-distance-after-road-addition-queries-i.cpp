dab90a3a98f29b16140dd901c953b5c691b28388
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-11-27 23:24:07 +0530

    Time: 195 ms (46.90%) | Memory: 144.8 MB (25.85%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -1,44 +1,52 @@
 class Solution {
 public:
+    int findShortestPath(int src,int des,unordered_map<int,vector<int>>&adj)
+    {
+        int n=des+1;
+        vector<int>minDis(n,INT_MAX);
+        // vector<bool>visited(n,false);l
+
+         queue<int> pq;
+        pq.push(src);
+        minDis[src] = 0;
+        while(!pq.empty())
+        {
+            int u=pq.front();
+            pq.pop();
+
+            for(auto &v:adj[u])
+            {
+                if(minDis[u]+1<minDis[v])
+                {
+                    minDis[v]=minDis[u]+1;
+                    pq.push(v);
+                }
+            }
+            
+
+        }
+
+        return (minDis[des] == INT_MAX) ? -1 : minDis[des];
+    }
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        vector<int>ans;
         unordered_map<int,vector<int>>adj;
+       
         for(int i=0;i<n-1;i++)
         {
             adj[i].push_back(i+1);
         }
-
-        
-        auto shortestPath = [&](int start, int end) {
-            vector<int> dist(n, INT_MAX);
-            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-            dist[start] = 0;
-            pq.push({0, start});
-            
-            while (!pq.empty()) {
-                auto [d, u] = pq.top();
-                pq.pop();
-                
-                for (auto &v : adj[u]) {
-                    int adjNode=v;
-                    if (1 + d < dist[v]) {
-                        dist[v] = dist[u] + 1;
-                        pq.push({dist[v], v});
-                    }
-                }
-            }
-            
-            return dist[end];
-        };
-
-        vector<int> ans;
     
-        for (auto& q : queries) {
-            int u = q[0];
-            int v = q[1];
+        for(auto &q:queries)
+        {
+            int u=q[0];
+            int v=q[1];
+             
             adj[u].push_back(v);
-            ans.push_back(shortestPath(0, n - 1));
+            int res=findShortestPath(0,n-1,adj);
+            ans.push_back(res);
         }
-        
+
         return ans;
     }
 };
