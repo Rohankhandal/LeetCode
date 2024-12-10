2ab9fcf52c7e70e59b4af71d94b8ecc7c85094c0
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-12-10 08:55:43 +0530

    Time: 44 ms (91.09%) | Memory: 126.2 MB (61.02%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
+     static bool cmp(vector<int>&a,vector<int>&b){
+           if(a[0]==b[0])return a[1]<b[1];
+           return a[0]<b[0];
+     }
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
-        return findEvents(events, 0, 0, dp);
-    }
-    // Recursive function to find the greatest sum for the pairs.
-    int findEvents(vector<vector<int>>& events, int idx, int cnt,
-                   vector<vector<int>>& dp) {
-        if (cnt == 2 || idx >= events.size()) return 0;
-        if (dp[idx][cnt] == -1) {
-            int end = events[idx][1];
-            int lo = idx + 1, hi = events.size() - 1;
-            while (lo < hi) {
-                int mid = lo + ((hi - lo) >> 1);
-                if (events[mid][0] > end)
-                    hi = mid;
-                else
-                    lo = mid + 1;
+        sort(events.begin(),events.end(),cmp);
+        int n=events.size(),ans=0,temp=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        for(int i=0;i<n;i++){      
+
+            while(!q.empty() && q.top().first<events[i][0]){
+                temp=max(temp,q.top().second);
+                q.pop();
             }
-            int include =
-                events[idx][2] + (lo < events.size() && events[lo][0] > end
-                                      ? findEvents(events, lo, cnt + 1, dp)
-                                      : 0);
-            int exclude = findEvents(events, idx + 1, cnt, dp);
-            dp[idx][cnt] = max(include, exclude);
+
+            ans=max(ans,events[i][2]+temp);
+            q.push({events[i][1],events[i][2]});
         }
-        return dp[idx][cnt];
+        return ans;
     }
 };
