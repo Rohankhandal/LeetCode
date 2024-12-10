91b7f356974e3679ae85f0bb905957e5b1cb0958
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-12-10 08:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (27.61%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,20 +1,26 @@
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        vector<int> bitArr(32, 0);
-        for (int num : nums) {
-            for (int i = 0; i < 32; i++) {
-                if (num & (1 << i)) {
-                    bitArr[i]++;
+class Solution
+{
+    public:
+        int singleNumber(vector<int> &nums)
+        {
+            vector<int> bitArr(32, 0);
+            for (int num: nums)
+            {
+                for (int i = 0; i < 32; i++)
+                {
+                    if (num &(1 << i))
+                    {
+                        bitArr[i]++;
+                    }
                 }
             }
-        }
 
-        long long ans = 0;
-        for (int i = 0; i < 32; i++) {
-            bitArr[i] %= 3;
-            ans += bitArr[i] * pow(2,i)*1LL;
+            long long ans = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                bitArr[i] %= 3;
+                ans += bitArr[i] *(1LL << i);
+            }
+            return static_cast<int> (ans);
         }
-        return static_cast<int>(ans);
-    }
 };
