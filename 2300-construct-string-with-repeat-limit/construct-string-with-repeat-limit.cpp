2464c32c890b6e2d8e9638caeec29b5c8a54964f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-12-18 17:51:10 +0530

    Time: 18 ms (88.08%) | Memory: 30.5 MB (6.90%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        vector<int> freq(26, 0);
-        for (char ch : s) {
-            freq[ch - 'a']++;
+        vector<int>freq(26,0);
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            freq[s[i]-'a']++;
         }
 
-        string result;
-        int currentCharIndex = 25;  // Start from the largest character
-        while (currentCharIndex >= 0) {
-            if (freq[currentCharIndex] == 0) {
-                currentCharIndex--;
-                continue;
+        string ans="";
+        for(int i=25;i>=0;)
+        {
+            if(freq[i]>0)
+            {
+                int use=min(repeatLimit,freq[i]);
+                freq[i]-=use;
+                ans+=string(use,'a'+i);
+
+                if(freq[i]>0) //find for next smaller element
+                {
+                    int j=i;
+                    j--;
+                    while(j>=0 && freq[j]==0)
+                    {
+                        j--;
+                    }
+
+                    if(j<0) //not found
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ans+=string(1,'a'+j);
+                        freq[j]--;
+                    }
+                }
+                else
+                {
+                    i--;
+                }
             }
-
-            int use = min(freq[currentCharIndex], repeatLimit);
-            result.append(use, 'a' + currentCharIndex);
-            freq[currentCharIndex] -= use;
-
-            if (freq[currentCharIndex] >
-                0) {  // Need to add a smaller character
-                int smallerCharIndex = currentCharIndex - 1;
-                while (smallerCharIndex >= 0 && freq[smallerCharIndex] == 0) {
-                    smallerCharIndex--;
-                }
-                if (smallerCharIndex < 0) {
-                    break;
-                }
-                result.push_back('a' + smallerCharIndex);
-                freq[smallerCharIndex]--;
+            else
+            {
+                i--;
             }
         }
+        return ans;
 
-        return result;
+
     }
 };
