d59e70ce62431878fd7cd6ccfeaf357747c44d78
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2024-12-24 18:13:16 +0530

    Time: 259 ms (68.44%) | Memory: 10.7 MB (63.68%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>>dir{{1,0},{-1,0},{0,1},{0,-1}};
-    bool find(vector<vector<char>>&board,int i,int j,string &word,int idx)
+        vector<vector<int>>dir{{-1,0},{0,1},{1,0},{0,-1}};
+    bool isSame(vector<vector<char>>& board, string &word,int i,int j,int idx)
     {
-        if(idx==word.length()) return true;
+        int n=board.size();
+        int m=board[0].size();
 
-        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='$')
+        if(idx>=word.size()) return true;
+        
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]!=word[idx]) return false;
+
+        // Temporarily mark the cell as visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        for(auto &d:dir)
         {
-            return false;
-        }
+            int newI=i+d[0];
+            int newJ=j+d[1];
 
-        if(board[i][j]!=word[idx])
-        {
-            return false;
-        }
-        //store current char in temp
-        char temp=board[i][j];
-        //visited the current char
-        board[i][j]='$';
-
-        //now word are match board[i][j]==word[idx]
-        for(auto &it:dir)
-        {
-            int new_x=i+it[0];
-            int new_y=j+it[1];
-
-            if(find(board,new_x,new_y,word,idx+1))
+            if(isSame(board,word,newI,newJ,idx+1))
             {
                 return true;
             }
+            
         }
-        //backtracking
-        board[i][j]=temp;
-
+        // Backtrack by unmarking the cell
+        board[i][j] = temp;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++)
+        int n=board.size();
+        int m=board[0].size();
+
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(board[i][j]==word[0] && find(board,i,j,word,0)){
+                if(board[i][j]==word[0] && isSame(board,word,i,j,0))
+                {
                     return true;
                 }
             }
