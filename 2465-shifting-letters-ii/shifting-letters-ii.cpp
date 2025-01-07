2521aa2774ee0416c2e966c313c5a5381df2835b
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-01-07 15:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 97.8 MB (84.18%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -2,44 +2,37 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
-        vector<int> diffArray(
-            n, 0);  // Initialize a difference array with all elements set to 0.
+        vector<int> v(n + 1, 0);
 
-        for (auto shift : shifts) {
-            if (shift[2] == 1) {        // If direction is forward (1):
-                diffArray[shift[0]]++;  // Increment at the start index to
-                                        // indicate a forward shift.
-                if (shift[1] + 1 < n) {
-                    diffArray[shift[1] +
-                              1]--;  // Decrement at the end+1 index to stop the
-                                     // effect of the shift.
-                }
-            } else {                    // If direction is backward (0):
-                diffArray[shift[0]]--;  // Decrement at the start index to
-                                        // indicate a backward shift.
-                if (shift[1] + 1 < n) {
-                    diffArray[shift[1] +
-                              1]++;  // Increment at the end+1 index to stop the
-                                     // effect of the shift.
-                }
+        // Apply the shifts to the difference array
+        for (auto &q : shifts) {
+            int l = q[0];
+            int r = q[1];
+            int dir = q[2];
+
+            if (dir == 1) {
+                v[l]++;
+                v[r + 1]--;
+            } else {
+                v[l]--;
+                v[r + 1]++;
             }
         }
 
-        string result(n, ' ');
-        int numberOfShifts = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            numberOfShifts = (numberOfShifts + diffArray[i]) %
-                             26;  // Update cumulative shifts, keeping within
-                                  // the alphabet range.
-            if (numberOfShifts < 0)
-                numberOfShifts +=
-                    26;  // Ensure `numberOfShifts` is non-negative.
-
-            // Calculate the new character by shifting `s[i]`
-            result[i] = 'a' + (s[i] - 'a' + numberOfShifts) % 26;
+        // Compute the prefix sum to get the net shift at each index
+        for (int i = 1; i < n; i++) {
+            v[i] += v[i - 1];
         }
 
-        return result;
+        // Apply the calculated shifts to the string
+        for (int i = 0; i < n; i++) {
+            int val = (s[i] - 'a' + v[i]) % 26;
+            if (val < 0) {
+                val += 26;
+            }
+            s[i] = 'a' + val;
+        }
+
+        return s;
     }
 };
