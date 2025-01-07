dc48e173b4f84d38bf8fa37cdd08a509c6a0641a
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-01-07 15:40:33 +0530

    Time: 7 ms (53.01%) | Memory: 98.3 MB (46.01%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.size();
-        vector<int> v(n + 1, 0);
+        int n=s.size();
+        vector<int>v(n+1,0);
+        for(auto &q:shifts)
+        {
+            int l=q[0];
+            int r=q[1];
+            int dir=q[2];
 
-        // Apply the shifts to the difference array
-        for (auto &q : shifts) {
-            int l = q[0];
-            int r = q[1];
-            int dir = q[2];
-
-            if (dir == 1) {
+            if(dir==1)
+            {
                 v[l]++;
-                v[r + 1]--;
-            } else {
+                v[r+1]--;
+            }
+            else
+            {
                 v[l]--;
-                v[r + 1]++;
+                v[r+1]++;
             }
         }
-
-        // Compute the prefix sum to get the net shift at each index
-        for (int i = 1; i < n; i++) {
-            v[i] += v[i - 1];
+        for(int i=1;i<=n;i++)
+        {
+            v[i]=v[i]+v[i-1];
         }
 
-        // Apply the calculated shifts to the string
-        for (int i = 0; i < n; i++) {
-            int val = (s[i] - 'a' + v[i]) % 26;
-            if (val < 0) {
-                val += 26;
+        string ans=s;
+        for(int i=0;i<n;i++)
+        {
+            // cout<<v[i]<<" ";
+            int val=((s[i]-'a')+v[i])%26;
+            if(val<0)
+            {
+                val+=26;
             }
-            s[i] = 'a' + val;
+            s[i]=val+'a';
         }
-
+        // cout<<(-1)%26<<endl;
         return s;
     }
 };
