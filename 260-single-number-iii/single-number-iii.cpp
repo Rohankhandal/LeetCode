c9c3d40be5512e534479f1aeb6b115ada325a3b5
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-01-23 22:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (59.10%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,25 +1,34 @@
+//Approach (Optimal)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        long long xorValue=0;
-        for(auto &it:nums)
-        {
-            xorValue^=it;
+        long long xor_r = 0;
+
+        for(int &num : nums) {
+            xor_r ^= num;
         }
 
-         long long firstUnsetBit=(xorValue&(xorValue-1))^xorValue;
-        cout<<firstUnsetBit<<endl;
-        int one=0,two=0;
-        for(auto &it:nums)
-        {
-            if(it&firstUnsetBit)  //1
-            {
-                one^=it;
+        //mask -> right most set bit search 
+        int mask = (xor_r) & (-xor_r);
+        /*
+            It works because if you take the two's complement negation of a number, first you complement it,
+            setting all zeroes to the right of the lowest set bit to one and the lowest set bit to zero,
+            then you add one, setting the bits on the right to zero and the lowest set bit becomes one again, ending the carry chain.
+        */
+
+        int groupa = 0;
+        int groupb = 0;
+
+        for(int &num : nums) {
+            if(num & mask) {
+                groupa ^= num;
+            } else {
+                groupb ^= num;
             }
-            else two^=it;
         }
-        return {one,two};
 
-
+        return {groupa, groupb};
     }
 };
