18b10fca11a3d577342c3fdf3d4c7d2e820ced7d
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-01-24 20:14:13 +0530

    Time: 189 ms (69.91%) | Memory: 25.3 MB (26.20%) - LeetSync

---
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findMinimumTime(vector<int>& strength, int k) {
+        int n = strength.size();
+    vector<int> perm = strength; // Create a copy for permutations
+    int minTime = INT_MAX;
+
+    // Try all permutations of the lock order
+    sort(perm.begin(), perm.end());
+    do {
+        int time = 0;
+        int energy = 0;
+        int x = 1;
+
+        for (int i = 0; i < n; ++i) {
+            int required = perm[i];
+
+            // Calculate the time to reach the required energy
+            if (energy < required) {
+                time += (required - energy + x - 1) / x; // Ceiling division
+                energy = required;
+            }
+
+            // Break the lock
+            energy = 0; // Reset energy
+            x += k;     // Increase factor x
+        }
+
+        minTime = min(minTime, time);
+
+    } while (next_permutation(perm.begin(), perm.end()));
+
+    return minTime;
+    }
+};
