1dfe4bf6ad045ddf842a1626dda81cbc088228ca
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-02-02 07:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (58.89%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int n=nums.size();
+//         for(int rotate=0;rotate<n;rotate++)
+//         {
+//             vector<int>temp(n,0);
+//             for(int j=0;j<n;j++)
+//             {
+//                 temp[((rotate+j)%n)]=nums[j];
+//             }
+//             if(is_sorted(temp.begin(),temp.end())) return true;
+//         }
+//         return false;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
         int n=nums.size();
-        for(int rotate=0;rotate<n;rotate++)
-        {
-            vector<int>temp(n,0);
-            for(int j=0;j<n;j++)
-            {
-                temp[((rotate+j)%n)]=nums[j];
-            }
-            if(is_sorted(temp.begin(),temp.end())) return true;
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]) cnt++;
         }
-        return false;
+
+        if(nums[n-1]>nums[0]) cnt++;
+
+        return (cnt<=1);
         
     }
 };
