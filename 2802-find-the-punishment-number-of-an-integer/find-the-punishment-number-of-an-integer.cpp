27bb5b3fae578fa24f13c8d75127a21eb7b0c379
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-02-15 06:50:16 +0530

    Time: 29 ms (71.79%) | Memory: 9 MB (71.28%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,30 +1,31 @@
 class Solution {
-private:
-    bool is_valid(int i, string s, int t) {
-        if(i == s.size() && t == 0) return true;
-        else if(i == s.size()) return false;
-
-        string temp = "";
-        bool ans = false;
-        for(int ind = i; ind < s.size(); ind++) {
-            temp += s[ind];
-            ans = ans || is_valid(ind+1, s, t - stoi(temp));
-            if(ans) return true;
-        }
-
-        return ans;
-    }
-
 public:
+    bool isValidPartition(string &str,int target,int currSum,int idx)
+    {
+        int n=str.size();
+        if(idx>=n)
+        {
+            return currSum==target;
+        }
+        int val=0;
+
+        for(int i=idx;i<n;i++)
+        {
+            val=val*10+(str[i]-'0');
+            // if(val>target) return false;
+            if(isValidPartition(str,target,currSum+val,i+1)) return true;
+        }
+        return false;
+    }
     int punishmentNumber(int n) {
-        int ans = 0;
-        for(int i=1;i<=n;i++) {
-            if(is_valid(0, to_string(i*i), i)) {
-                ans += i*i;
-                cout<<i*i<<endl;
+         int totalSum = 0;
+        for (int i = 1; i <= n; ++i) {
+            int square = i * i;
+            string squareStr = to_string(square);
+            if (isValidPartition(squareStr, i,0,0)) {
+                totalSum += square;
             }
         }
-
-        return ans;
+        return totalSum;
     }
 };
