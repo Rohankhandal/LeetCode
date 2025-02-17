a120c5c7b361c5f2b4e2ca9feb413de4b723c03f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-02-17 13:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.61%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,22 +1,21 @@
-// T.C:-(26^N) S.C:-O(N)
 class Solution {
-    int buildString(vector<int>& freq){
-        int ways = 0;
-        //Any char can be at current position
-        for(int i=0;i<26;++i){
-            if(freq[i]>0){//We can use curr char
+public:
+    int solve(vector<int>&freq)
+    {
+        int ways=0;
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]>0){
                 freq[i]--;
-                ways += 1 + buildString(freq);
+                ways+=1+solve(freq);
                 freq[i]++;
             }
         }
         return ways;
     }
-public:
     int numTilePossibilities(string tiles) {
-        vector<int> freq(26,0);
-        for(char c: tiles)
-            freq[c-'A']++;
-        return buildString(freq);
+        vector<int>freq(26,0);
+        for(auto &c:tiles) freq[c-'A']++;
+        return solve(freq);
     }
 };
