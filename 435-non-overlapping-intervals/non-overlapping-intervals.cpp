6b3c7f916217623cc9978cb27d400e9c7ff18642
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-02-18 17:05:33 +0530

    Time: 56 ms (41.97%) | Memory: 94.1 MB (41.43%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,80 +1,26 @@
-//Approach-1
+//https://leetcode.com/problems/non-overlapping-intervals/description/comments/1576818
+//Three ways of collesion -> 
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(begin(intervals), end(intervals));
-        int n = intervals.size(); 
-        
-        int count = 0;
-        int i = 0, j = 1;
-        
-        while(j < n) {
-            vector<int> curr_interval = intervals[i];
-            vector<int> next_interval = intervals[j];
-            
-            int curr_start = curr_interval[0];
-            int curr_end   = curr_interval[1];
-            
-            int next_start = next_interval[0];
-            int next_end   = next_interval[1];
-            
-            if(curr_end <= next_start) { //SAFE
-                i = j;
-                j++;
-            } else if(curr_end <= next_end) {
-                /*
-                    GREEDY : Remove next interval as it has high chances to
-                             overlap with future intervals
-                */
-                j++;
-                count++;
-            } else if(curr_end > next_end) {
-                /*
-                    GREEDY : Remove current interval as it has high chances to
-                             overlap with future intervals
-                */
-                i = j;
-                j++;
-                count++;
-            }
+      int n=intervals.size();
+      sort(intervals.begin(),intervals.end());
+      int end=intervals[0][1];
+      int overlap=0;
+      for(int i=1;i<n;i++)
+      {
+        int s=intervals[i][0];
+        int e=intervals[i][1];
+
+        if(s<end){
+            overlap++;
+            end=min(e,end);
         }
-
-        return count;
-
+        else 
+        {
+            end=e;
+        }
+      }  
+      return overlap;
     }
 };
-
-
-//Approach-2 (Same as 1st Approach, Just different implementation)
-// class Solution {
-// public:
-//     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-//         sort(begin(intervals), end(intervals));
-//         int n = intervals.size(); 
-        
-//         int count = 0;
-//         int i = 1;
-//         vector<int> lastInterval = intervals[0];
-//         while(i < n)  {
-            
-//             int curr_end   = intervals[i][1];
-//             int curr_start = intervals[i][0];
-            
-//             int last_end = lastInterval[1];
-            
-//             if(curr_start >= last_end) {
-//                 lastInterval = intervals[i]; //safe
-//             } else if(curr_end >= last_end) {
-//                 count++; //Being Greedy
-//             } else if(curr_end < last_end) {
-//                 lastInterval = intervals[i]; //Being Greedy
-//                 count++;
-//             }
-            
-//             i++;
-//         }
-
-//         return count;
-
-//     }
-// };
