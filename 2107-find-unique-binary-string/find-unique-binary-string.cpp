547db6f29e93ef38d8fe92696c43e5646443a8d6
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-02-20 10:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (99.65%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,32 +1,50 @@
+// class Solution {
+// public:
+//     void solve(int i,string &temp,set<string>&st,string &ans)
+//     {
+//         int n=temp.size();
+//         if(i>=temp.size())
+//         {
+//             // cout<<temp<<endl;
+//             if(st.count(temp)==0)
+//             {
+//                 ans=temp;
+//             } 
+//             return ; 
+//         } 
+
+//         if(ans!="") return ;
+
+//         for(char ch='0';ch<='1';ch++)
+//         {
+//             temp[i]=ch;
+//             solve(i+1,temp,st,ans);
+//         }
+//     }
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         set<string>st(nums.begin(),nums.end());
+//         int n=nums.size();
+//         string temp(n,'_');
+//         string ans="";
+//         solve(0,temp,st,ans);
+//         return ans;
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void solve(int i,string &temp,set<string>&st,string &ans)
-    {
-        int n=temp.size();
-        if(i>=temp.size())
-        {
-            // cout<<temp<<endl;
-            if(st.count(temp)==0)
-            {
-                ans=temp;
-            } 
-            return ; 
-        } 
-
-        if(ans!="") return ;
-
-        for(char ch='0';ch<='1';ch++)
-        {
-            temp[i]=ch;
-            solve(i+1,temp,st,ans);
-        }
-    }
+    
     string findDifferentBinaryString(vector<string>& nums) {
-        set<string>st(nums.begin(),nums.end());
-        int n=nums.size();
-        string temp(n,'_');
-        string ans="";
-        solve(0,temp,st,ans);
+         int n = nums.size();
+        string ans = "";
+        
+        for(int i = 0; i < n; i++) {
+            ans += (nums[i][i] == '0') ? '1' : '0';
+        }
+        
         return ans;
         
     }
