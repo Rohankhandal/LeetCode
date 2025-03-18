3c291de9122ed5e629f480cf6bc06e5b287c3920
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-03-17 23:27:20 -0700

    Time: 0 ms (100.00%) | Memory: 60.9 MB (64.15%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -6,16 +6,23 @@ public:
         int l=0;
         int r=0;
         int ans=0;
+        int currSum=0;
+        int currXor=0;
         while(r<n)
         {
-            while((nums[r]&res)!=0)
+            currSum+=nums[r];
+            currXor^=nums[r];
+
+            while(currSum!=currXor && l<r)
             {
-                res^=nums[l];
+                currSum-=nums[l];
+                currXor^=nums[l];
                 l++;
             }
 
-            ans=max(ans,r-l+1);
-            res|=nums[r];
+            if(currSum==currXor)
+                ans=max(ans,r-l+1);
+
             r++;
         }
         return ans;
