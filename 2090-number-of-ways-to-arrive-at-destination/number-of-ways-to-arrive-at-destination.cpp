773957127344a5ff6e730b27d38e276076f81642
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-03-23 09:27:50 +0530

    Time: 8 ms (66.46%) | Memory: 35.2 MB (68.08%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,48 +1,47 @@
-// Time complexity:
-// O(n∗log(n))
-
-// Space complexity:
-// O(n)
 class Solution {
 public:
-    int M = 1e9 + 7;
-
+    int M=1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<int> ways(n, 0);
-        vector<long long> dist(n, LLONG_MAX);  // Change to long long
-        unordered_map<int, vector<pair<int, int>>> adj;
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto &r:roads)
+        {
+            int u=r[0];
+            int v=r[1];
+            int wt=r[2];
 
-        for (auto &it : roads) {
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
+            adj[u].push_back({wt,v});
+            adj[v].push_back({wt,u});
         }
 
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> q;
-        q.push({0, 0});  // {distance, node}
-        dist[0] = 0;
-        ways[0] = 1;
+        vector<long long>dist(n,LLONG_MAX);
+        vector<int>ways(n,0);
+        
+        dist[0]=0;
+        ways[0]=1;
+        priority_queue<pair<long long ,int>,vector<pair<long long ,int>>,greater<pair<long long ,int>>>pq;
+        pq.push({0,0});
+        while(!pq.empty())
+        {
+            long long  wt=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
 
-        while (!q.empty()) {
-            int u = q.top().second;
-            long long dis = q.top().first;  // Change to long long
-            q.pop();
-
-            for (auto &it : adj[u]) {
-                int adjNode = it.first;
-                int d = it.second;
-
-                if (dis + d < dist[adjNode]) {
-                    // Found a shorter path
-                    ways[adjNode] = ways[u];
-                    dist[adjNode] = dis + d;
-                    q.push({dist[adjNode], adjNode});
-                } else if (dis + d == dist[adjNode]) {
-                    // Found another path with the same shortest distance
-                    ways[adjNode] = (ways[adjNode] + ways[u]) % M;
+            for(auto &it:adj[node])
+            {
+                long long  d=it.first;
+                int v=it.second;
+                if((d+wt)<dist[v])
+                {
+                    dist[v]=d+wt;
+                    ways[v]=ways[node];
+                    pq.push({dist[v],v});
+                }
+                else if(d+wt==dist[v])
+                {
+                    ways[v]=(ways[v]+ways[node])%M;
                 }
             }
         }
-
-        return ways[n-1] % M;
+        return ways[n-1]%M;
     }
 };
