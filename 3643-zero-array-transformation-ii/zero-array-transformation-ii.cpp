6bd612fea218a711de820b6d2d989885ca681d29
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-03-26 19:30:00 +0530

    Time: 85 ms (9.65%) | Memory: 373.2 MB (5.15%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    bool isValidZero(int m,vector<int>&nums,vector<vector<int>>&queries){
+    bool isPossible(vector<int> nums, vector<vector<int>>& queries,int limit)
+    {
         int n=nums.size();
-        vector<int>pre(n+1,0);   // to efficiently perform range updates on an array in constant time
-        for(int i=0;i<m;i++){
-            int left=queries[i][0];
-            int right=queries[i][1];
+        vector<int>v(n+2,0);
+        for(int i=0;i<limit;i++)
+        {
+            int l=queries[i][0];
+            int r=queries[i][1];
             int val=queries[i][2];
-            pre[left]+=val;    //Increment pre[left] by val → pre[left] += val
-
-            if(right+1<n)pre[right+1]-=val;  //Decrement pre[right+1] by val → pre[right+1] -= val (to stop adding beyond right)
+            v[l]+=val;
+            v[r+1]-=val;
         }
-            int pre_sum=0;
-            for(int i=0;i<nums.size();i++){
-                pre_sum+=pre[i];   //calculate prefix sum of designed pre array.
-                if(pre_sum<nums[i])return false;  // if prefix sum till ith index is les than nums[i] return false(invalid).
+
+        for(int i=1;i<n;i++)
+        {
+            v[i]+=v[i-1];
+        }
+
+        for(int i=0;i<n;i++){
+            if(v[i]>=nums[i])
+            {
+                nums[i]=0;
             }
+            if(nums[i]!=0) return false;
+        }
         return true;
     }
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-     int n=nums.size();
-     int low=0,high=queries.size();
-     int ans=-1;
-     while(low<=high){
-        int mid=low+(high-low)/2;
-        if(isValidZero(mid,nums,queries)){
-            high=mid-1;
-            ans=mid;
-        }else{
-            low=mid+1;
+        int l=0,r=queries.size();
+        int ans=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(isPossible(nums,queries,mid))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else {
+                l=mid+1;
+            }
         }
-     }   
-     return ans;
+        return ans;
     }
 };
