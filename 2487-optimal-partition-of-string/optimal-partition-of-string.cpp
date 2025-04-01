7713fbbaccec440819d0bcf0b8ee748ab177784f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-01 14:25:18 +0530

    Time: 2 ms (88.25%) | Memory: 12.9 MB (90.39%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,23 +1,38 @@
+// class Solution {
+// public:
+//     int partitionString(string s) {
+//         unordered_map<char,int>mp;
+//         int n=s.size();
+//         int ans=1;
+//         for(int i=0;i<n;i++){
+//             if(mp.find(s[i])!=mp.end())
+//             {
+//                 ans++;
+//                 mp.clear();
+//             }
+//             mp[s[i]]++;
+//         }   
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_map<char,int>mp;
-        int n=s.size();
-        int i=0,j=0;
-        int cnt=1;
-        while(j<n)
-        {
-            mp[s[j]]++;
-            bool flag=true;
-            if(mp[s[j]]>=2)
-            {
-                cnt++;
-                mp.clear();
-                mp[s[j]]++;
-            }
-            j++;
+        vector<int> lastSeen(26, -1);
+        
+        int count = 0;
+        int substringStart = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            if (lastSeen[s[i] - 'a'] >= substringStart) {
+                count++;
+                substringStart = i;
+            }
+            lastSeen[s[i] - 'a'] = i;
         }
-        return cnt;
+
+        return count+1;
     }
 };
