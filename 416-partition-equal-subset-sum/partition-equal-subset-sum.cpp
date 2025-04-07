dcdbc01e61d65a436a381cd39bb2e68316e62633
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-07 07:53:14 +0530

    Time: 374 ms (5.00%) | Memory: 242.8 MB (5.25%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,79 +1,22 @@
 class Solution {
 public:
-bool solve(vector<int>&nums,int target,int i)
-{
-    if(i>=nums.size()) return false;
-    if(target<0) return 0;
-    if(target==0) return 1;
-
-    bool include=solve(nums,target-nums[i],i+1);
-    bool exclude=solve(nums,target,i+1);
-
-    return include||exclude;
-}
-bool solveMem(vector<int>&nums,int target,int i,vector<vector<int>>&dp)
-{
-    if(i>=nums.size()) return 0;
-    if(target<0) return 0;
-    if(target==0) return 1;
-
-    if(dp[i][target]!=-1) return dp[i][target];
-
-    bool include=solveMem(nums,target-nums[i],i+1,dp);
-    bool exclude=solveMem(nums,target,i+1,dp);
-
-    dp[i][target]=include||exclude;
-
-    return dp[i][target];
-
-}
-bool solveTab(vector<int>&nums,int target)
-{
-    vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,0));
-
-    for(int i=0;i<nums.size();i++)
-    {
-        dp[i][0]=1;
-    }
-
-    for(int i=nums.size()-1;i>=0;i--)
-    {
-        for(int t=1;t<=target;t++)
+    int f(int i,vector<int>&nums,int s1,int total, vector<vector<int>>&dp){
+        if(i>=nums.size()) 
         {
-            bool include=0;
-            if(t-nums[i]>=0)
-            {
-                include=dp[i+1][t-nums[i]];
-            }
-            bool exclude=dp[i+1][t];
-
-            dp[i][t]=include||exclude;
+            int s2=total-s1;
+            return s1==s2;
         }
+        if(dp[i][s1]!=-1) return dp[i][s1];
+
+        int take=f(i+1,nums,s1+nums[i],total,dp);
+        int skip=f(i+1,nums,s1,total,dp);
+
+        return dp[i][s1]=(take||skip);
     }
-
-    return dp[0][target];
-
-}
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(auto &it:nums)
-        {
-            sum+=it;
-        }
-        if(sum%2!=0) return false;
-        
-        // return solve(nums,sum/2,0);
-
-        // using MEM
         int n=nums.size();
-        int target=sum/2;
-        // vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        // return solveMem(nums,target,0,dp);
-
-
-        //using bottomm
-        return solveTab(nums,target);
+        int total=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(n,vector<int>(total+1,-1));
+        return f(0,nums,0,total,dp);
     }
-
-
 };
