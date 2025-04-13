5d4bb21e93fb4940ec947764d95449d4a1d021cd
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-13 07:24:12 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (34.93%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,43 +1,26 @@
-#define M 1000000007
+
 class Solution {
 public:
-    long long power(long long x,long long y)
+int M=1e9+7;
+    long long power(long long x,long long n)
     {
         long long ans=1;
-        while(y>0)
+        while(n>0)
         {
-            if(y&1)  //odd number
+            if(n&1)
             {
-                ans=(ans*x)%M; 
+                ans=(ans*x)%M;
             }
-            x=(x*x)%M;
 
-            y>>=1;  //y=y/2 right shift
+            x=(x*x)%M;
+            n>>=1;
         }
         return ans%M;
     }
-    
     int countGoodNumbers(long long n) {
+        long long even=(n+1)/2;
         long long odd=n/2;
-        long long even=n/2+n%2;
 
-        return (power(5,even)* power(4,odd))%M;  //we have define our own power function 
-        // otherwise , we get TLE
+        return (power((long long)5,even)*power((long long)4,odd))%M;
     }
 };
-
-
-
-
-// long long power(long long x,long long y)   //T.c:-O(logN)  S.c:-O(logN) (recusion call stack)
-//     {
-//         if(y==0) return 1;
-
-//         long long ans=power(x,y/2);
-//         ans*=ans;  //ans=(x,y/2)*(x,y/2)
-//         ans%=mod;
-
-//         if(y%2) ans*=x; //(x,7) =(x,3)*(x,3)*x
-//         ans%=mod;
-//         return  ans;
-//     }
