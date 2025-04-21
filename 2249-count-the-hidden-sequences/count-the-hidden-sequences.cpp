8d8de5ae11f456807b003633ad6cd423235d49f2
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-21 11:07:30 +0530

    Time: 0 ms (100.00%) | Memory: 112.6 MB (75.00%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -1,56 +1,76 @@
+// class Solution {
+// public:
+//     typedef long long ll;
+//     bool isPossible(vector<int>& differences, ll tar, ll lower, ll upper) {
+//         int fVal = tar;
+//         for (int i = 0; i < differences.size(); i++) {
+//             fVal += differences[i];
+//             if (fVal < lower || fVal > upper) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     int numberOfArrays(vector<int>& differences, int lower, int upper) {
+//         // compute prefix sum bounds
+//         long long minPrefix = 0, maxPrefix = 0, sum = 0;
+//         for (int diff : differences) {
+//             sum += diff;
+//             minPrefix = min(minPrefix, sum);
+//             maxPrefix = max(maxPrefix, sum);
+//         }
+
+//         ll l = lower - minPrefix;
+//         ll r = upper - maxPrefix;
+
+//         ll leftBound = INT_MAX, rightBound = INT_MIN;
+
+//         // Left bound
+//         ll lo = l, hi = r;
+//         while (lo <= hi) {
+//             ll mid = lo + (hi - lo) / 2;
+//             if (isPossible(differences, mid, lower, upper)) {
+//                 leftBound = mid;
+//                 hi = mid - 1;
+//             } else {
+//                 lo = mid + 1;
+//             }
+//         }
+
+//         // Right bound
+//         lo = l, hi = r;
+//         while (lo <= hi) {
+//             ll mid = lo + (hi - lo) / 2;
+//             if (isPossible(differences, mid, lower, upper)) {
+//                 rightBound = mid;
+//                 lo = mid + 1;
+//             } else {
+//                 hi = mid - 1;
+//             }
+//         }
+
+//         if (leftBound == INT_MAX || rightBound == INT_MIN) return 0;
+//         return rightBound - leftBound + 1;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    typedef long long ll;
-    bool isPossible(vector<int>& differences, ll tar, ll lower, ll upper) {
-        int fVal = tar;
-        for (int i = 0; i < differences.size(); i++) {
-            fVal += differences[i];
-            if (fVal < lower || fVal > upper) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        // compute prefix sum bounds
-        long long minPrefix = 0, maxPrefix = 0, sum = 0;
+        long long minPref = 0, maxPref = 0, sum = 0;
+
         for (int diff : differences) {
             sum += diff;
-            minPrefix = min(minPrefix, sum);
-            maxPrefix = max(maxPrefix, sum);
+            minPref = min(minPref, sum);
+            maxPref = max(maxPref, sum);
         }
 
-        ll l = lower - minPrefix;
-        ll r = upper - maxPrefix;
+        long long minStart = lower - minPref;
+        long long maxStart = upper - maxPref;
 
-        ll leftBound = INT_MAX, rightBound = INT_MIN;
-
-        // Left bound
-        ll lo = l, hi = r;
-        while (lo <= hi) {
-            ll mid = lo + (hi - lo) / 2;
-            if (isPossible(differences, mid, lower, upper)) {
-                leftBound = mid;
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
-        }
-
-        // Right bound
-        lo = l, hi = r;
-        while (lo <= hi) {
-            ll mid = lo + (hi - lo) / 2;
-            if (isPossible(differences, mid, lower, upper)) {
-                rightBound = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-
-        if (leftBound == INT_MAX || rightBound == INT_MIN) return 0;
-        return rightBound - leftBound + 1;
+        return max(0LL, maxStart - minStart + 1);
     }
 };
