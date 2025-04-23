a3ce58acc0a0961d1d93177e7b479f81651098a8
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-23 06:15:46 +0530

    Time: 2 ms (49.15%) | Memory: 8.3 MB (27.46%) - LeetSync

--- 1500-count-largest-group/count-largest-group.cpp
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     int countLargestGroup(int n) {
-        unordered_map<int,int>mp;
-        for(int i=1;i<=n;i++)
-        {
-            string s=to_string(i);
-            int sum=0;
-            for(auto &num:s){
-                sum+=(num-'0');
+        unordered_map<int, int> mp;
+        int maxSize = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0, num = i;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
             }
             mp[sum]++;
+            maxSize = max(maxSize, mp[sum]);
         }
 
-        int maxSize=INT_MIN;
-        for(auto &it:mp)
-        {
-            if(it.second>maxSize)
-            {
-                maxSize=it.second;
-            }
-        }
-
-        int ans=0;
-         for(auto &it:mp)
-        {
-            if(it.second==maxSize)
-            {
+        int ans = 0;
+        for (auto &it : mp) {
+            if (it.second == maxSize)
                 ans++;
-            }
         }
         return ans;
     }
