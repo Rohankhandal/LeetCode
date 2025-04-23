5752bc8e1e44b0d2adbefb6742b8096e6b8471d9
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-23 10:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.05%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int cnt = __builtin_popcount(num2); // Count set bits in num2
-        int ans = 0;
+        int setBits = __builtin_popcount(num2);
+        vector<int> result(32, 0);
 
-        // Step 1: Set bits in `num1` from highest to lowest while cnt > 0
-        for (int i = 31; i >= 0; i--) {
-            if ((num1 & (1 << i)) && cnt > 0) {
-                ans |= (1 << i);
-                cnt--;
+        // Step 1: Try to match set bits of num1 from MSB to LSB
+        for (int i = 31; i >= 0 && setBits > 0; --i) {
+            if (num1 & (1 << i)) {
+                result[i] = 1;
+                --setBits;
             }
         }
 
-        // Step 2: Set remaining bits from lowest to highest
-        for (int i = 0; i < 32 && cnt > 0; i++) {
-            if ((ans & (1 << i)) == 0) {
+        // Step 2: If set bits still remain, fill from LSB to MSB
+        for (int i = 0; i < 32 && setBits > 0; ++i) {
+            if ((num1 & (1 << i)) == 0) {
+                result[i] = 1;
+                --setBits;
+            }
+        }
+
+        // Step 3: Construct the result from bits
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            if (result[i]) {
                 ans |= (1 << i);
-                cnt--;
             }
         }
 
