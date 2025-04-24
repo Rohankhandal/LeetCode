0e8041ce9b4a2198b93d400077f1a0a9857c1369
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-24 09:12:18 +0530

    Time: 7 ms (97.72%) | Memory: 41.8 MB (71.22%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int> freq;
-        
-        for(int i = 0; i < n; i++) {  //to count distinct ele
-            freq.insert(nums[i]);
-        }
-        
-        int distinct = freq.size();
-        int ans = 0;
-    
-        for (int i = 0; i < n; i++) {
-            unordered_set<int> temp;
+    int countCompleteSubarrays(vector<int>& nums, int left = 0, int res = 0) {
+        int k = unordered_set<int>(nums.begin(),nums.end()).size();
+        unordered_map<int, int> mpp;
 
-            for(int j = i; j < n; j++) {
-                temp.insert(nums[j]);
-                if (temp.size() == distinct) {
-                    ans++;
-                }
+        for (int i = 0; i < nums.size(); ++i) {
+            mpp[nums[i]]++;
+            while (mpp.size() == k) {
+                res += nums.size() - i;  
+                mpp[nums[left]]--;
+                if (mpp[nums[left]] == 0) mpp.erase(nums[left]);
+                left++;
             }
         }
-    
-    return ans;   
+        return res;
     }
 };
+
+
+// class Solution {
+// public:
+//     int countCompleteSubarrays(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_set<int> freq;
+        
+//         for(int i = 0; i < n; i++) {  //to count distinct ele
+//             freq.insert(nums[i]);
+//         }
+        
+//         int distinct = freq.size();
+//         int ans = 0;
+    
+//         for (int i = 0; i < n; i++) {
+//             unordered_set<int> temp;
+
+//             for(int j = i; j < n; j++) {
+//                 temp.insert(nums[j]);
+//                 if (temp.size() == distinct) {
+//                     ans++;
+//                 }
+//             }
+//         }
+    
+//     return ans;   
+//     }
+// };
