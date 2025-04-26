1694c8c70d722871d45cf5af7cd2e5276d296523
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-04-27 04:10:12 +0530

    Time: 24 ms (5.15%) | Memory: 94.9 MB (7.92%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,33 +1,28 @@
-//Approach-1 (Brute Force)
-//Find all subarrays and check if min is minK and max is maxK
-//Code will be provided soon for brute force
-
-//Approach-2 (Optimal O(n)) - Sliding Window
 class Solution {
-public:
-    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long ans = 0;
-        
-        int minPosition = -1;
-        int maxPosition = -1;
-        int leftBound   = -1;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            
-            if(nums[i] < minK || nums[i] > maxK)
-                leftBound = i;
-            
-            if(nums[i] == minK)
-                minPosition = i;
-            if(nums[i] == maxK)
-                maxPosition = i;
-            
-            int count = min(maxPosition, minPosition) - leftBound;
-            
-            ans += (count <= 0) ? 0 : count;
-            
+    public:
+        long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+            long long count = 0, left = 0;
+            deque<int> dq_min, dq_max;
+    
+            for (int i = 0; i < nums.size(); ++i) {
+                if (nums[i] < minK || nums[i] > maxK) {
+                    dq_min.clear();
+                    dq_max.clear();
+                    left = i + 1;
+                    continue;
+                }
+    
+                while (!dq_min.empty() && nums[dq_min.back()] >= nums[i]) dq_min.pop_back();
+                dq_min.push_back(i);
+    
+                while (!dq_max.empty() && nums[dq_max.back()] <= nums[i]) dq_max.pop_back();
+                dq_max.push_back(i);
+    
+                if (nums[dq_min.front()] == minK && nums[dq_max.front()] == maxK) {
+                    int start = min(dq_min.front(), dq_max.front());
+                    count += (start - left + 1);
+                }
+            }
+            return count;
         }
-        
-        return ans;
-    }
-};
+    };
