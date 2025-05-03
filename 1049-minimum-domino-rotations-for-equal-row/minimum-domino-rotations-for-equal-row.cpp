72e2916f7ffb13caf0e67ce9a99d46dde2cda13b
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-05-03 07:40:37 +0530

    Time: 3 ms (67.46%) | Memory: 115.4 MB (76.51%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -1,22 +1,71 @@
 class Solution {
 public:
-    int check(int target, vector<int>& tops, vector<int>& bottoms) {
-        int rotateTop = 0, rotateBottom = 0;
-        for (int i = 0; i < tops.size(); ++i) {
-            if (tops[i] != target && bottoms[i] != target) {
-                return INT_MAX; // Not possible
-            }
-            if (tops[i] != target) rotateTop++;
-            if (bottoms[i] != target) rotateBottom++;
-        }
-        return min(rotateTop, rotateBottom);
-    }
-
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int result = INT_MAX;
-        for (int val = 1; val <= 6; ++val) {
-            result = min(result, check(val, tops, bottoms));
+        int n = tops.size();
+        int ans = INT_MAX;
+
+        for (int target = 1; target <= 6; ++target) {
+            bool flag = true;
+            int cnt1 = 0;
+            for (int i = 0; i < n; i++) {
+                if (tops[i] != target) {
+                    if (bottoms[i] != target) {
+                        flag = false;
+                        break;
+                    } else {
+                        cnt1++;
+                    }
+                }
+            }
+            if (flag == false) {
+                cnt1 = INT_MAX;
+            }
+
+            flag = true;
+            int cnt2 = 0;
+            for (int i = 0; i < n; i++) {
+                if (bottoms[i] != target) {
+                    if (tops[i] != target) {
+                        flag = false;
+                        break;
+                    } else {
+                        cnt2++;
+                    }
+                }
+            }
+            if (flag == false) {
+                cnt2 = INT_MAX;
+            }
+
+            ans = min(ans, min(cnt1, cnt2));
         }
-        return (result == INT_MAX) ? -1 : result;
+
+        if (ans == INT_MAX) return -1;
+        return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int check(int target, vector<int>& tops, vector<int>& bottoms) {
+//         int rotateTop = 0, rotateBottom = 0;
+//         for (int i = 0; i < tops.size(); ++i) {
+//             if (tops[i] != target && bottoms[i] != target) {
+//                 return INT_MAX; // Not possible
+//             }
+//             if (tops[i] != target) rotateTop++;
+//             if (bottoms[i] != target) rotateBottom++;
+//         }
+//         return min(rotateTop, rotateBottom);
+//     }
+
+//     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+//         int result = INT_MAX;
+//         for (int val = 1; val <= 6; ++val) {
+//             result = min(result, check(val, tops, bottoms));
+//         }
+//         return (result == INT_MAX) ? -1 : result;
+//     }
+// };
