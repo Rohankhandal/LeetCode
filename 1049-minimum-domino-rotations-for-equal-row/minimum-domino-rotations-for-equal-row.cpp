5e7c5aa56af6f68891f9025f3a86848e87e95bb5
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-05-03 07:39:00 +0530

    Time: 0 ms (100.00%) | Memory: 115.4 MB (48.71%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -1,82 +1,22 @@
 class Solution {
 public:
+    int check(int target, vector<int>& tops, vector<int>& bottoms) {
+        int rotateTop = 0, rotateBottom = 0;
+        for (int i = 0; i < tops.size(); ++i) {
+            if (tops[i] != target && bottoms[i] != target) {
+                return INT_MAX; // Not possible
+            }
+            if (tops[i] != target) rotateTop++;
+            if (bottoms[i] != target) rotateBottom++;
+        }
+        return min(rotateTop, rotateBottom);
+    }
+
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int n=tops.size();
-        unordered_map<int,int>mp;
-        int maxFreq=0;
-        int maxEle=0;
-        for(auto &it:tops)
-        {
-            mp[it]++;
-            if(mp[it]>maxFreq)
-            {
-                maxFreq=mp[it];
-                maxEle=it;
-            }
+        int result = INT_MAX;
+        for (int val = 1; val <= 6; ++val) {
+            result = min(result, check(val, tops, bottoms));
         }
-        int ans=-1;
-        cout<<maxEle<<" "<<maxFreq<<endl;
-
-        bool flag=true;
-        int cnt1=0;
-        for(int i=0;i<n;i++)
-        {
-            if(tops[i]!=maxEle)
-            {
-                if(bottoms[i]!=maxEle)
-                {
-                    flag=false;
-                    break;
-                }
-                else{
-                    // swap(tops[i],bottoms[i]);
-                    cnt1++;
-                }
-            }
-        }
-        if(flag==false)
-        {
-            cout<<"H"<<endl;
-            cnt1=INT_MAX;
-        }
-
-        mp.clear();
-        maxFreq=0;
-        maxEle=0;
-        flag=true;
-        for(auto &it:bottoms)
-        {
-            mp[it]++;
-            if(mp[it]>maxFreq)
-            {
-                maxFreq=mp[it];
-                maxEle=it;
-            }
-        }
-
-       cout<<maxEle<<" "<<maxFreq<<endl;
-        int cnt2=0;
-        for(int i=0;i<n;i++)
-        {
-            if(bottoms[i]!=maxEle)
-            {
-                if(tops[i]!=maxEle)
-                {
-                    flag=false;
-                    break;
-                }
-                else{
-                    cout<<"F"<<endl;
-                    swap(tops[i],bottoms[i]);
-                    cnt2++;
-                }
-            }
-        }
-        if(flag==false) cnt2=INT_MAX;
-        ans=min(cnt1,cnt2);
-
-        if(ans==INT_MAX) return -1;
-
-        return ans;
+        return (result == INT_MAX) ? -1 : result;
     }
 };
