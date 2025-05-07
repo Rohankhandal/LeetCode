4319847728732448a708875666fa5d8416c24005
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-05-07 19:05:46 +0530

    Time: 36 ms (25.18%) | Memory: 32 MB (25.06%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,46 +1,43 @@
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n=moveTime.size();
-        int m=moveTime[0].size();
+        int n = moveTime.size(), m = moveTime[0].size();
+        vector<vector<int>> minArrival(n, vector<int>(m, INT_MAX));
+        
+        // Priority queue holds: {current_time, x, y}
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0, 0, 0});  // Start at (0, 0) at time 0
+        minArrival[0][0] = 0;
+        
+        vector<vector<int>> dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        
+        while (!pq.empty()) {
+           int t= pq.top()[0];
+            int x= pq.top()[1];
+            int y= pq.top()[2]; pq.pop();
+            
+            if (x == n - 1 && y == m - 1) return t;
+            if (t > minArrival[x][y]) continue; // Already found a better way
+            
+            for (auto& d : dirs) {
+                int nx = x + d[0], ny = y + d[1];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    int wait = moveTime[nx][ny];
+                    int nextTime = (t >= wait) ? t + 1 : wait + 1;
 
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-
-        pq.push({0,0,0});  //time,i,j
-        vector<vector<int>>dir{{-1,0},{1,0},{0,-1},{0,1}};
-
-        moveTime[0][0]=-1;
-
-        while(!pq.empty())
-        {
-            int t=pq.top()[0];
-            int x=pq.top()[1];
-            int y=pq.top()[2];
-            pq.pop();
-
-            if(x==n-1 && y==m-1) return t;
-
-            for(auto &d:dir)
-            {
-                int i=d[0]+x;
-                int j=d[1]+y;
-                if(i>=0 & j>=0 && i<n && j<m && moveTime[i][j]!=-1)
-                {
-                    int newT=t;
-                    if(moveTime[i][j]<=t)
-                    {
-                        newT++;
+                    if (nextTime < minArrival[nx][ny]) {
+                        minArrival[nx][ny] = nextTime;
+                        pq.push({nextTime, nx, ny});
                     }
-                    else {
-                        newT=moveTime[i][j]+1;
-                    }
-                    
-                    moveTime[i][j]=-1;
-                    pq.push({newT,i,j});
                 }
             }
         }
-        return -1;
-
+        
+        return -1; // Should not happen under valid constraints
     }
 };
