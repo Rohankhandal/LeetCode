8fe1762e8224e52a4df9ec792657179f28cc224a
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-05-16 20:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 29.8 MB (99.25%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -3,41 +3,20 @@ public:
     vector<string> getLongestSubsequence(vector<string>& words,
                                          vector<int>& groups) {
         vector<string> ans;
-
-        int startIdx = 0;
-        int maxLen = 1;
         int n = words.size();
-        for (int i = 0; i < n; i++) {
-            int val = 1 - groups[i];
-            int len = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (groups[j] != val)
-                    continue;
-                else {
-                    len++;
-                    val = 1 - groups[j];
-                }
-            }
-            if (len > maxLen) {
-                cout<<i<<endl;
-                startIdx = i;
-                maxLen = len;
-            }
-        }
+        ans.push_back(words[0]);
+        int val = 1 - groups[0];
 
-        int len = 1;
-        ans.push_back(words[startIdx]);
-        int val = 1-groups[startIdx];
-        for (int j = startIdx + 1; j < n; j++) {
+        for (int i = 1; i < n; i++) {
 
-            
-            if (groups[j] != val)
+            if (groups[i] != val)
                 continue;
             else {
-                ans.push_back(words[j]);
-                val = 1 - groups[j];
+                ans.push_back(words[i]);
+                val = 1 - groups[i];
             }
         }
+
         return ans;
     }
 };
