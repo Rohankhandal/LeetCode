69ad7562abf1d9987dae14eef948d11ffb053d2b
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-05-23 15:41:18 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (62.10%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
         int n1=nums1.size();
         int n2=nums2.size();
-
-        int left=(n1+n2+1)/2;
-        if(n2<n1) return findMedianSortedArrays(nums2,nums1);
-
+        int p1=(n1+n2+1)/2;
         int l=0,r=n1;
+
         while(l<=r)
         {
-
             int mid1=(l+r)/2;
-            int mid2=left-mid1;
+            int mid2=p1-mid1;
 
-            int l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
-
-            if(mid1<n1) r1=nums1[mid1];
-            if(mid2<n2) r2=nums2[mid2];
-            if(mid1>0) l1=nums1[mid1-1];
-            if(mid2>0) l2=nums2[mid2-1];
+            int l1=(mid1==0)?INT_MIN:nums1[mid1-1];
+            int l2=(mid2==0)?INT_MIN:nums2[mid2-1];
+            int r1=(mid1==n1)?INT_MAX:nums1[mid1];
+            int r2=(mid2==n2)?INT_MAX:nums2[mid2];
 
             if(l1<=r2 && l2<=r1)
             {
-                if((n1+n2)%2==0) //eve
+                if((n1+n2)%2==0)
                 {
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                }else
+                     return (max(l1,l2)+min(r1,r2))/2.0;
+                }
+                else
                 {
                     return max(l1,l2);
                 }
+               
             }
-            else if(l1>r2)
-            {
-                r=mid1-1;
-            }
-            else
+            else if(l2>r1)
             {
                 l=mid1+1;
             }
+            else
+            {
+                r=mid1-1;
+            }
 
         }
-        return -1.0;
-        
+
+        return 0;
     }
 };
