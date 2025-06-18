3fd033dca203e54bc8820d35d33fbeba238d08e6
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-06-18 17:06:07 +0530

    Time: 305 ms (19.30%) | Memory: 26.2 MB (31.28%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-    bool isPalindrome(string &s,int l,int r)
-    {
-        while(l<r)
-        {
-            if(s[l]!=s[r]) return false;
-            l++;r--;
-        }
-        return true;
-    }
     string longestPalindrome(string s) {
         int n=s.size();
-        string ans="";
-        int maxLen=0;
-        for(int i=0;i<n;i++)
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        for(int L=1;L<=n;L++)
         {
-            for(int j=i;j<n;j++)
-            {
-                if(isPalindrome(s,i,j))
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+
+                if(i==j)
                 {
-                    int len=j-i+1;
-                    if(len>maxLen)
-                    {
-                        maxLen=len;
-                        ans=s.substr(i,j-i+1);
-                    }
+                    dp[i][j]=true;
+                }
+                else if(i+1==j)
+                {
+                    dp[i][j]=(s[i]==s[j]);
+                }
+                else
+                {
+                    dp[i][j]=(s[i]==s[j])&&dp[i+1][j-1];
                 }
             }
         }
-        return ans;
+        
+        int idx=0;
+        int maxLen=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int L=0;L<n;L++)
+            {
+                if(dp[i][L])
+                {
+                    int len=L-i+1;
+                    if(len>maxLen)
+                    {
+                        maxLen=len;
+                        idx=i;
+                    }
+                }
+                // cout<<dp[i][L]<<" ";
+            }
+            // cout<<endl;
+        }
+        // cout<<maxLen<<" "<<idx<<endl;
+        return s.substr(idx,maxLen);
     }
 };
