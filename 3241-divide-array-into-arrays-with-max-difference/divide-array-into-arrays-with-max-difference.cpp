36737a0a80db4bc00b9989282dfdfc7e59fd3098
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-06-18 08:44:22 +0530

    Time: 47 ms (66.75%) | Memory: 120 MB (64.59%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,17 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>>ans;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i=i+3)
-        {
-            if(nums[i+2]-nums[i]>k)
-            {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        int n = nums.size();
+
+        if (n % 3 != 0) return {}; // Optional sanity check
+
+        for (int i = 0; i < n; i += 3) {
+            int a = nums[i], b = nums[i + 1], c = nums[i + 2];
+            if (c - a <= k) {
+                result.push_back({a, b, c});
+            } else {
                 return {};
             }
-            ans.push_back({nums[i],nums[i+1],nums[i+2]});
         }
-        return ans;
+
+        return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> divideArray(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end());
+
+//         vector<vector<int>>ans;
+//         int i=0;
+//         int n=nums.size();
+//         while(i<n)
+//         {
+//             int first=i;
+//             int last=i+2;
+//             if(abs(nums[first]-nums[last])<=k) 
+//             {
+//                 vector<int>temp;
+//                 for(int j=i;j<=last;j++)
+//                 {
+//                     temp.push_back(nums[j]);
+//                 }
+//                 ans.push_back(temp);
+//             }
+//             else
+//             {
+//                 return {};
+//             }
+//             i=last+1;
+//         }
+
+//         return ans;
+
+//     }
+// };
