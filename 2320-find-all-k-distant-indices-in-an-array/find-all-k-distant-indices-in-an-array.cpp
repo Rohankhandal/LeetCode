f99078c561d7b5518efa4cfd172cde70d11ef76d
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-06-24 06:52:58 +0530

    Time: 19 ms (34.31%) | Memory: 14.7 MB (55.05%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -1,23 +1,52 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int>ans;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            int cnt=0;
-            for(int j=0;j<n;j++)
-            {
-                // if(i==j) continue;
+        int n = nums.size();
+        vector<bool> valid(n, false);
+        vector<int> ans;
 
-                if(abs(i-j)<=k && nums[j]==key)
-                {
-                    cnt++;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == key) {
+                int start = max(0, i - k);
+                int end = min(n - 1, i + k);
+                for (int j = start; j <= end; ++j) {
+                    valid[j] = true;
                 }
             }
-
-            if(cnt>=1) ans.push_back(i);
         }
+
+        for (int i = 0; i < n; ++i) {
+            if (valid[i]) {
+                ans.push_back(i);
+            }
+        }
+
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+//         vector<int>ans;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             int cnt=0;
+//             for(int j=0;j<n;j++)
+//             {
+//                 // if(i==j) continue;
+//                 if(abs(i-j)<=k && nums[j]==key)
+//                 {
+//                     cnt++;
+//                 }
+//             }
+
+//             if(cnt>=1) ans.push_back(i);
+//         }
+//         return ans;
+
+//     }
+// };
