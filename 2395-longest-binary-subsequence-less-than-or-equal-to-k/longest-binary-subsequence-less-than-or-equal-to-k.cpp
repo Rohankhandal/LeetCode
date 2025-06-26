bb611737ea9d7e44fbd9c26070645234988fd34f
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-06-26 17:30:43 +0530

    Time: 7 ms (23.81%) | Memory: 9.4 MB (73.55%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        int n = s.size();
-        int count = 0;
-        long long val = 0;
-        int bit = 0;
-
-      
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[i] == '0') {
-                count++; 
-            } else {
-                if (bit < 32 && (val + (1LL << bit)) <= k) {
-                    val += (1LL << bit);
-                    count++;
-                }
-            }class Solution {
-public:
-    int longestSubsequence(string s, int k) {
       long long n=s.size(),len=0,x=0;
       bool safe=true;
       for(int i=n-1;i>=0;i--){
         if(s[i]=='1' && safe==true){
           cout<<len<<" ";
-          long long num=(1LL<<len);
+          if(len<=32)
+          {
+            long long num=(1LL<<len);
           
            if (x + num <= k) {
             x += num;
@@ -32,6 +17,8 @@ public:
              safe = false;
             }
         
+          }
+          
         }
         else if(s[i]=='0'){
           len++;
@@ -39,11 +26,5 @@ public:
    
       }
       return len;
-    }
-};
-            bit++;
-        }
-
-        return count;
     }
 };
