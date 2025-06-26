45f71ce55b81adf23e4afb0be5e1b60050915a75
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-06-26 17:22:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (50.79%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -6,16 +6,41 @@ public:
         long long val = 0;
         int bit = 0;
 
-        // Traverse from the right (LSB side)
+      
         for (int i = n - 1; i >= 0; i--) {
             if (s[i] == '0') {
-                count++; // zero adds nothing, always safe
+                count++; 
             } else {
                 if (bit < 32 && (val + (1LL << bit)) <= k) {
                     val += (1LL << bit);
                     count++;
                 }
+            }class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+      long long n=s.size(),len=0,x=0;
+      bool safe=true;
+      for(int i=n-1;i>=0;i--){
+        if(s[i]=='1' && safe==true){
+          cout<<len<<" ";
+          long long num=(1LL<<len);
+          
+           if (x + num <= k) {
+            x += num;
+            len++;
+            } else {       
+             safe = false;
             }
+        
+        }
+        else if(s[i]=='0'){
+          len++;
+        }
+   
+      }
+      return len;
+    }
+};
             bit++;
         }
 
