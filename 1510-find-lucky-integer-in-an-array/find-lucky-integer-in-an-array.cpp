a781a9b2d5c21b63cc82a01a78e9c5b8f6914fed
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-07-05 12:14:04 +0530

    Time: 3 ms (21.69%) | Memory: 13.9 MB (65.75%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int ans=-1;
+        unordered_map<int,int>mp;
+        for(auto &it:arr)
+        {
+            mp[it]++;
+        }
+
+        for(auto &it:mp)
+        {
+            if(it.first==it.second)
+            {
+                if(ans<it.first)
+                {
+                    ans=it.first;
+                }
+            }
+        }
+        return ans;
+    }
+};
