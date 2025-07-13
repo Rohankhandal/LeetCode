0a7379d74e9f5f5d238cd1e06c9f39383f3f0b04
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-07-13 12:59:00 +0530

    Time: 37 ms (12.56%) | Memory: 28.5 MB (39.28%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,63 +1,26 @@
-// e.g:- 1,1,1,2 and 2,1,1,1 are same {that's why we don't use for loop}
-
-//Approach-1 (Recursion + Mempozation) : O(n*amount)
 class Solution {
 public:
-    int n;
-    int t[301][5001];
-    
+    int f(int idx,int amount,vector<int>&coins, vector<vector<int>>&dp)
+    {
+        if(amount==0) return 1;
+        if(amount<0) return INT_MAX;
+        if(idx>=coins.size()) return 0;
 
-    // int solve(int i, vector<int>& coins, int amount) {
-        
-    //     if(amount == 0)
-    //         return t[i][amount] = 1;
-        
-    //     if(i == n || amount < 0)
-    //         return 0;
-        
-    //      if(t[i][amount] != -1)
-    //         return t[i][amount];
-        
-    //     if(coins[i] > amount)
-    //         return t[i][amount] = solve(i+1, coins, amount);
-        
-    //     int take = solve(i, coins, amount-coins[i]);
-    //     int skip = solve(i+1, coins, amount);
-        
-    //     return t[i][amount] = take+skip;
-        
-    // }
-    
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
 
-   int solveTab(vector<int>& coins, int amt) {
-        int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amt + 1, 0));
+        int take=f(idx,amount-coins[idx],coins,dp);
         
-        // There's exactly one way to make amount 0 (by not using any coins)
-        for(int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-        
-        // Fill the DP table
-        for(int i = 1; i <= n; i++) {
-            for(int amount = 1; amount <= amt; amount++) {
-                int take = 0;
-                if(amount >= coins[i - 1])  // Use i-1 because dp is 1-based indexing
-                    take = dp[i][amount - coins[i - 1]];
-                int skip = dp[i - 1][amount];
-                
-                dp[i][amount] = take + skip;
-            }
-        }
-        
-        return dp[n][amt];  // Return the number of ways to make amount using all coins
+        if(take==INT_MAX) take=0;
+
+        int skip=f(idx+1,amount,coins,dp);
+
+        if(skip==INT_MAX) skip=0;
+
+        return dp[idx][amount]=take+skip;
     }
     int change(int amount, vector<int>& coins) {
-        n = coins.size();
-        // memset(t, -1, sizeof(t));
-        // return solve(0, coins, amount);
-
-        return solveTab(coins,amount);
-
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return f(0,amount,coins,dp);
     }
 };
