e6b24714bede35f2e928a8b112673fa5923a50cd
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-07-14 18:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (93.84%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,30 +1,54 @@
 class Solution {
 public:
-    bool canMeasureWater(int x, int y, int target) {
-        queue<int> q;
-        unordered_set<int> visit;
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
 
-        q.push(0);
-        visit.insert(0);
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        if (targetCapacity > jug1Capacity + jug2Capacity) return false;
+        if (targetCapacity == 0) return true;
 
-        vector<int> dir {x, -x, y, -y};
-
-        while (!q.empty()) {
-            int val = q.front(); q.pop();
-
-            if (val == target) return true;
-
-            for (auto &d : dir) {
-                int next = val + d;
-
-                // ✅ Only consider valid, non-negative and not too large values
-                if (next >= 0 && next <= x + y && !visit.count(next)) {
-                    q.push(next);
-                    visit.insert(next);
-                }
-            }
-        }
-
-        return false;
+        int g = gcd(jug1Capacity, jug2Capacity);
+        return (targetCapacity % g == 0);
     }
 };
+
+
+
+// // ⏱ Time Complexity
+// // Worst-case states: O(x + y)
+
+// // Each state is visited once → Time: O(x + y)
+
+// // Space: O(x + y) for queue and visited set
+// class Solution {
+// public:
+//     bool canMeasureWater(int x, int y, int target) {
+//         queue<int> q;
+//         unordered_set<int> visit;
+
+//         q.push(0);
+//         visit.insert(0);
+
+//         vector<int> dir {x, -x, y, -y};
+
+//         while (!q.empty()) {
+//             int val = q.front(); q.pop();
+
+//             if (val == target) return true;
+
+//             for (auto &d : dir) {
+//                 int next = val + d;
+
+//                 // ✅ Only consider valid, non-negative and not too large values
+//                 if (next >= 0 && next <= x + y && !visit.count(next)) {
+//                     q.push(next);
+//                     visit.insert(next);
+//                 }
+//             }
+//         }
+
+//         return false;
+//     }
+// };
