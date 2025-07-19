856ff79782ba35399e16a4bedc91d25612aee5f7
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-07-20 01:13:47 +0530

    Time: 1977 ms (5.10%) | Memory: 552.9 MB (5.10%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        vector<string>ans;
-        sort(folder.begin(),folder.end());
-        int n=folder.size();
-        string prev="";
-        for(auto &s:folder)
-        {
-            if(prev=="")
-            {
-                ans.push_back(s);
-                 prev=s+"/";
+        sort(folder.begin(), folder.end());
+        vector<string> res;
+
+        for (string& f : folder) {
+            bool isSub = false;
+            for (string& r : res) {
+                if (f.size() > r.size() && f.substr(0, r.size()) == r && f[r.size()] == '/') {
+                    isSub = true;
+                    break;
+                }
             }
-            else if(s.find(prev)==0)
-            {
-                continue;
-            }
-            else
-            {
-                prev=s+"/";
-                ans.push_back(s);
-            }
+            if (!isSub) res.push_back(f);
         }
-        return ans;
+        return res;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<string> removeSubfolders(vector<string>& folder) {
+//         vector<string>ans;
+//         sort(folder.begin(),folder.end());
+//         int n=folder.size();
+//         string prev="";
+//         for(auto &s:folder)
+//         {
+//             if(prev=="")
+//             {
+//                 ans.push_back(s);
+//                  prev=s+"/";
+//             }
+//             else if(s.find(prev)==0)
+//             {
+//                 continue;
+//             }
+//             else
+//             {
+//                 prev=s+"/";
+//                 ans.push_back(s);
+//             }
+//         }
+//         return ans;
+//     }
+// };
