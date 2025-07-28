fc4f57f004f214eb8bad6614ae565071c7a74ac7
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-07-28 09:35:12 +0530

    Time: 130 ms (15.34%) | Memory: 313.6 MB (8.70%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,27 +1,77 @@
+// //Approach-1 (Using simple recursion to find subsets)
+// //T.C : O(2^n)
+// //S.C : O(1)
+// class Solution {
+// public:
+//     //O(2^n)
+//     int countSubsets(int idx, int currOr, vector<int>& nums, int maxOr) {
+//         if(idx == nums.size()) {
+//             if(currOr == maxOr)
+//                 return 1; //Found one subset
+//             return 0;
+//         }
+
+//         //Take nums[idx]
+//         int takeCount = countSubsets(idx+1, currOr | nums[idx], nums, maxOr);
+
+//         //Not taken nums[idx]
+//         int notTakeCount = countSubsets(idx+1, currOr, nums, maxOr);
+
+
+//         return takeCount + notTakeCount;
+
+//     }
+
+//     int countMaxOrSubsets(vector<int>& nums) {
+//         int maxOr = 0;
+//         for(int &num : nums) {
+//             maxOr |= num;
+//         }
+
+//         int currOr = 0;
+//         return countSubsets(0, currOr, nums, maxOr);
+//     }
+// };
+
+
+
+//Approach-2 (Memoizing to store subproblems result)
+//T.C : O(n*maxOr)
+//S.C : O(1)
 class Solution {
 public:
-    int countMaxOrSubsets(vector<int>& nums) {
-       int n=nums.size();
-       int maxOr=0;
-       for(auto &num:nums)
-       {
-        maxOr|=num;
-       }
-       int cnt=0;
+    //O(2^n)
+    int countSubsets(int idx, int currOr, vector<int>& nums, int maxOr, vector<vector<int>>& t) {
+        if(idx == nums.size()) {
+            if(currOr == maxOr)
+                return t[idx][currOr] = 1; //Found one subset
+            return t[idx][currOr] = 0;
+        }
 
-       int total=(1<<n);
-       for(int i=0;i<total;i++)
-       {
-         int orVal=0;
-         for(int j=0;j<n;j++)
-         {
-            if(i&(1<<j)){
-                orVal|=nums[j];
-            }
-         }
-      
-         cnt+=(orVal==maxOr);
-       }
-       return cnt;
+        if(t[idx][currOr] != -1) {
+            return t[idx][currOr];
+        }
+
+        //Take nums[idx]
+        int takeCount = countSubsets(idx+1, currOr | nums[idx], nums, maxOr, t);
+
+        //Not taken nums[idx]
+        int notTakeCount = countSubsets(idx+1, currOr, nums, maxOr, t);
+
+
+        return t[idx][currOr] = takeCount + notTakeCount;
+
+    }
+
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOr = 0;
+        for(int &num : nums) {
+            maxOr |= num;
+        }
+        int n = nums.size();
+
+        vector<vector<int>> t(n+1, vector<int>(maxOr+1, -1));
+        int currOr = 0;
+        return countSubsets(0, currOr, nums, maxOr, t);
     }
 };
