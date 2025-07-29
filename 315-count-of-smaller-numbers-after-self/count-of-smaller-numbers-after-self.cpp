b38db564e6a3273b9be35521b307bccc61cece42
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-07-29 16:07:00 +0530

    Time: 363 ms (60.38%) | Memory: 252.5 MB (30.93%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,64 +1,50 @@
 class Solution {
 public:
-    void merge(vector<pair<int,int>>&v,int l,int mid,int r,vector<int>&ans)
-    {
-        int n1=mid-l+1;
-        int n2=r-mid;
+    void merge(vector<pair<int, int>>& v, int l, int mid, int r, vector<int>& ans) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
 
-        vector<pair<int,int>>dummy(n1+n2);
+        vector<pair<int, int>> dummy(n1 + n2);
+        int i = l, j = mid + 1, k = 0;
 
-        int i=l,j=mid+1;
-        int k=0;
-        while(i<=mid && j<=r)
-        {
-            if(v[i].first<=v[j].first)
-            {
-                dummy[k++]=v[j];
-                j++;
-            }
-            else{
-                ans[v[i].second]+=(r-j+1);
-                dummy[k++]=v[i];
-                i++;
+        while (i <= mid && j <= r) {
+            if (v[i].first <= v[j].first) {
+                // No element on the right is smaller than v[i]
+                dummy[k++] = v[j++];
+            } else {
+                // All elements from v[j] to v[r] are smaller than v[i]
+                ans[v[i].second] += (r - j + 1);
+                dummy[k++] = v[i++];
             }
         }
 
-        while(i<=mid)
-        {
-            dummy[k++]=v[i];
-            i++;
-        }
-        while(j<=r)
-        {
-            dummy[k++]=v[j];
-            j++;
-        }
-        k=0;
+        while (i <= mid) dummy[k++] = v[i++];
+        while (j <= r) dummy[k++] = v[j++];
 
-        for(int i=l;i<=r;i++)
-        {
-            v[i]=dummy[k++];
+        for (int p = 0; p < k; ++p) {
+            v[l + p] = dummy[p];
         }
     }
-    void mergeSort(vector<pair<int,int>>&v,int l,int r,vector<int>&ans)
-    {
-        if(l>=r) return ;
 
-        int mid=(l+r)/2;
-        mergeSort(v,l,mid,ans);
-        mergeSort(v,mid+1,r,ans);
-        merge(v,l,mid,r,ans);
+    void mergeSort(vector<pair<int, int>>& v, int l, int r, vector<int>& ans) {
+        if (l >= r) return;
+
+        int mid = (l + r) / 2;
+        mergeSort(v, l, mid, ans);
+        mergeSort(v, mid + 1, r, ans);
+        merge(v, l, mid, r, ans);
     }
+
     vector<int> countSmaller(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
+        vector<pair<int, int>> v;
+        vector<int> ans(n, 0);
 
-        vector<int>ans(n,0);
-        vector<pair<int,int>>v;
-        for(int i=0;i<n;i++)
-        {
-            v.push_back({nums[i],i});
+        for (int i = 0; i < n; ++i) {
+            v.push_back({nums[i], i});
         }
-        mergeSort(v,0,n-1,ans);
+
+        mergeSort(v, 0, n - 1, ans);
         return ans;
     }
 };
