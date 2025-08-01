92e28ceb194feba7d86dc547bce0922f32185799
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-08-01 11:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (31.06%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,34 +1,28 @@
-//Approach (Optimal)
-//T.C : O(n)
-//S.C : O(1)
 class Solution {
 public:
-    vector<int> singleNumber(vector<int>& nums) {
-        long long xor_r = 0;
-
-        for(int &num : nums) {
-            xor_r ^= num;
+    vector<int> singleNumber(vector<int>& arr) {
+        long long  xorVal=0;
+        for(auto &num:arr)
+        {
+            xorVal^=num;
         }
-
-        //mask -> right most set bit search 
-        int mask = (xor_r) & (-xor_r);
-        /*
-            It works because if you take the two's complement negation of a number, first you complement it,
-            setting all zeroes to the right of the lowest set bit to one and the lowest set bit to zero,
-            then you add one, setting the bits on the right to zero and the lowest set bit becomes one again, ending the carry chain.
-        */
-
-        int groupa = 0;
-        int groupb = 0;
-
-        for(int &num : nums) {
-            if(num & mask) {
-                groupa ^= num;
-            } else {
-                groupb ^= num;
+        int n=arr.size();
+        
+        long long  firstRightSet=(xorVal&(xorVal-1) ^ xorVal);
+        // cout<<firstRightSet<<endl;
+        int a=0,b=0;
+        for(int i=0;i<n;i++)
+        {
+            if(firstRightSet&arr[i]){
+                a^=arr[i];
+            }
+            else
+            {
+                b^=arr[i];
             }
         }
-
-        return {groupa, groupb};
+        vector<int>ans{a,b};
+        sort(ans.rbegin(),ans.rend());
+        return ans;
     }
 };
