2d834d94ae19d60e337a0eb121b12f7b71a1de47
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-08-13 22:31:23 +0530

    Time: 5 ms (32.93%) | Memory: 9.2 MB (5.34%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,9 +1,26 @@
+// n=3^i ; where i=>interger
+// take log3 both side
+// log3(n)=log3(3^i);
+// log3(n)=i*1;
+// so i=log10(n)/log10(3);   //if i is interger then true
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0) return false;
-        if(n==1) return true;
-
-        return (n>0 && (n%3==0) && isPowerOfThree(n/3));
+        if(n <= 0) return false;
+        double logValue = log10(n) / log10(3);
+        return logValue == (int)logValue;  //if logValue is interger then, true
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(n==0) return false;
+//         if(n==1) return true;
+
+//         return (n>0 && (n%3==0) && isPowerOfThree(n/3));
+//     }
+// };
