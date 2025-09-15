28849adc676b9debbd68ff1d5940cd8cf9ac179f
Rohan Khandal <84795512+Rohankhandal@users.noreply.github.com>
2025-09-15 10:19:08 +0530

    Time: 5 ms (52.35%) | Memory: 59 MB (37.65%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.js
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.js
@@ -3,19 +3,45 @@
  * @return {number[]}
  */
 var intersection = function(nums) {
-    var ans=[];
-    var i=0;
-    for(var num of nums[0]){
-        console.log(num);
-        var flag=true;
-        for(var i=1;i<nums.length;i++){
-            if(!nums[i].includes(num)){
-                flag=false;
-                break;
+    // var ans=[];
+    // var i=0;
+    // for(var num of nums[0]){
+    //     // console.log(num);
+    //     var flag=true;
+    //     for(var i=1;i<nums.length;i++){
+    //         if(!nums[i].includes(num)){
+    //             flag=false;
+    //             break;
+    //         }
+    //     }
+    //     if(flag) ans.push(num);
+    // }
+    // ans.sort((a,b)=>a-b);
+    // return ans;
+
+
+    let freq=new Map();
+    let k=nums.length;
+
+    for(let arr of nums){
+        let unique= new Set(arr);
+        for(let num of unique){
+            if (freq[num] === undefined) {
+                freq[num] = 1;
+            } else {
+                freq[num] = freq[num] + 1;
             }
         }
-        if(flag) ans.push(num);
     }
-    ans.sort((a,b)=>a-b);
+
+    // collect numbers that appear in all arrays
+    let ans = [];
+    for (let num in freq) {
+        if (freq[num] === k) {
+            ans.push(Number(num));
+        }
+    }
+
+    ans.sort((a, b) => a - b);   // sort in ascending order
     return ans;
 };
